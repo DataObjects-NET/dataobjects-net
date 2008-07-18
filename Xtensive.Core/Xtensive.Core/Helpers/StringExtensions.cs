8e53ecc06ac6970adea085421a2643dbb8f3aeb1
Dima Maximov <dmitri.maximov@gmail.com>
2008-07-18 07:37:59 +0000

    - String.IsNullOrEmpty extension method was introduced.

---
+++ Xtensive.Core/Xtensive.Core/Helpers/StringExtensions.cs
@@ -0,0 +1,28 @@
+// Copyright (C) 2008 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Dmitri Maximov
+// Created:    2008.07.18
+
+namespace Xtensive.Core.Helpers
+{
+  /// <summary>
+  /// <see cref="string"/> related extension methods.
+  /// </summary>
+  public static class StringExtensions
+  {
+    /// <summary>
+    /// Indicates whether the specified <paramref name="value"/> is <see langword="null"/> 
+    /// or is <see cref="string.Empty"/>.
+    /// </summary>
+    /// <param name="value">A string.</param>
+    /// <returns>
+    /// <see langword="true"/> if the specified <paramref name="value"/> is <see langword="null"/> or 
+    /// is <see cref="string.Empty"/>; otherwise, <see langword="false"/>.
+    /// </returns>
+    public static bool IsNullOrEmpty(this string value)
+    {
+      return string.IsNullOrEmpty(value);
+    }
+  }
+}
