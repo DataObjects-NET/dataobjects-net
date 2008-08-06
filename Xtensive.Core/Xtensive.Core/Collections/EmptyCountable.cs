36879dc337ac7dfafd742c5c08c9ac7ea92e508a
Alex Yakunin <alex.yakunin@gmail.com>
2008-08-06 07:12:07 +0000

    No comment.

---
+++ Xtensive.Core/Xtensive.Core/Collections/EmptyCountable.cs
@@ -0,0 +1,38 @@
+// Copyright (C) 2007 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Dmitri Maximov
+// Created:    2007.11.22
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Xtensive.Core.Collections
+{
+  /// <summary>
+  /// Empty <see cref="ICountable{TItem}"/> implementation.
+  /// </summary>
+  /// <typeparam name="TItem">The type of the item.</typeparam>
+  public sealed class EmptyCountable<TItem> : ICountable<TItem>
+  {
+    /// <inheritdoc/>
+    [DebuggerHidden]
+    public long Count
+    {
+      get { return 0; }
+    }
+
+    /// <inheritdoc/>
+    public IEnumerator<TItem> GetEnumerator()
+    {
+      yield break;
+    }
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+  }
+}
