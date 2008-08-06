0d6b1a905ee6e260ba1cdfdb5fc3b7eccb2a1956
Alex Kofman <al.kofman@gmail.com>
2008-08-06 10:28:46 +0000

    - StringBuilderExtensions.AppendIndented() was fixed.

--- Xtensive.Core/Xtensive.Core/Helpers/StringBuilderExtensions.cs
+++ Xtensive.Core/Xtensive.Core/Helpers/StringBuilderExtensions.cs
@@ -4,10 +4,12 @@
 // Created by: Alex Kofman
 // Created:    2008.07.30
 
+using System;
+using System.Diagnostics;
 using System.Text;
 
 namespace Xtensive.Core.Helpers
-{
+{  
   /// <summary>
   /// <see cref="StringBuilder"/> related extension methods.
   /// </summary>
@@ -16,16 +18,51 @@ namespace Xtensive.Core.Helpers
     private const char IndentChar = ' ';
 
     /// <summary>
-    /// Appends the line with specified <see cref="string"/> indented by specified count of spaces.
+    /// Appends the specified <see cref="string"/> indented by specified count of spaces.
     /// </summary>
-    /// <param name="builder">The builder to write line to.</param>
-    /// <param name="value">The string value to write.</param>
+    /// <param name="builder">The builder to write indented string to.</param>
     /// <param name="indent">Count of spaces to indent.</param>
-    /// <returns>A reference to the <paramref name="builder"/> after append operation has completed.</returns>
+    /// <param name="value">The string value to write.</param>    
+    /// <returns>
+    /// A reference to the <paramref name="builder"/> after append operation has completed.
+    /// </returns>
     public static StringBuilder AppendIndented(this StringBuilder builder, int indent, string value)
     {
-      builder.Append(new string(IndentChar, indent));
-      builder.Append(value);
+      return builder.AppendIndented(indent, value, true);
+    }
+
+    /// <summary>
+    /// Appends the specified <see cref="string"/> indented by specified count of spaces.
+    /// </summary>
+    /// <param name="builder">The builder to write indented string to.</param>
+    /// <param name="indent">Count of spaces to indent.</param>
+    /// <param name="value">The string value to write.</param>
+    /// <param name="indentFirstLine">if set to <see langword="true"/> first line of string will be indented, otherwise not.</param>
+    /// <returns>
+    /// A reference to the <paramref name="builder"/> after append operation has completed.
+    /// </returns>
+    public static StringBuilder AppendIndented(this StringBuilder builder, int indent, string value, bool indentFirstLine)
+    {
+      string indentString = new string(IndentChar, indent);
+
+      string[] lines = value.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+
+      for (int i = 0; i < lines.Length; i++) {       
+        string line = lines[i];        
+
+        bool isLast = i==lines.Length-1;
+        if (isLast && line.Trim() == string.Empty)
+          break;
+        
+        if (i!=0 || indentFirstLine)
+          builder.Append(indentString);
+
+        builder.Append(line);
+
+        if (!isLast)
+          builder.AppendLine();
+      }
+
       return builder;
     }
   }
