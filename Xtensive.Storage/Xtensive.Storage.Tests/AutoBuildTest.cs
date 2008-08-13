27e9c54e06dd9653e663b5ba9a910e270108624d
Alexis Kochetov <alexis.kochetov@gmail.com>
2008-08-13 07:19:36 +0000

    No comment.

--- Xtensive.Storage/Xtensive.Storage.Tests/AutoBuildTest.cs
+++ Xtensive.Storage/Xtensive.Storage.Tests/AutoBuildTest.cs
@@ -35,8 +35,8 @@ namespace Xtensive.Storage.Tests
     protected virtual DomainConfiguration BuildConfiguration()
     {
       DomainConfiguration config;
-//      config = DomainConfigurationFactory.Create("memory");
-      config = DomainConfigurationFactory.Create("mssql2005");
+      config = DomainConfigurationFactory.Create("memory");
+//      config = DomainConfigurationFactory.Create("mssql2005");
 //      config = DomainConfigurationFactory.Create("memory", InheritanceSchema.SingleTable);
 //      config = DomainConfigurationFactory.Create("memory", InheritanceSchema.SingleTable, TypeIdBehavior.Include);
       return config;
