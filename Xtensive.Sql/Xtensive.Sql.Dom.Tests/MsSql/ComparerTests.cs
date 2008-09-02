84e18fedde659a6fcf3373593f3173a9131aa0ae
Alexey Gamzov <alex@gamzov.com>
2008-09-02 09:54:34 +0000

    No comment.

--- Xtensive.Sql/Xtensive.Sql.Dom.Tests/MsSql/ComparerTests.cs
+++ Xtensive.Sql/Xtensive.Sql.Dom.Tests/MsSql/ComparerTests.cs
@@ -9,6 +9,7 @@ using NUnit.Framework;
 using Xtensive.Sql.Dom.Database;
 using Xtensive.Sql.Dom.Database.Comparer;
 using Xtensive.Sql.Dom.Database.Providers;
+using Assertion=Xtensive.Sql.Dom.Database.Assertion;
 
 namespace Xtensive.Sql.Dom.Tests.MsSql
 {
@@ -27,7 +28,12 @@ namespace Xtensive.Sql.Dom.Tests.MsSql
     {
       Database.Model model1 = GetModel(ConnectionString1);
       Database.Model model2 = GetModel(ConnectionString2);
-      var result = new SqlComparer().Compare(model1.DefaultServer.DefaultCatalog, model2.DefaultServer.DefaultCatalog, null);
+      SqlComparer comparer = new SqlComparer();
+      var result = comparer.Compare(model1.DefaultServer.DefaultCatalog, model2.DefaultServer.DefaultCatalog, null);
+      var singleResult = comparer.Compare(model1.DefaultServer.DefaultCatalog, model1.DefaultServer.DefaultCatalog, null);
+      foreach (IComparisonResult comparisonResult in singleResult.NestedComparisons) {
+        Assert.IsTrue(comparisonResult.ResultType==ComparisonResultType.Unchanged);
+      }
     }
 
     [Test]
@@ -41,26 +47,67 @@ namespace Xtensive.Sql.Dom.Tests.MsSql
       Catalog catalog2 = model2.DefaultServer.DefaultCatalog;
       var result = new SqlComparer().Compare(catalog1, catalog2, null);
       navigator = new ComparisonResultNavigator(result);
-      EnsureOriginalNodeIsInComparison(catalog1);
-      EnsureNewNodeIsInComparison(catalog2);
+      CheckCatalog(catalog1, true);
+      CheckCatalog(catalog2, false);
     }
 
-    private void EnsureOriginalNodeIsInComparison(Node node)
+    private void CheckCatalog(Catalog catalog, bool isOriginal)
     {
-      EnsureNodeIsInComparison(node, true, ComparisonResultType.Added | ComparisonResultType.Modified);
+      CheckNode(catalog, isOriginal);
+      CheckNode(catalog.DefaultSchema, isOriginal);
+      foreach (Schema schema in catalog.Schemas) {
+        CheckNode(schema, isOriginal);
+        foreach (Assertion assertion in schema.Assertions) {
+          CheckNode(assertion, isOriginal);
+        }
+        foreach (CharacterSet characterSet in schema.CharacterSets) {
+          CheckNode(characterSet, isOriginal);
+        }
+        foreach (Collation collation in schema.Collations) {
+          CheckNode(collation, isOriginal);
+        }
+        foreach (Domain domain in schema.Domains) {
+          CheckNode(domain, isOriginal);
+          foreach (DomainConstraint constraint in domain.DomainConstraints) {
+            CheckNode(constraint, isOriginal);
+          }
+        }
+        foreach (Sequence sequence in schema.Sequences) {
+          CheckNode(sequence, isOriginal);
+        }
+        foreach (Table table in schema.Tables) {
+          CheckNode(table, isOriginal);
+          foreach (DataTableColumn column in table.Columns) {
+            CheckNode(column, isOriginal);
+          }
+          foreach (TableConstraint constraint in table.TableConstraints) {
+            CheckNode(constraint, isOriginal);
+          }
+          foreach (Index index in table.Indexes) {
+            CheckNode(index, isOriginal);
+          }
+        }
+        foreach (Translation translation in schema.Translations) {
+          CheckNode(translation, isOriginal);
+        }
+        foreach (View view in schema.Views) {
+          CheckNode(view, isOriginal);
+          foreach (DataTableColumn column in view.Columns) {
+            CheckNode(column, isOriginal);
+          }
+          foreach (Index index in view.Indexes) {
+            CheckNode(index, isOriginal);
+          }
+        }
+      }
     }
 
-    private void EnsureNewNodeIsInComparison(Node node)
-    {
-      EnsureNodeIsInComparison(node, false, ComparisonResultType.Removed | ComparisonResultType.Modified);
-    }
-
-    private void EnsureNodeIsInComparison(Node node, bool isOriginal, ComparisonResultType resultType)
+    private void CheckNode(Node node, bool isOriginal)
     {
       var result = navigator.Find(node);
       Assert.IsNotNull(result);
       Assert.IsTrue(isOriginal ? result.OriginalValue==node : result.NewValue==node);
-      Assert.IsTrue((result.ResultType & resultType) > 0);
+      Assert.IsTrue((result.ResultType & ComparisonResultType.Modified | (isOriginal ? ComparisonResultType.Removed : ComparisonResultType.Added)) > 0);
     }
 
     private Model GetModel(string connectionString)
