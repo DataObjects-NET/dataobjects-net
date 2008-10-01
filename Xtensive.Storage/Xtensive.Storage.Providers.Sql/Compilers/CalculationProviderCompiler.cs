361d52ced6932c1ead7cbb8535fbba67b86db5be
Dima Maximov <dmitri.maximov@gmail.com>
2008-10-01 11:04:07 +0000

    - Minor fixes.

--- Xtensive.Storage/Xtensive.Storage.Providers.Sql/Compilers/CalculationProviderCompiler.cs
+++ Xtensive.Storage/Xtensive.Storage.Providers.Sql/Compilers/CalculationProviderCompiler.cs
@@ -4,16 +4,8 @@
 // Created by: Elena Vakhtina
 // Created:    2008.09.30
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using Xtensive.Core;
-using Xtensive.Core.Tuples;
-using Xtensive.Sql.Dom.Database;
 using Xtensive.Sql.Dom.Dml;
 using Xtensive.Storage.Providers.Sql.Expressions;
-using Xtensive.Storage.Rse;
 using Xtensive.Storage.Rse.Providers;
 using Xtensive.Storage.Rse.Providers.Compilable;
 using SqlFactory = Xtensive.Sql.Dom.Sql;
@@ -26,18 +18,17 @@ namespace Xtensive.Storage.Providers.Sql.Compilers
     protected override ExecutableProvider Compile(CalculationProvider provider, params ExecutableProvider[] compiledSources)
     {
       var source = compiledSources[0] as SqlProvider;
-      if (source == null)
+      if (source==null)
         return null;
 
-      var sqlSelect = (SqlSelect)source.Request.Statement.Clone();
+      var sqlSelect = (SqlSelect) source.Request.Statement.Clone();
       var request = new SqlFetchRequest(sqlSelect, provider.Header, source.Request.ParameterBindings);
       var visitor = new Visitor(request);
 
       foreach (var column in provider.CalculatedColumns) {
         visitor.AppendCalculationToRequest(column.Expression, column.Name);
       }
-      var resultRequest = new SqlFetchRequest(request.Statement, provider.Header, request.ParameterBindings);
-      return new SqlProvider(provider, resultRequest, Handlers, source);
+      return new SqlProvider(provider, request, Handlers, source);
     }
 
 
