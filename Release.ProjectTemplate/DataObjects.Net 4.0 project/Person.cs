22e7548efab1f2a15d17f28c5cc5af07c91a3e2d
Dima Maximov <dmitri.maximov@gmail.com>
2008-12-18 10:51:02 +0000

    - DataObjects.Net 4.0 project template has been added.

---
+++ Release.ProjectTemplate/DataObjects.Net 4.0 project/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xtensive.Storage;
+using Xtensive.Storage.Attributes;
+
+namespace $safeprojectname$
+{
+  [HierarchyRoot(typeof(KeyGenerator), "Id")]
+  public class Person : Entity
+  {
+    [Field]
+    public int Id { get; private set; }
+
+    [Field]
+    public string FirstName { get; set; }
+
+    [Field]
+    public string LastName { get; set; }
+
+    [Field]
+    public int Age { get; set; }
+  }
+}
