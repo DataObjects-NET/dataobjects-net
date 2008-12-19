af89c33577a466ad9d9ec874ff089b2a0baf6e1e
Elena Vakhtina <info@x-tensive.com>
2008-12-19 10:13:24 +0000

    No comment.

--- Xtensive.Core/Xtensive.Core/Reflection/WellKnown.cs
+++ Xtensive.Core/Xtensive.Core/Reflection/WellKnown.cs
@@ -57,69 +57,29 @@ namespace Xtensive.Core.Reflection
     public static class Queryable
     {
       /// <summary>
-      /// Returns "Where".
+      /// Returns "Aggregate".
       /// </summary>
-      public const string Where = "Where";
+      public const string Aggregate = "Aggregate";
 
       /// <summary>
-      /// Returns "Select".
+      /// Returns "All".
       /// </summary>
-      public const string Select = "Select";
+      public const string All = "All";
 
       /// <summary>
-      /// Returns "SelectMany".
+      /// Returns "Any".
       /// </summary>
-      public const string SelectMany = "SelectMany";
+      public const string Any = "Any";
 
       /// <summary>
-      /// Returns "Join".
+      /// Returns "AsEnumerable".
       /// </summary>
-      public const string Join = "Join";
+      public const string AsEnumerable = "AsEnumerable";
 
       /// <summary>
-      /// Returns "OrderBy".
+      /// Returns "AsQueryable".
       /// </summary>
-      public const string OrderBy = "OrderBy";
-
-      /// <summary>
-      /// Returns "OrderByDescending".
-      /// </summary>
-      public const string OrderByDescending = "OrderByDescending";
-
-      /// <summary>
-      /// Returns "ThenBy".
-      /// </summary>
-      public const string ThenBy = "ThenBy";
-
-      /// <summary>
-      /// Returns "ThenByDescending".
-      /// </summary>
-      public const string ThenByDescending = "ThenByDescending";
-
-      /// <summary>
-      /// Returns "GroupBy".
-      /// </summary>
-      public const string GroupBy = "GroupBy";
-
-      /// <summary>
-      /// Returns "Count".
-      /// </summary>
-      public const string Count = "Count";
-
-      /// <summary>
-      /// Returns "Min".
-      /// </summary>
-      public const string Min = "Min";
-
-      /// <summary>
-      /// Returns "Max".
-      /// </summary>
-      public const string Max = "Max";
-
-      /// <summary>
-      /// Returns "Sum".
-      /// </summary>
-      public const string Sum = "Sum";
+      public const string AsQueryable = "AsQueryable";
 
       /// <summary>
       /// Returns "Average".
@@ -127,19 +87,49 @@ namespace Xtensive.Core.Reflection
       public const string Average = "Average";
 
       /// <summary>
+      /// Returns "Cast".
+      /// </summary>
+      public const string Cast = "Cast";
+
+      /// <summary>
+      /// Returns "Concat".
+      /// </summary>
+      public const string Concat = "Concat";
+
+      /// <summary>
+      /// Returns "All".
+      /// </summary>
+      public const string Contains = "Contains";
+
+      /// <summary>
+      /// Returns "Count".
+      /// </summary>
+      public const string Count = "Count";
+
+      /// <summary>
+      /// Returns "DefaultIfEmpty".
+      /// </summary>
+      public const string DefaultIfEmpty = "DefaultIfEmpty";
+
+      /// <summary>
       /// Returns "Distinct".
       /// </summary>
       public const string Distinct = "Distinct";
 
       /// <summary>
-      /// Returns "Skip".
+      /// Returns "ElementAt".
       /// </summary>
-      public const string Skip = "Skip";
+      public const string ElementAt = "ElementAt";
 
       /// <summary>
-      /// Returns "Take".
+      /// Returns "ElementAtOrDefault".
       /// </summary>
-      public const string Take = "Take";
+      public const string ElementAtOrDefault = "ElementAtOrDefault";
+
+      /// <summary>
+      /// Returns "Except".
+      /// </summary>
+      public const string Except = "Except";
 
       /// <summary>
       /// Returns "First".
@@ -152,6 +142,86 @@ namespace Xtensive.Core.Reflection
       public const string FirstOrDefault = "FirstOrDefault";
 
       /// <summary>
+      /// Returns "GroupBy".
+      /// </summary>
+      public const string GroupBy = "GroupBy";
+
+      /// <summary>
+      /// Returns "GroupJoin".
+      /// </summary>
+      public const string GroupJoin = "GroupJoin";
+
+      /// <summary>
+      /// Returns "Intersect".
+      /// </summary>
+      public const string Intersect = "Intersect";
+
+      /// <summary>
+      /// Returns "Join".
+      /// </summary>
+      public const string Join = "Join";
+
+      /// <summary>
+      /// Returns "Last".
+      /// </summary>
+      public const string Last = "Last";
+
+      /// <summary>
+      /// Returns "LastOrDefault".
+      /// </summary>
+      public const string LastOrDefault = "LastOrDefault";
+
+      /// <summary>
+      /// Returns "LongCount".
+      /// </summary>
+      public const string LongCount = "LongCount";
+
+      /// <summary>
+      /// Returns "Max".
+      /// </summary>
+      public const string Max = "Max";
+
+      /// <summary>
+      /// Returns "Min".
+      /// </summary>
+      public const string Min = "Min";
+
+      /// <summary>
+      /// Returns "OfType".
+      /// </summary>
+      public const string OfType = "OfType";
+
+      /// <summary>
+      /// Returns "OrderBy".
+      /// </summary>
+      public const string OrderBy = "OrderBy";
+
+      /// <summary>
+      /// Returns "OrderByDescending".
+      /// </summary>
+      public const string OrderByDescending = "OrderByDescending";
+
+      /// <summary>
+      /// Returns "Reverse".
+      /// </summary>
+      public const string Reverse = "Reverse";
+
+      /// <summary>
+      /// Returns "Select".
+      /// </summary>
+      public const string Select = "Select";
+
+      /// <summary>
+      /// Returns "SelectMany".
+      /// </summary>
+      public const string SelectMany = "SelectMany";
+
+      /// <summary>
+      /// Returns "SequenceEqual".
+      /// </summary>
+      public const string SequenceEqual = "SequenceEqual";
+
+      /// <summary>
       /// Returns "Single".
       /// </summary>
       public const string Single = "Single";
@@ -162,19 +232,59 @@ namespace Xtensive.Core.Reflection
       public const string SingleOrDefault = "SingleOrDefault";
 
       /// <summary>
-      /// Returns "Any".
+      /// Returns "Skip".
       /// </summary>
-      public const string Any = "Any";
+      public const string Skip = "Skip";
 
       /// <summary>
-      /// Returns "All".
+      /// Returns "SkipWhile".
       /// </summary>
-      public const string All = "All";
-      
+      public const string SkipWhile = "SkipWhile";
+
       /// <summary>
-      /// Returns "All".
+      /// Returns "Sum".
       /// </summary>
-      public const string Contains = "Contains";
+      public const string Sum = "Sum";
+
+      /// <summary>
+      /// Returns "Take".
+      /// </summary>
+      public const string Take = "Take";
+
+      /// <summary>
+      /// Returns "TakeWhile".
+      /// </summary>
+      public const string TakeWhile = "TakeWhile";
+
+      /// <summary>
+      /// Returns "ThenBy".
+      /// </summary>
+      public const string ThenBy = "ThenBy";
+
+      /// <summary>
+      /// Returns "ThenByDescending".
+      /// </summary>
+      public const string ThenByDescending = "ThenByDescending";
+
+      /// <summary>
+      /// Returns "ToArray".
+      /// </summary>
+      public const string ToArray = "ToArray";
+
+      /// <summary>
+      /// Returns "ToList".
+      /// </summary>
+      public const string ToList = "ToList";
+
+      /// <summary>
+      /// Returns "Union".
+      /// </summary>
+      public const string Union = "Union";
+
+      /// <summary>
+      /// Returns "Where".
+      /// </summary>
+      public const string Where = "Where";
     }
   }
 }
