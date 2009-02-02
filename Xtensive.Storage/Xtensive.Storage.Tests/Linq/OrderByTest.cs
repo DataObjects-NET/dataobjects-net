176ce485f993f8c016fa35fb97a85c0ee829e2c4
Dima Maximov <dmitri.maximov@gmail.com>
2009-02-02 15:38:06 +0000

    - OrderByTest has been added.

--- Xtensive.Storage/Xtensive.Storage.Tests/Linq/OrderByTest.cs
+++ Xtensive.Storage/Xtensive.Storage.Tests/Linq/OrderByTest.cs
@@ -4,6 +4,8 @@
 // Created by: Dmitri Maximov
 // Created:    2009.01.29
 
+using System;
+using System.Linq;
 using NUnit.Framework;
 using Xtensive.Storage.Configuration;
 using Xtensive.Storage.Tests.ObjectModel.NorthwindDO;
@@ -19,10 +21,25 @@ namespace Xtensive.Storage.Tests.Linq
       return config;
     }
 
+    protected override Domain BuildDomain(DomainConfiguration configuration)
+    {
+      Domain domain = base.BuildDomain(configuration);
+      DataBaseFiller.Fill(domain);
+      return domain;
+    }
+
     [Test]
     public void MainTest()
     {
-      DataBaseFiller.Fill(Domain);
+      using (Domain.OpenSession()) {
+        using (Transaction.Open()) {
+          var contacts = Enumerable.OrderBy(Session.Current.All<Customer>(), s => s.ContactName);
+          foreach (var item in contacts)
+            Console.WriteLine(item.ContactName);
+          foreach (var item in contacts.OrderByDescending(s => s.ContactName))
+            Console.WriteLine(item.ContactName);
+        }
+      }
     }
   }
 }
