28c0b75bf96acec971af0247c52f6a286438762b
Alexis Kochetov <alexis.kochetov@gmail.com>
2009-02-04 08:24:14 +0000

    No comment.

--- Xtensive.Storage/Xtensive.Storage.Tests/Linq/WhereTest.cs
+++ Xtensive.Storage/Xtensive.Storage.Tests/Linq/WhereTest.cs
@@ -14,65 +14,20 @@ using System.Linq;
 namespace Xtensive.Storage.Tests.Linq
 {
   [TestFixture]
-  public class WhereTest : AutoBuildTest
+  public class WhereTest : NorthwindDOModelTest
   {
     private Key supplier20Key;
-    private Key categoryKey;
+    private Key category1Key;
 
-    protected override DomainConfiguration BuildConfiguration()
-    {
-      DomainConfiguration config = base.BuildConfiguration();
-      config.Types.Register(Assembly.GetExecutingAssembly(), "Xtensive.Storage.Tests.ObjectModel.NorthwindDO");
-      return config;
-    }
-
-    protected override Domain BuildDomain(DomainConfiguration configuration)
-    {
-      Xtensive.Storage.Domain result = base.BuildDomain(configuration);
-      return result;
-    }
-
-    [TestFixtureSetUp]
     public override void TestFixtureSetUp()
     {
       base.TestFixtureSetUp();
       using (Domain.OpenSession()) {
-        using (var t = Transaction.Open()) {
-          var category = new Category();
-          category.CategoryName = "Category";
-          category.Description = "Description of category";
-          categoryKey = category.Key;
-          for (int i = 0; i < 100; i++) {
-            var supplier = new Supplier();
-            supplier.Address = new Address();
-            supplier.Address.City = "City" + i;
-            supplier.Address.Country = "Country" + i;
-            supplier.Address.PostalCode = "Code" + i;
-            supplier.Address.StreetAddress = "Address" + i;
-            supplier.Address.Region = "Region" + i;
-            supplier.CompanyName = "Company" + i;
-            supplier.ContactName = "Contact" + i;
-            supplier.ContactTitle = "Title" + i;
-            supplier.Phone = "Phone" + i;
-            supplier.Fax = "Fax" + i;
-            supplier.HomePage = "www.homepage.com" + i;
-            if (supplier.Id == 20)
-              supplier20Key = supplier.Key;
-            for (int j = 0; j < 10; j++) {
-              Product product = new Product();
-              product.ProductName = string.Format("Product_{0}_{1}", i, j);
-              product.UnitPrice = j;
-              product.UnitsInStock = 10;
-              product.UnitsOnOrder = 1;
-              product.Category = category;
-              product.Supplier = supplier;
-            }
-
-          }
-          t.Complete();
-        }
+        supplier20Key = Session.Current.All<Supplier>().Single(s => s.Id == 20).Key;
+        category1Key = Session.Current.All<Category>().Single(c => c.Id == 1).Key;
       }
     }
+
 
     [Test]
     public void ColumnTest()
@@ -80,9 +35,9 @@ namespace Xtensive.Storage.Tests.Linq
       using (Domain.OpenSession()) {
         using (var t = Transaction.Open()) {
           var suppliers = Session.Current.All<Supplier>();
-          var supplier = suppliers.Where(s => s.CompanyName=="Company20").First();
+          var supplier = suppliers.Where(s => s.CompanyName == "Tokyo Traders").First();
           Assert.IsNotNull(supplier);
-          Assert.AreEqual("Company20", supplier.CompanyName);
+          Assert.AreEqual("Tokyo Traders", supplier.CompanyName);
           t.Complete();
         }
       }
@@ -94,8 +49,8 @@ namespace Xtensive.Storage.Tests.Linq
       using (Domain.OpenSession()) {
         using (var t = Transaction.Open()) {
           var products = Session.Current.All<Product>();
-          var list = products.Where(p => p.UnitPrice * p.UnitsInStock==90).ToList();
-          Assert.AreEqual(100, list.Count);
+          var list = products.Where(p => p.UnitPrice * p.UnitsInStock >= 100).ToList();
+          Assert.AreEqual(67, list.Count);
           t.Complete();
         }
       }
@@ -107,9 +62,9 @@ namespace Xtensive.Storage.Tests.Linq
       using (Domain.OpenSession()) {
         using (var t = Transaction.Open()) {
           var suppliers = Session.Current.All<Supplier>();
-          var supplier = suppliers.Where(s => s.Address.Region == "Region30").First();
+          var supplier = suppliers.Where(s => s.Address.Region == "Victoria").First();
           Assert.IsNotNull(supplier);
-          Assert.AreEqual("Region30", supplier.Address.Region);
+          Assert.AreEqual("Victoria", supplier.Address.Region);
           t.Complete();
         }
       }
@@ -123,7 +78,7 @@ namespace Xtensive.Storage.Tests.Linq
           var suppliers = Session.Current.All<Supplier>();
           var supplier = suppliers.Where(s => s.Id == 20 ).First();
           Assert.IsNotNull(supplier);
-          Assert.AreEqual("Company19", supplier.CompanyName);
+          Assert.AreEqual("Leka Trading", supplier.CompanyName);
           t.Complete();
         }
       }
@@ -138,7 +93,7 @@ namespace Xtensive.Storage.Tests.Linq
           var key = Key.Create<Supplier>(Tuple.Create(20));
           var supplier = suppliers.Where(s => s.Key == key).First();
           Assert.IsNotNull(supplier);
-          Assert.AreEqual("Company19", supplier.CompanyName);
+          Assert.AreEqual("Leka Trading", supplier.CompanyName);
           t.Complete();
         }
       }
@@ -153,7 +108,7 @@ namespace Xtensive.Storage.Tests.Linq
           var suppliers = Session.Current.All<Supplier>();
           var supplier = suppliers.Where(s => s == supplier20).First();
           Assert.IsNotNull(supplier);
-          Assert.AreEqual("Company19", supplier.CompanyName);
+          Assert.AreEqual("Leka Trading", supplier.CompanyName);
           t.Complete();
         }
       }
@@ -168,7 +123,7 @@ namespace Xtensive.Storage.Tests.Linq
           var products = Session.Current.All<Product>();
           var product = products.Where(p => p.Supplier.Key == supplier20.Key).First();
           Assert.IsNotNull(product);
-          Assert.AreEqual("Product_19_0", product.ProductName);
+          Assert.AreEqual("Singaporean Hokkien Fried Mee", product.ProductName);
           t.Complete();
         }
       }
@@ -183,7 +138,7 @@ namespace Xtensive.Storage.Tests.Linq
           var products = Session.Current.All<Product>();
           var product = products.Where(p => p.Supplier.Id == supplier20.Id).First();
           Assert.IsNotNull(product);
-          Assert.AreEqual("Product_19_0", product.ProductName);
+          Assert.AreEqual("Singaporean Hokkien Fried Mee", product.ProductName);
           t.Complete();
         }
       }
@@ -198,7 +153,7 @@ namespace Xtensive.Storage.Tests.Linq
           var products = Session.Current.All<Product>();
           var product = products.Where(p => p.Supplier == supplier20).First();
           Assert.IsNotNull(product);
-          Assert.AreEqual("Product_19_0", product.ProductName);
+          Assert.AreEqual("Singaporean Hokkien Fried Mee", product.ProductName);
           t.Complete();
         }
       }
@@ -210,12 +165,12 @@ namespace Xtensive.Storage.Tests.Linq
       using (Domain.OpenSession()) {
         using (var t = Transaction.Open()) {
           var products = Session.Current.All<Product>();
-          var product = products.Where(p => p.Supplier.CompanyName == "Company20").First();
+          var product = products.Where(p => p.Supplier.CompanyName == "Leka Trading").First();
           Assert.IsNotNull(product);
-          Assert.AreEqual("Company20", product.Supplier.CompanyName);
-          product = products.Where(p => p.Supplier.CompanyName == "Company20" && p.Category.Key == categoryKey && p.Supplier.ContactTitle == "Title20" ).First();
+          Assert.AreEqual("Leka Trading", product.Supplier.CompanyName);
+          product = products.Where(p => p.Supplier.CompanyName == "Leka Trading" && p.Category.Key == category1Key && p.Supplier.ContactTitle == "Owner").First();
           Assert.IsNotNull(product);
-          Assert.AreEqual("Company20", product.Supplier.CompanyName);
+          Assert.AreEqual("Leka Trading", product.Supplier.CompanyName);
           t.Complete();
         }
       }
