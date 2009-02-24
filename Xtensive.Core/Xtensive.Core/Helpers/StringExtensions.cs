ee7fcfb84b08d3a3fcbb457c5cd4b18803befb40
Alexis Kochetov <alexis.kochetov@gmail.com>
2009-02-24 11:13:25 +0000

    No comment.

--- Xtensive.Core/Xtensive.Core/Helpers/StringExtensions.cs
+++ Xtensive.Core/Xtensive.Core/Helpers/StringExtensions.cs
@@ -4,6 +4,8 @@
 // Created by: Dmitri Maximov
 // Created:    2008.07.18
 
+using System;
+
 namespace Xtensive.Core.Helpers
 {
   /// <summary>
@@ -52,6 +54,82 @@ namespace Xtensive.Core.Helpers
       if (!value.StartsWith(prefix))
         return value;
       return value.Substring(prefix.Length);        
+    }
+
+    /// <summary>
+    /// Determines whether <paramref name="x"/> <see cref="string"/> is less than <paramref name="y"/> <see cref="string"/>.
+    /// </summary>
+    /// <param name="x">The first argument.</param>
+    /// <param name="y">The second argument.</param>
+    /// <returns>
+    /// <see langword="true" /> if <paramref name="x"/> is less than <paramref name="y"/>; otherwise <see langword="false" />.
+    /// </returns>
+    public static bool LessThan(this string x, string y)
+    {
+      if (x == y)
+        return false;
+      if (x == null)
+        return true;
+      if (y == null)
+        return false;
+      return x.CompareTo(y) < 0;
+    }
+
+    /// <summary>
+    /// Determines whether <paramref name="x"/> <see cref="string"/> is less than or equals to <paramref name="y"/> <see cref="string"/>.
+    /// </summary>
+    /// <param name="x">The first argument.</param>
+    /// <param name="y">The second argument.</param>
+    /// <returns>
+    /// <see langword="true" /> if <paramref name="x"/> is less than or equals to <paramref name="y"/>; otherwise <see langword="false" />.
+    /// </returns>
+    public static bool LessThanOrEqual(this string x, string y)
+    {
+      if (x == y)
+        return true;
+      if (x == null)
+        return true;
+      if (y == null)
+        return false;
+      return x.CompareTo(y) <= 0;
+    }
+
+    /// <summary>
+    /// Determines whether <paramref name="x"/> <see cref="string"/> is greater than <paramref name="y"/> <see cref="string"/>.
+    /// </summary>
+    /// <param name="x">The first argument.</param>
+    /// <param name="y">The second argument.</param>
+    /// <returns>
+    /// <see langword="true" /> if <paramref name="x"/> is greater than <paramref name="y"/>; otherwise <see langword="false" />.
+    /// </returns>
+    public static bool GreaterThan(this string x, string y)
+    {
+      if (x == y)
+        return false;
+      if (x == null)
+        return false;
+      if (y == null)
+        return true;
+      return x.CompareTo(y) > 0;
+    }
+
+    /// <summary>
+    /// Determines whether <paramref name="x"/> <see cref="string"/> is less than <paramref name="y"/> <see cref="string"/>.
+    /// </summary>
+    /// <param name="x">The first argument.</param>
+    /// <param name="y">The second argument.</param>
+    /// <returns>
+    /// <see langword="true" /> if <paramref name="x"/> is less than <paramref name="y"/>; otherwise <see langword="false" />.
+    /// </returns>
+    public static bool GreaterThanOrEqual(this string x, string y)
+    {
+      if (x == y)
+        return true;
+      if (x == null)
+        return false;
+      if (y == null)
+        return true;
+      return x.CompareTo(y) >= 0;
     }
   }
 }
