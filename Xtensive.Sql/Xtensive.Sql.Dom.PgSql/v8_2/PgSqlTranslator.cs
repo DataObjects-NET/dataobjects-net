f0617eea8c5c45a9d70b9e4644a6955d281a8a8d
Dima Maximov <dmitri.maximov@gmail.com>
2009-02-24 08:25:45 +0000

    - CurrentDate, CurrentTime, CurrentTimeStamp translation has been updated.

--- Xtensive.Sql/Xtensive.Sql.Dom.PgSql/v8_2/PgSqlTranslator.cs
+++ Xtensive.Sql/Xtensive.Sql.Dom.PgSql/v8_2/PgSqlTranslator.cs
@@ -1,6 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Text;
 using Xtensive.Sql.Dom.Database;
+using Xtensive.Sql.Dom.Dml;
 
 namespace Xtensive.Sql.Dom.PgSql.v8_2
 {
@@ -22,5 +24,22 @@ namespace Xtensive.Sql.Dom.PgSql.v8_2
       if (index.FillFactor!=null)
         builder.AppendFormat("WITH(FILLFACTOR={0})", index.FillFactor);
     }
+
+    public override string Translate(SqlFunctionType type)
+    {
+      switch (type) {
+
+        //date
+
+      case SqlFunctionType.CurrentDate:
+      case SqlFunctionType.CurrentTime:
+      case SqlFunctionType.CurrentTimeStamp:
+        return "clock_timestamp";
+
+      default:
+        return base.Translate(type);
+      }
+    }
+
   }
 }
