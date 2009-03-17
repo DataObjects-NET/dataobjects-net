6391d797eb1432903417b68e8974fe08440f008e
Alex Yakunin <alex.yakunin@gmail.com>
2009-03-17 08:11:03 +0000

    No comment.

---
+++ Xtensive.Core/Xtensive.Core/Security/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,35 @@
+// Copyright (C) 2009 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alex Yakunin
+// Created:    2009.03.17
+
+namespace Xtensive.Core.Security
+{
+  /// <summary>
+  /// Authentication service.
+  /// </summary>
+  public interface IAuthenticationService
+  {
+    /// <summary>
+    /// Authenticates the specified user.
+    /// </summary>
+    /// <param name="userName">Name of the user.</param>
+    /// <param name="password">The password.</param>
+    /// <returns>
+    /// Signed (and normally, encrypted) security token;
+    /// <see langword="null"/>, if authentication has failed.
+    /// </returns>
+    string Authenticate(string userName, string password);
+
+    /// <summary>
+    /// Checks the security token.
+    /// </summary>
+    /// <param name="signedSecurityToken">The signed security token to check.</param>
+    /// <returns>
+    /// Unsigned (original) security token;
+    /// <see langword="null"/>, if authentication has failed.
+    /// </returns>
+    string CheckSecurityToken(string signedSecurityToken);
+  }
+}
---
+++ Xtensive.Core/Xtensive.Core/Security/Interfaces/ISecurityTokenProvider.cs
@@ -0,0 +1,22 @@
+// Copyright (C) 2009 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alex Yakunin
+// Created:    2009.03.17
+
+namespace Xtensive.Core.Security
+{
+  /// <summary>
+  /// Security token provider.
+  /// </summary>
+  public interface ISecurityTokenProvider
+  {
+    /// <summary>
+    /// Gets the security token.
+    /// </summary>
+    /// <param name="userName">Name of the user.</param>
+    /// <param name="options">The additional options.</param>
+    /// <returns>Security token for the specified user and options.</returns>
+    string GetSecurityToken(string userName, string options);
+  }
+}
