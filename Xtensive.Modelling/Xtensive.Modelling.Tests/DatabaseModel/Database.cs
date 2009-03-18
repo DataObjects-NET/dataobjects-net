491feb1737a2fe38f7ccc3807db78bcbb3faf80c
Alex Yakunin <alex.yakunin@gmail.com>
2009-03-18 12:53:05 +0000

    No comment.

---
+++ Xtensive.Modelling/Xtensive.Modelling.Tests/DatabaseModel/Database.cs
@@ -0,0 +1,20 @@
+// Copyright (C) 2009 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alex Yakunin
+// Created:    2009.03.18
+
+using System;
+using System.Diagnostics;
+
+namespace Xtensive.Modelling.Tests.DatabaseModel
+{
+  [Serializable]
+  public class Database : Model
+  {
+    public Database(string name)
+      : base(name)
+    {
+    }
+  }
+}
