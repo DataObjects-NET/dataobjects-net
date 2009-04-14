d6de9d034075643373a371caae6266f5830807b3
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2009-04-14 13:17:28 +0000

    No comment.

--- Xtensive.Storage/Xtensive.Storage/Linq/Translator.Queryable.cs
+++ Xtensive.Storage/Xtensive.Storage/Linq/Translator.Queryable.cs
@@ -860,7 +860,7 @@ namespace Xtensive.Storage.Linq
     {
       this.context = context;
       recordIsUsed = new Parameter<bool>("recordIsUsed", oldValue => {
-        if (recordIsUsed.HasValue && ignoreRecordUsage.HasValue && !ignoreRecordUsage.Value)
+        if (!ignoreRecordUsage.Value)
           recordIsUsed.Value |= oldValue;
       });
     }
