6af624fcc15c3012bcf9b3b7664fb6504163c144
Alex Yakunin <alex.yakunin@gmail.com>
2009-04-17 05:52:11 +0000

    No comment.

--- Xtensive.Core/Xtensive.Core/Reflection/TypeHelper.cs
+++ Xtensive.Core/Xtensive.Core/Reflection/TypeHelper.cs
@@ -758,31 +758,24 @@ namespace Xtensive.Core.Reflection
       if (seqType == null || seqType == typeof(string))
         return null;
       if (seqType.IsArray)
-        return typeof(IEnumerable<>).MakeGenericType(seqType.GetElementType());
-      if (seqType.IsGenericType)
-      {
-        foreach (Type arg in seqType.GetGenericArguments())
-        {
-          Type ienum = typeof(IEnumerable<>).MakeGenericType(arg);
+        return typeof (IEnumerable<>).MakeGenericType(seqType.GetElementType());
+      if (seqType.IsGenericType) {
+        foreach (Type arg in seqType.GetGenericArguments()) {
+          Type ienum = typeof (IEnumerable<>).MakeGenericType(arg);
           if (ienum.IsAssignableFrom(seqType))
-          {
             return ienum;
-          }
         }
       }
       Type[] ifaces = seqType.GetInterfaces();
-      if (ifaces != null && ifaces.Length > 0)
-      {
-        foreach (Type iface in ifaces)
-        {
+      if (ifaces != null && ifaces.Length > 0) {
+        foreach (Type iface in ifaces) {
           Type ienum = FindIEnumerable(iface);
-          if (ienum != null) return ienum;
+          if (ienum != null) 
+            return ienum;
         }
       }
       if (seqType.BaseType != null && seqType.BaseType != typeof(object))
-      {
         return FindIEnumerable(seqType.BaseType);
-      }
       return null;
     }
 
