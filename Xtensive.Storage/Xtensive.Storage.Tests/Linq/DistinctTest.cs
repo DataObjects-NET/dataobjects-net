62316fdd58b31a5a3c50c8039f22be660486894f
Alexey Gamzov <alex@gamzov.com>
2009-05-07 13:09:38 +0000

    No comment.

--- Xtensive.Storage/Xtensive.Storage.Tests/Linq/DistinctTest.cs
+++ Xtensive.Storage/Xtensive.Storage.Tests/Linq/DistinctTest.cs
@@ -18,199 +18,316 @@ namespace Xtensive.Storage.Tests.Linq
     [Test]
     public void OrderBy2Test()
     {
-      IQueryable<string> result = Query<Customer>.All
-        //.OrderBy(c => c.CompanyName)
-        .Select(c=>c.Address.City)
+      var result = Query<Customer>.All
+        .OrderBy(c => c.CompanyName)
+        .Select(c => c.Address.City)
         .Distinct()
         .OrderBy(c => c);
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .OrderBy(c => c.CompanyName)
+        .Select(c => c.Address.City)
+        .Distinct()
+        .OrderBy(c => c);
+      Assert.IsTrue(expected.SequenceEqual(result));
       QueryDumper.Dump(result);
     }
 
     [Test]
     public void DefaultTest()
     {
-        var result = Query<Customer>.All.Distinct();
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      var result = Query<Customer>.All.Distinct();
+      var expected = Query<Customer>.All.AsEnumerable().Distinct();
+      Assert.AreEqual(0, expected.Except(result).Count());
+      Assert.Greater(result.ToList().Count, 0);
     }
 
     [Test]
     public void ScalarTest()
     {
-        var result = Query<Customer>.All.Select(c => c.Address.City).Distinct();
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      var result = Query<Customer>.All
+        .Select(c => c.Address.City)
+        .Distinct();
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .Select(c => c.Address.City)
+        .Distinct();
+      Assert.AreEqual(0, expected.Except(result).Count());
+      Assert.Greater(result.ToList().Count, 0);
     }
 
     [Test]
     public void OrderByTest()
     {
-        var result = Query<Customer>.All.OrderBy(c => c.Id).Select(c => c.Address.City).Distinct();
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      var result = Query<Customer>.All
+        .OrderBy(c => c.Id)
+        .Select(c => c.Address.City)
+        .Distinct();
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .OrderBy(c => c.Id)
+        .Select(c => c.Address.City)
+        .Distinct();
+      Assert.AreEqual(0, expected.Except(result).Count());
+      Assert.Greater(result.ToList().Count, 0);
     }
 
 
     [Test]
     public void DistinctOrderByTest()
     {
-        var result = Query<Customer>.All.Select(c => c.Address.City).Distinct().OrderBy(c => c);
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      var result = Query<Customer>.All
+        .Select(c => c.Address.City)
+        .Distinct()
+        .OrderBy(c => c);
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .Select(c => c.Address.City)
+        .Distinct()
+        .OrderBy(c => c);
+      Assert.IsTrue(expected.SequenceEqual(result));
+      QueryDumper.Dump(result);
     }
 
     [Test]
     public void CountTest()
     {
-        var result = Query<Customer>.All.Distinct().Count();
-        Assert.Greater(result, 0);
+      var result = Query<Customer>.All
+        .Distinct()
+        .Count();
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .Distinct()
+        .Count();
+      Assert.AreEqual(expected, result);
     }
 
     [Test]
     public void SelectDistinctCountTest()
     {
-        var result = Query<Customer>.All.Select(c => c.Address.City).Distinct().Count();
-        Assert.Greater(result, 0);
+      var result = Query<Customer>.All
+        .Select(c => c.Address.City)
+        .Distinct()
+        .Count();
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .Select(c => c.Address.City)
+        .Distinct()
+        .Count();
+      Assert.AreEqual(expected, result);
+      Assert.Greater(result, 0);
     }
 
     [Test]
     public void NestedSelectDistinctCountTest()
     {
-        var result = Query<Customer>.All.Select(c => c.Address).Select(a => a.City).Distinct().Count();
-        Assert.Greater(result, 0);
+      var result = Query<Customer>.All
+        .Select(c => c.Address)
+        .Select(a => a.City)
+        .Distinct()
+        .Count();
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .Select(c => c.Address)
+        .Select(a => a.City)
+        .Distinct()
+        .Count();
+      Assert.AreEqual(expected, result);
+      Assert.Greater(result, 0);
     }
 
     [Test]
     public void CountPredicateTest()
     {
-        var result = Query<Customer>.All.Distinct().Count(c => c.Id == "ALFKI");
-        Assert.Greater(result, 0);
+      var result = Query<Customer>.All
+        .Distinct()
+        .Count(c => c.Id=="ALFKI");
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .Distinct()
+        .Count(c => c.Id=="ALFKI");
+      Assert.AreEqual(expected, result);
+      Assert.Greater(result, 0);
     }
 
     [Test]
     public void SumWithArgTest()
     {
-        var result = Query<Order>.All.Distinct().Sum(o => o.Id);
-        Assert.Greater(result, 0);
+      var result = Query<Order>.All
+        .Distinct()
+        .Sum(o => o.Id);
+      var expected = Query<Order>.All
+        .AsEnumerable()
+        .Distinct()
+        .Sum(o => o.Id);
+      Assert.AreEqual(expected, result);
+      Assert.Greater(result, 0);
     }
 
     [Test]
     public void SumTest()
     {
-        var result = Query<Order>.All.Select(o => o.Id).Distinct().Sum();
-        Assert.Greater(result, 0);
+      var result = Query<Order>.All
+        .Select(o => o.Id)
+        .Distinct()
+        .Sum();
+      var expected = Query<Order>.All
+        .AsEnumerable()
+        .Select(o => o.Id)
+        .Distinct()
+        .Sum();
+      Assert.AreEqual(expected, result);
+      Assert.Greater(result, 0);
     }
 
     [Test]
     public void TakeTest()
     {
-        // NOTE: Distinct must be forced to apply after top has been computed
-        var result = Query<Order>.All.Take(5).Distinct();
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      // NOTE: Distinct must be forced to apply after top has been computed
+      var result = Query<Order>.All
+        .Take(5)
+        .Distinct();
+      var expected = Query<Order>.All
+        .AsEnumerable()
+        .Take(5)
+        .Distinct();
+      Assert.IsTrue(expected.SequenceEqual(result));
+      Assert.Greater(result.ToList().Count, 0);
     }
 
     [Test]
     public void DistinctTakeTest()
     {
-        // NOTE: Top must be forced to apply after distinct has been computed
-        var result = Query<Order>.All.Distinct().Take(5);
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      // NOTE: Top must be forced to apply after distinct has been computed
+      var result = Query<Order>.All
+        .Distinct()
+        .Take(5);
+      var expected = Query<Order>.All
+        .AsEnumerable()
+        .Distinct()
+        .Take(5);
+      Assert.IsTrue(expected.SequenceEqual(result));
+      Assert.Greater(result.ToList().Count, 0);
     }
 
     [Test]
     public void TakeCountTest()
     {
-        var result = Query<Order>.All.Distinct().Take(5).Count();
-        Assert.Greater(result, 0);
+      var result = Query<Order>.All
+        .Distinct()
+        .Take(5)
+        .Count();
+      var expected = Query<Order>.All
+        .AsEnumerable()
+        .Distinct()
+        .Take(5)
+        .Count();
+      Assert.AreEqual(expected, result);
+      Assert.Greater(result, 0);
     }
 
     [Test]
     public void TakeDistinctCountTest()
     {
-        var result = Query<Order>.All.Take(5).Distinct().Count();
-        Assert.Greater(result, 0);
+      var result = Query<Order>.All
+        .Take(5)
+        .Distinct()
+        .Count();
+      var expected = Query<Order>.All
+        .AsEnumerable()
+        .Take(5)
+        .Distinct()
+        .Count();
+      Assert.AreEqual(expected, result);
+      Assert.Greater(result, 0);
     }
 
     [Test]
     public void SkipTest()
     {
-        var result = Query<Customer>.All.OrderBy(c => c.ContactName).Skip(5).Distinct();
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      var result = Query<Customer>.All
+        .OrderBy(c => c.ContactName)
+        .Skip(5)
+        .Distinct();
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .OrderBy(c => c.ContactName)
+        .Skip(5)
+        .Distinct();
+      Assert.IsTrue(expected.SequenceEqual(result));
+      Assert.Greater(result.ToList().Count, 0);
     }
 
     [Test]
     public void DistinctSkipTest()
     {
-        var result = Query<Customer>.All.Distinct().OrderBy(c => c.ContactName).Skip(5);
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      var result = Query<Customer>.All
+        .Distinct()
+        .OrderBy(c => c.ContactName)
+        .Skip(5);
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .Distinct()
+        .OrderBy(c => c.ContactName)
+        .Skip(5);
+      Assert.IsTrue(expected.SequenceEqual(result));
+      Assert.Greater(result.ToList().Count, 0);
     }
 
     [Test]
     public void SkipTakeTest()
     {
-        var result = Query<Customer>.All.OrderBy(c => c.ContactName).Skip(5).Take(10).Distinct();
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      var result = Query<Customer>.All
+        .OrderBy(c => c.ContactName)
+        .Skip(5)
+        .Take(10)
+        .Distinct();
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .OrderBy(c => c.ContactName)
+        .Skip(5)
+        .Take(10)
+        .Distinct();
+      Assert.IsTrue(expected.SequenceEqual(result));
+      Assert.Greater(result.ToList().Count, 0);
     }
 
     [Test]
     public void TakeSkipTest()
     {
-        var result = Query<Customer>.All.OrderBy(c => c.ContactName).Take(10).Skip(5).Distinct();
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      var result = Query<Customer>.All
+        .OrderBy(c => c.ContactName)
+        .Take(10)
+        .Skip(5)
+        .Distinct();
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .OrderBy(c => c.ContactName)
+        .Take(10)
+        .Skip(5)
+        .Distinct();
+      Assert.IsTrue(expected.SequenceEqual(result));
+      Assert.Greater(result.ToList().Count, 0);
     }
 
     [Test]
     public void DistinctSkipTakeTest()
     {
-        var result = Query<Customer>.All.Select(c => c.ContactName).Distinct().OrderBy(c => c).Skip(5).Take(10);
-        var list = result.ToList();
-        Assert.Greater(list.Count, 0);
+      var result = Query<Customer>.All
+        .Select(c => c.ContactName)
+        .Distinct()
+        .OrderBy(c => c)
+        .Skip(5)
+        .Take(10);
+      var expected = Query<Customer>.All
+        .AsEnumerable()
+        .Select(c => c.ContactName)
+        .Distinct()
+        .OrderBy(c => c)
+        .Skip(5)
+        .Take(10);
+      Assert.IsTrue(expected.SequenceEqual(result));
+      Assert.Greater(result.ToList().Count, 0);
     }
-
-    /*
-     * 
-     * public void TestSkip()
-        {
-            TestQuery(
-                db.Customers.OrderBy(c => c.ContactName).Skip(5)
-                );
-        }
-
-        public void TestSkipTake()
-        {
-            TestQuery(
-                db.Customers.OrderBy(c => c.ContactName).Skip(5).Take(10)
-                );
-        }
-
-        public void TestTakeSkip()
-        {
-            TestQuery(
-                db.Customers.OrderBy(c => c.ContactName).Take(10).Skip(5)
-                );
-        }
-     * 
-     * public void TestDistinctGroupBy()
-        {
-            TestQuery(
-                db.Orders.Distinct().GroupBy(o => o.CustomerID)
-                );
-        }
-
-        public void TestGroupByDistinct()
-        {
-            TestQuery(
-                db.Orders.GroupBy(o => o.CustomerID).Distinct()
-                );
-
-        }
-
-*/
   }
 }
