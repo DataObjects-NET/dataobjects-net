3fd8ab6467383252b55834a1f2a55f00bfa6d2d6
Alex Yakunin <alex.yakunin@gmail.com>
2009-05-26 11:09:17 +0000

    No comment.

---
+++ Xtensive.Core/Xtensive.Core/Collections/SetUtils.cs
@@ -0,0 +1,32 @@
+// Copyright (C) 2007 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alex Yakunin
+// Created:    2009.05.26
+
+namespace Xtensive.Core.Collections
+{
+  /// <summary>
+  /// <see cref="ISet"/> related utilities.
+  /// </summary>
+  /// <typeparam name="TItem">Type of set item.</typeparam>
+  public static class SetUtils<TItem>
+  {
+    private static readonly ReadOnlySet<TItem> emptySet;
+
+    /// <summary>
+    /// Gets empty set of items of <typeparamref name="TItem"/> type.
+    /// </summary>
+    public static ReadOnlySet<TItem> EmptySet {
+      get { return emptySet; }
+    }
+
+
+    // Constructors
+
+    static SetUtils()
+    {
+      emptySet = new ReadOnlySet<TItem>(new SetSlim<TItem>(), false);
+    }
+  }
+}
