dfec0dea3306d8d45859e275e9cd0e9670255308
Alexander Nickolaev <alnikola@microsoft.com>
2009-06-01 05:55:40 +0000

    - Issue #102: Add \ fix documentation

--- Xtensive.Core/Xtensive.Core/Linq/ComparisonOperation.cs
+++ Xtensive.Core/Xtensive.Core/Linq/ComparisonOperation.cs
@@ -6,18 +6,70 @@
 
 namespace Xtensive.Core.Linq
 {
+  /// <summary>
+  /// Comparison operation.
+  /// </summary>
   public enum ComparisonOperation
   {
+    /// <summary>
+    /// The operation is unknown.
+    /// </summary>
     Unknown = 0,
+
+    /// <summary>
+    /// The equality operation.
+    /// </summary>
     Equal,
+
+    /// <summary>
+    /// The non-equality operation.
+    /// </summary>
     NotEqual,
+
+    /// <summary>
+    /// The 'less than' operation.
+    /// </summary>
     LessThan,
+
+    /// <summary>
+    /// The 'less than or equal' operation
+    /// </summary>
     LessThanOrEqual,
+
+    /// <summary>
+    /// The 'greater than' operation.
+    /// </summary>
     GreaterThan,
+
+    /// <summary>
+    /// The 'greater than or equal' operation.
+    /// </summary>
     GreaterThanOrEqual,
+
+    /// <summary>
+    /// The operation corresponding to a call to one of following methods: 
+    /// <see cref="string.StartsWith(string)"/>, 
+    /// <see cref="string.StartsWith(string,bool,System.Globalization.CultureInfo)"/>, 
+    /// <see cref="string.StartsWith(string,System.StringComparison)"/>.
+    /// </summary>
     LikeStartsWith,
+
+    /// <summary>
+    /// The operation corresponding to a call to one of following methods: 
+    /// <see cref="string.EndsWith(string)"/>, 
+    /// <see cref="string.EndsWith(string,bool,System.Globalization.CultureInfo)"/>, 
+    /// <see cref="string.EndsWith(string,System.StringComparison)"/>.
+    /// </summary>
     LikeEndsWith,
+
+    /// <summary>
+    /// The negation of the 'like starts with' operation.
+    /// </summary>
     NotLikeStartsWith,
+
+    /// <summary>
+    /// The negation of the 'like ends with' operation.
+    /// </summary>
     NotLikeEndsWith
   }
 }
