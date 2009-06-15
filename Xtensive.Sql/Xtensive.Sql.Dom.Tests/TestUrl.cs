8a3c0499ac490243534408b7507e5f347d5705a4
Dima Maximov <dmitri.maximov@gmail.com>
2009-06-15 09:39:12 +0000

    - Fixed issue #203: Fix issue with running some PgSql SQL DOM tests on AppServer

--- Xtensive.Sql/Xtensive.Sql.Dom.Tests/TestUrl.cs
+++ Xtensive.Sql/Xtensive.Sql.Dom.Tests/TestUrl.cs
@@ -11,10 +11,10 @@ namespace Xtensive.Sql.Dom.Tests
     public const string MsSql2005 = "mssql2005://localhost/SqlDomTests";
     public const string MsSql2005AW = "mssql2005://localhost/AdventureWorks";
 
-    public const string PgSql80 = "pgsql://do4test:do4testpwd@localhost:8032/do40test?Encoding=ASCII&Pooling=on&MinPoolSize=1&MaxPoolSize=5";
-    public const string PgSql81 = "pgsql://do4test:do4testpwd@localhost:8132/do40test?Encoding=ASCII&Pooling=on&MinPoolSize=1&MaxPoolSize=5";
-    public const string PgSql82 = "pgsql://do4test:do4testpwd@localhost:8232/do40test?Encoding=ASCII&Pooling=on&MinPoolSize=1&MaxPoolSize=5";
-    public const string PgSql83 = "pgsql://do4test:do4testpwd@localhost:8332/do40test?Encoding=ASCII&Pooling=on&MinPoolSize=1&MaxPoolSize=5";
+    public const string PgSql80 = "pgsql://do4test:do4testpwd@127.0.0.1:8032/do40test?Encoding=ASCII&Pooling=on&MinPoolSize=1&MaxPoolSize=5";
+    public const string PgSql81 = "pgsql://do4test:do4testpwd@127.0.0.1:8132/do40test?Encoding=ASCII&Pooling=on&MinPoolSize=1&MaxPoolSize=5";
+    public const string PgSql82 = "pgsql://do4test:do4testpwd@127.0.0.1:8232/do40test?Encoding=ASCII&Pooling=on&MinPoolSize=1&MaxPoolSize=5";
+    public const string PgSql83 = "pgsql://do4test:do4testpwd@127.0.0.1:8332/do40test?Encoding=ASCII&Pooling=on&MinPoolSize=1&MaxPoolSize=5";
 
     public const string VistaDb = "vistadb://localhost/VistaDb/VDBTests.vdb3";
   }
