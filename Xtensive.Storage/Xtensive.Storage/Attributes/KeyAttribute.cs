fd87d4210f4a482e19fe08b47439eb0984b09d2d
Alex Kofman <al.kofman@gmail.com>
2009-06-16 16:36:50 +0000

    - Comments on KeyAttribute class

--- Xtensive.Storage/Xtensive.Storage/Attributes/KeyAttribute.cs
+++ Xtensive.Storage/Xtensive.Storage/Attributes/KeyAttribute.cs
@@ -11,7 +11,8 @@ using Xtensive.Core.Internals.DocTemplates;
 namespace Xtensive.Storage
 {
   /// <summary>
-  /// Marks persistent property as a part of primary key.
+  /// Marks persistent property as a part of primary key and 
+  /// specifies <see cref="Position"/> and <see cref="Direction"/> of the field in key.
   /// </summary>
   [Serializable]
   [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
@@ -20,11 +21,23 @@ namespace Xtensive.Storage
     /// <summary>
     /// Gets or sets the position of persistent property inside primary key.
     /// </summary>
+    /// <remarks>
+    /// <para>Each key in hierarchy should have an unique position from 0 to N-1 where N is number of key fields.</para>
+    /// <para>
+    /// Key fields positions and <see cref="Direction">directions</see> choise can be based on some optimization purposes, 
+    /// e.g. sometimes it can be better to have specific order and <see cref="Direction"/> of key fields.
+    /// </para>
+    /// <para>Default position is <c>0</c>.</para>
+    /// </remarks>
     public int Position { get; set; }
 
     /// <summary>
     /// Gets or sets the sort direction. Default is <see cref="Core.Direction.Positive"/>.
     /// </summary>
+    /// <para>
+    /// Key fields <see cref="Position">positions</see> and directions choise can be based on some optimization purposes, 
+    /// e.g. sometimes it can be better to have specific order and direction of key fields.
+    /// </para>
     public Direction Direction { get; set; }
 
     /// <summary>
@@ -38,7 +51,7 @@ namespace Xtensive.Storage
     /// <summary>
     /// <see cref="ClassDocTemplate.Ctor" copy="true"/>
     /// </summary>
-    /// <param name="position">The position of persistent property inside primary key.</param>
+    /// <param name="position">The <see cref="Position">position</see> of persistent property inside primary key.</param>
     public KeyAttribute(int position)
       : this(position, Direction.Positive)
     {
@@ -47,7 +60,7 @@ namespace Xtensive.Storage
     /// <summary>
     /// <see cref="ClassDocTemplate.Ctor" copy="true"/>
     /// </summary>
-    /// <param name="direction">The sort direction.</param>
+    /// <param name="direction">The <see cref="Direction">sort direction</see>.</param>
     public KeyAttribute(Direction direction)
       : this(0, direction)
     {
@@ -56,8 +69,8 @@ namespace Xtensive.Storage
     /// <summary>
     /// <see cref="ClassDocTemplate.Ctor" copy="true"/>
     /// </summary>
-    /// <param name="position">The position of persistent property inside primary key.</param>
-    /// <param name="direction">The sort direction.</param>
+    /// <param name="position">The <see cref="Position">position</see> of persistent property inside primary key.</param>
+    /// <param name="direction">The <see cref="Direction">sort direction</see>.</param>
     public KeyAttribute(int position, Direction direction)
     {
       Position = position;
