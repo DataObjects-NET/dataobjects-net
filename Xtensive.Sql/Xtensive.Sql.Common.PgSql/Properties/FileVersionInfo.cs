6e43ac8f8be440df0b01ccce3d78e48290f0918c
Alex Yakunin <alex.yakunin@gmail.com>
2009-06-29 22:17:19 +0000

    No comment.

--- Xtensive.Sql/Xtensive.Sql.Common.PgSql/Properties/FileVersionInfo.cs
+++ Xtensive.Sql/Xtensive.Sql.Common.PgSql/Properties/FileVersionInfo.cs
@@ -7,4 +7,4 @@
 using System.Reflection;
 
 
-[assembly: AssemblyFileVersion("1.0.0.11631")]
+[assembly: AssemblyFileVersion("1.0.0.11767")]
--- Xtensive.Storage/Xtensive.Storage.Indexing/Properties/FileVersionInfo.cs
+++ Xtensive.Storage/Xtensive.Storage.Indexing/Properties/FileVersionInfo.cs
@@ -7,4 +7,4 @@
 using System.Reflection;
 
 
-[assembly: AssemblyFileVersion("1.0.0.11599")]
+[assembly: AssemblyFileVersion("1.0.0.11767")]
