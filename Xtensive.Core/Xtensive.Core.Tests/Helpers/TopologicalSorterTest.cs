1c368b39d02c656252782a98cde9620491841507
Alexey Gamzov <alex@gamzov.com>
2009-07-08 10:24:51 +0000

    No comment.

--- Xtensive.Core/Xtensive.Core.Tests/Helpers/TopologicalSorterTest.cs
+++ Xtensive.Core/Xtensive.Core.Tests/Helpers/TopologicalSorterTest.cs
@@ -6,6 +6,7 @@
 
 using System.Collections.Generic;
 using NUnit.Framework;
+using Xtensive.Core.Collections;
 using Xtensive.Core.Sorting;
 using Xtensive.Core.Testing;
 using System.Linq;
@@ -15,6 +16,22 @@ namespace Xtensive.Core.Tests.Helpers
   [TestFixture]
   public class TopologicalSorterTest
   {
+    [Test]
+    public void SelfReferenceTest()
+    {
+      var node = new Node<int, string>(1);
+      var connection = new NodeConnection<int, string>(node, node, "ConnectionItem");
+      node.AddConnection(connection);
+
+      List<NodeConnection<int, string>> removedEdges;
+      var result = TopologicalSorter.Sort(EnumerableUtils.One(node), out removedEdges);
+      Assert.AreEqual(1, result.Count);
+      Assert.AreEqual(node.Item, result[0]);
+      Assert.AreEqual(1, removedEdges.Count);
+      Assert.AreEqual(connection, removedEdges[0]);
+      
+    }
+
     [Test]
     public void CombinedTest()
     {
