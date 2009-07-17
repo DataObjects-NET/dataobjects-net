094db2c95e5796e90fe1f5b9f7ec14a6c1df3691
Alex Yakunin <alex.yakunin@gmail.com>
2009-07-17 00:22:23 +0000

    No comment.

--- Xtensive.Integrity/Xtensive.Integrity/Properties/FileVersionInfo.cs
+++ Xtensive.Integrity/Xtensive.Integrity/Properties/FileVersionInfo.cs
@@ -7,4 +7,4 @@
 using System.Reflection;
 
 
-[assembly: AssemblyFileVersion("1.0.0.11886")]
+[assembly: AssemblyFileVersion("1.0.0.11969")]
--- Xtensive.Storage/Xtensive.Storage/Properties/FileVersionInfo.cs
+++ Xtensive.Storage/Xtensive.Storage/Properties/FileVersionInfo.cs
@@ -7,4 +7,4 @@
 using System.Reflection;
 
 
-[assembly: AssemblyFileVersion("1.0.0.11963")]
+[assembly: AssemblyFileVersion("1.0.0.11969")]
