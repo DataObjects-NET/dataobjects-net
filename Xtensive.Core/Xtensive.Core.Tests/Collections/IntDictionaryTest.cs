fd1053293f2c2ff4874a27c266f785c0db598eef
Alexander Nickolaev <alnikola@microsoft.com>
2009-07-24 14:20:44 +0000

    - IntDictionary is implemented

---
+++ Xtensive.Core/Xtensive.Core.Tests/Collections/IntDictionaryTest.cs
@@ -0,0 +1,238 @@
+// Copyright (C) 2009 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexander Nikolaev
+// Created:    2009.07.23
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using Xtensive.Core.Collections;
+using Xtensive.Core.Diagnostics;
+using Xtensive.Core.Testing;
+
+namespace Xtensive.Core.Tests.Collections
+{
+  [TestFixture]
+  public sealed class IntDictionaryTest
+  {
+    [Test]
+    public void SimpleTest()
+    {
+      var dictionary = new IntDictionary<double>(3);
+      var keys = new[] {0, 1, 2, 0x53};
+      var values = new double[] {24, 435, 876, 3};
+      for (int i = 0; i < keys.Length; i++)
+        dictionary.Add(keys[i], values[i]);
+      CheckValues(dictionary, keys, values);
+      AssertEx.Throws<KeyNotFoundException>(() => { var t = dictionary[-1];});
+      AssertEx.Throws<KeyNotFoundException>(() => { var t = dictionary[4];});
+      AssertEx.Throws<KeyNotFoundException>(() => { var t = dictionary[0x54];});
+    }
+
+    [Test]
+    public void ResizeTest()
+    {
+      var dictionary = new IntDictionary<int>(3);
+      Assert.AreEqual(4, GetBucketCount(dictionary));
+      var keys = new[]
+                 {
+                   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+                 };
+      var values = GenerateValues(keys);
+      for (int i = 0; i < keys.Length; i++)
+        dictionary.Add(keys[i], values[i]);
+      CheckValues(dictionary, keys, values);
+      AssertEx.Throws<KeyNotFoundException>(() => { var t = dictionary[-1];});
+      AssertEx.Throws<KeyNotFoundException>(() => { var t = dictionary[0x16];});
+      var items = GetItemsCollection(dictionary);
+      for (var i = 0; i < items.Length; i++)
+        if (i < 16) {
+          Assert.IsNotNull(items[i]);
+          Assert.AreEqual(1, items[i].Length);
+        }
+        else
+          Assert.IsNull(items[i]);
+    }
+
+    [Test]
+    public void CollisionTest()
+    {
+      var dictionary = new IntDictionary<int>(15);
+      var initialBucketCount = GetBucketCount(dictionary);
+      var keys = new[]
+                 {
+                   0, 0xA00000, 1, 0x100001, 0x200001, 0x500002, 0x800002, 0x300003,0xA00003,
+                   0x400003, 0x700003, 0xF00004, 0xA00004, 0x900005,0x700005, 0x200005, 0xE00005, 5
+                 };
+      var values = GenerateValues(keys);
+      for (int i = 0; i < keys.Length; i++)
+        dictionary.Add(keys[i], values[i]);
+      CheckValues(dictionary, keys, values);
+      Assert.AreEqual(initialBucketCount, GetBucketCount(dictionary));
+      var items = GetItemsCollection(dictionary);
+      for (var i = 0; i < items.Length; i++)
+        if (i < 6) {
+          Assert.IsNotNull(items[i]);
+          Assert.Greater(items[i].Length, 1);
+        }
+        else
+          Assert.IsNull(items[i]);
+    }
+
+    [Test]
+    public void RemoveTest()
+    {
+      var dictionary = new IntDictionary<int>(15);
+      var keys = new[]
+                 {
+                   0, 0xA00000, 1, 0x100001, 0x200001, 0x500002, 0x800002, 0x300003,0xA00003,
+                   0x400003, 0x700003, 0xA00004, 0x900005,0x700005, 0x200005, 0xE00005, 5
+                 };
+      var values = GenerateValues(keys);
+      for (int i = 0; i < keys.Length; i++)
+        dictionary.Add(keys[i], values[i]);
+      Assert.IsFalse(dictionary.Remove(4));
+      Assert.IsFalse(dictionary.Remove(0xB00003));
+      CheckValues(dictionary, keys, values);
+      var items = GetItemsCollection(dictionary);
+      Assert.IsNotNull(items[4]);
+      CheckRemoving(ref keys, ref values, 0xA00004, dictionary);
+      Assert.IsNull(items[4]);
+      CheckRemoving(ref keys, ref values, 1, dictionary);
+      CheckRemoving(ref keys, ref values, 0x100001, dictionary);
+      CheckRemoving(ref keys, ref values, 5, dictionary);
+      CheckRemoving(ref keys, ref values, 0xE00005, dictionary);
+    }
+
+    [Test]
+    public void ClearTest()
+    {
+      var dictionary = new IntDictionary<int>(15);
+      var keys = new[]
+                 {
+                   0, 0xA00000, 1, 0x100001, 0x200001, 0x500002, 0x800002, 0x300003,0xA00003,
+                   0x400003, 0x700003, 0xA00004, 0x900005,0x700005, 0x200005, 0xE00005, 5
+                 };
+      var values = GenerateValues(keys);
+      for (int i = 0; i < keys.Length; i++)
+        dictionary.Add(keys[i], values[i]);
+      dictionary.Clear();
+      var items = GetItemsCollection(dictionary);
+      foreach (var bucket in items)
+        Assert.IsNull(bucket);
+    }
+    
+    [Test]
+    public void DuplicateKeyInsertTest()
+    {
+      var dictionary = new IntDictionary<int>(15);
+      var keys = new[] {0, 0x900005, 0x700005, 0x200005, 0xE00005, 5};
+      var values = GenerateValues(keys);
+      for (int i = 0; i < keys.Length; i++)
+        dictionary.Add(keys[i], values[i]);
+      AssertEx.Throws<ArgumentException>(() => dictionary.Add(0, 1));
+      AssertEx.Throws<ArgumentException>(() => dictionary.Add(0x900005, 1));
+      AssertEx.Throws<ArgumentException>(() => dictionary.Add(5, 1));
+    }
+
+    [Test]
+    public void MaskRequiringOffsetTest()
+    {
+      var dictionary = new IntDictionary<int>(252);
+      var keyOffsetField = typeof (IntDictionary<int>).GetField("keyOffset",
+        BindingFlags.NonPublic | BindingFlags.Instance);
+      Assert.Greater((int)keyOffsetField.GetValue(dictionary), 0);
+      var keysCount = 155;
+      var keys = new int[keysCount];
+      for (int i = 0; i < keysCount; i++)
+        keys[i] = 100 + i;
+      var values = GenerateValues(keys);
+      for (int i = 0; i < keys.Length; i++)
+        dictionary.Add(keys[i], values[i]);
+      CheckValues(dictionary, keys, values);
+    }
+    
+    [Test]
+    [Category("Profile")]
+    public void PerformanceTest()
+    {
+      var dictionary = new IntDictionary<int>(255);
+      var keysCount = 355;
+      var keys = new int[keysCount];
+      for (int i = 0; i < keysCount; i++)
+        keys[i] = 100 + i;
+      var values = GenerateValues(keys);
+      for (int i = 0; i < keys.Length; i++)
+        dictionary.Add(keys[i], values[i]);
+      int count = 10000000;
+      MeasureIntDictionary(keys, dictionary, count);
+      MeasureOriginalDictionary(keys, dictionary, count);
+    }
+
+    private static void MeasureIntDictionary(int[] keys, IntDictionary<int> dictionary, int count)
+    {
+      int keysLength = keys.Length;
+      int t = 0;
+      using (new Measurement("Reading IntDictionary<int>", count))
+        for (int i = 0; i < count; i++)
+          dictionary.TryGetValue(keys[i % keysLength], out t);
+      Assert.Greater(t, int.MinValue);
+    }
+
+    private static void MeasureOriginalDictionary(int[] keys, IEnumerable<KeyValuePair<int, int>> dictionary,
+      int count)
+    {
+      var keysLength = keys.Length;
+      var t = 0;
+      var reference = dictionary.ToDictionary(pair => pair.Key, pair => pair.Value);
+      using (new Measurement("Reading Dictionary<int, int>", count))
+        for (var i = 0; i < count; i++)
+          reference.TryGetValue(keys[i % keysLength], out t);
+      Assert.Greater(t, int.MinValue);
+    }
+    
+    private static void CheckValues<T>(IntDictionary<T> dictionary, int[] keys, T[] values)
+    {
+      for (var i = 0; i < keys.Length; i++)
+        Assert.AreEqual(values[i], dictionary[keys[i]]);
+    }
+
+    private static int GetBucketCount<T>(IntDictionary<T> dictionary)
+    {
+      return (GetItemsCollection(dictionary)).Length;
+    }
+
+    private static KeyValuePair<int, T>[][] GetItemsCollection<T>(IntDictionary<T> dictionary)
+    {
+      var itemsField = typeof (IntDictionary<T>)
+        .GetField("items", BindingFlags.NonPublic | BindingFlags.Instance);
+      return (KeyValuePair<int, T>[][])itemsField.GetValue(dictionary);
+    }
+    
+    private static int[] GenerateValues(IEnumerable<int> keys)
+    {
+      var rnd = new Random();
+      return (from key in keys select (int) (rnd.NextDouble() * 100000)).ToArray();
+    }
+
+    private static void CheckRemoving(ref int[] keys, ref int[] values, int keyToRemove,
+      IntDictionary<int> dictionary)
+    {
+      Assert.IsTrue(dictionary.Remove(keyToRemove));
+      Remove(ref keys, ref values, keyToRemove);
+      AssertEx.Throws<KeyNotFoundException>(() => { var t = dictionary[keyToRemove]; });
+      CheckValues(dictionary, keys, values);
+    }
+
+    private static void Remove(ref int[] keys, ref int[] values, int removedKey)
+    {
+      var removedValue = values[keys.IndexOf(removedKey)];
+      keys = (from key in keys where key!=removedKey select key).ToArray();
+      values = (from value in values where value!=removedValue select value).ToArray();
+    }
+  }
+}
---
+++ Xtensive.Core/Xtensive.Core/Collections/IntDictionary.cs
@@ -0,0 +1,290 @@
+// Copyright (C) 2009 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexander Nikolaev
+// Created:    2009.07.22
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xtensive.Core.Internals.DocTemplates;
+using Xtensive.Core.Resources;
+
+namespace Xtensive.Core.Collections
+{
+  /// <summary>
+  /// The fast dictionary-like collection using keys of type <see cref="int"/>.
+  /// </summary>
+  /// <typeparam name="TValue">The type of a value.</typeparam>
+  [Serializable]
+  public sealed class IntDictionary<TValue> : IEnumerable<KeyValuePair<int, TValue>>
+  {
+    private const int maxMaskPattern = int.MinValue;
+    private const int indexOfAbsenceKey = -1;
+    private const double maxFillFactor = 0.7;
+
+    private KeyValuePair<int, TValue>[][] items;
+    private int mask;
+    private int keyOffset;
+    private int occupiedBucketCount;
+
+    /// <summary>
+    /// Gets a value by the specified key.
+    /// </summary>
+    public TValue this[int key]
+    {
+      get {
+        var hashCode = CalculateHashCode(key);
+        var bucket = items[hashCode];
+        if (bucket==null)
+          throw new KeyNotFoundException();
+        if (bucket.Length==1) {
+          var pair = bucket[0];
+          if (pair.Key==key)
+            return pair.Value;
+          throw new KeyNotFoundException();
+        }
+        var keyIndex = FindKeyIndexInBucket(bucket, key);
+        if (keyIndex==indexOfAbsenceKey)
+          throw new KeyNotFoundException();
+        return bucket[keyIndex].Value;
+      }
+    }
+
+    /// <summary>
+    /// Adds the item to the collection.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <param name="value">The value.</param>
+    public void Add(int key, TValue value)
+    {
+      var newPair = new KeyValuePair<int, TValue>(key, value);
+      if (AddPair(newPair, items, CalculateHashCode(newPair.Key), ref occupiedBucketCount))
+        IncreaseSizeIfNecessary();
+    }
+
+    /// <summary>
+    /// Removes the item with the specified key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <returns><see langword="true" /> if an item was found and removed; 
+    /// otherwise <see langword="false" />.</returns>
+    public bool Remove(int key)
+    {
+      var hashCode = CalculateHashCode(key);
+      var bucket = items[hashCode];
+      if (bucket==null)
+        return false;
+      if (bucket.Length==1) {
+        if (bucket[0].Key==key) {
+          items[hashCode] = null;
+          return true;
+        }
+        return false;
+      }
+      var keyIndex = FindKeyIndexInBucket(bucket, key);
+      if (keyIndex==indexOfAbsenceKey)
+        return false;
+      RemoveFromBucket(bucket, hashCode, keyIndex);
+      return true;
+    }
+
+    /// <summary>
+    /// Removes all items.
+    /// </summary>
+    public void Clear()
+    {
+      for (int i = 0; i < items.Length; i++)
+        items[i] = null;
+    }
+
+    /// <summary>
+    /// Tries to get a value by the specified key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <param name="value">The value.</param>
+    /// <returns><see langword="bool" /> if the key was found; 
+    /// otherwise <see langword="false" />.</returns>
+    public bool TryGetValue(int key, out TValue value)
+    {
+      var hashCode = CalculateHashCode(key);
+      var bucket = items[hashCode];
+      if (bucket==null) {
+        value = default(TValue);
+        return false;
+      }
+      if (bucket.Length==1) {
+        var pair = bucket[0];
+        if (pair.Key==key) {
+          value = pair.Value;
+          return true;
+        }
+        value = default(TValue);
+        return false;
+      }
+      var keyIndex = FindKeyIndexInBucket(bucket, key);
+      if (keyIndex==indexOfAbsenceKey) {
+        value = default(TValue);
+        return false;
+      }
+      value = bucket[keyIndex].Value;
+      return true;
+    }
+
+    /// <inheritdoc/>
+    public IEnumerator<KeyValuePair<int, TValue>> GetEnumerator()
+    {
+      foreach (var bucket in items)
+        if (bucket!=null)
+          foreach (var pair in bucket)
+            yield return pair;
+    }
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+
+    #region Private / internal members
+
+    private int CalculateBucketCount()
+    {
+      var tempMask = mask;
+      keyOffset = 0;
+      while ((tempMask & 1)!=1) {
+        keyOffset++;
+        tempMask = tempMask >> 1;
+      }
+      return tempMask + 1;
+    }
+
+    private int CalculateHashCode(int key)
+    {
+      var result = key & mask;
+      var currentOffset = keyOffset;
+      while (currentOffset > 0) {
+        result = result >> 1;
+        currentOffset--;
+      }
+      return result;
+    }
+
+    private static bool AddPair(KeyValuePair<int, TValue> newPair, KeyValuePair<int, TValue>[][] items,
+      int hashCode, ref int occupiedBucketCount)
+    {
+      var bucket = items[hashCode];
+      if (bucket==null) {
+        occupiedBucketCount++;
+        items[hashCode] = new KeyValuePair<int, TValue>[1];
+        items[hashCode][0] = newPair;
+        return true;
+      }
+      var bucketSize = bucket.Length;
+      Array.Resize(ref bucket, bucketSize + 1);
+      items[hashCode] = bucket;
+      InsertIntoBucket(newPair, bucket);
+      return false;
+    }
+
+    private static void InsertIntoBucket(KeyValuePair<int, TValue> newPair,
+      KeyValuePair<int, TValue>[] bucket)
+    {
+      var index = 0;
+      var endIndex = bucket.Length - 1;
+      while (bucket[index].Key < newPair.Key && index < endIndex)
+        index++;
+      if (bucket[index].Key==newPair.Key)
+        throw new ArgumentException(Strings.ExItemWithTheSameKeyHasBeenAdded);
+      for (int i = bucket.Length - 1; i > index; i--)
+        bucket[i] = bucket[i - 1];
+      bucket[index] = newPair;
+    }
+
+    private void IncreaseSizeIfNecessary()
+    {
+      if ((double) occupiedBucketCount / items.Length < maxFillFactor)
+        return;
+      if ((mask & maxMaskPattern)!=0)
+        return;
+      ExtendMask();
+      var bucketCount = CalculateBucketCount();
+      var newCollection = new KeyValuePair<int, TValue>[bucketCount][];
+      ResizeBucketCollection(newCollection);
+    }
+
+    private void ExtendMask()
+    {
+      var leftOffset = 0;
+      var tempMask = (uint) mask;
+      while ((tempMask & maxMaskPattern >> 1)==0) {
+        tempMask = tempMask << 1;
+        leftOffset++;
+      }
+      tempMask = (uint) (tempMask | maxMaskPattern);
+      mask = (int) (tempMask >> leftOffset);
+    }
+
+    private void ResizeBucketCollection(KeyValuePair<int, TValue>[][] target)
+    {
+      var newOccupiedBucketCount = 0;
+      foreach (var pair in this) {
+        var hashCode = CalculateHashCode(pair.Key);
+        AddPair(pair, target, hashCode, ref newOccupiedBucketCount);
+      }
+      occupiedBucketCount = newOccupiedBucketCount;
+      items = target;
+    }
+
+    private void RemoveFromBucket(KeyValuePair<int, TValue>[] bucket, int hashCode, int keyIndex)
+    {
+      var endIndex = bucket.Length - 2;
+      for (int i = keyIndex; i <= endIndex; i++)
+        bucket[i] = bucket[i + 1];
+      Array.Resize(ref bucket, bucket.Length - 1);
+      items[hashCode] = bucket;
+    }
+
+    private static int FindKeyIndexInBucket(KeyValuePair<int, TValue>[] bucket, int key)
+    {
+      return FindKeyIndexInBucket(0, bucket.Length - 1, bucket, key);
+    }
+
+    private static int FindKeyIndexInBucket(int start, int end, KeyValuePair<int, TValue>[] bucket, int key)
+    {
+      while (end >= start) {
+        if (end==start)
+          if (key==bucket[start].Key)
+            return start;
+          else
+            return indexOfAbsenceKey;
+        var middle = ((end - start) / 2) + start;
+        var middleKey = bucket[middle].Key;
+        if (key==bucket[middle].Key)
+          return middle;
+        if (key > middleKey) {
+          start = middle + 1;
+          continue;
+        }
+        end = middle - 1;
+      }
+      return indexOfAbsenceKey;
+    }
+
+    #endregion
+
+    // Constructors
+
+    /// <summary>
+    /// <see cref="ClassDocTemplate.Ctor" copy="true"/>
+    /// </summary>
+    /// <param name="mask">The mask to calculate the hash code of a key.</param>
+    public IntDictionary(int mask)
+    {
+      ArgumentValidator.EnsureArgumentIsGreaterThan(mask, 0, "mask");
+      this.mask = mask;
+      var bucketCount = CalculateBucketCount();
+      items = new KeyValuePair<int, TValue>[bucketCount][];
+    }
+  }
+}
