baceb764a4315d9ba4790d8f6b687e19f1b428d9
Alex Yakunin <alex.yakunin@gmail.com>
2009-08-12 18:11:23 +0000

    No comment.

--- Xtensive.Sql/Xtensive.Sql.Oracle/Properties/FileVersionInfo.cs
+++ Xtensive.Sql/Xtensive.Sql.Oracle/Properties/FileVersionInfo.cs
@@ -7,4 +7,4 @@
 using System.Reflection;
 
 
-[assembly: AssemblyFileVersion("1.0.0.11611")]
+[assembly: AssemblyFileVersion("1.0.0.12292")]
--- Xtensive.Storage/Xtensive.Storage.Rse/Properties/FileVersionInfo.cs
+++ Xtensive.Storage/Xtensive.Storage.Rse/Properties/FileVersionInfo.cs
@@ -7,4 +7,4 @@
 using System.Reflection;
 
 
-[assembly: AssemblyFileVersion("1.0.0.11969")]
+[assembly: AssemblyFileVersion("1.0.0.12279")]
