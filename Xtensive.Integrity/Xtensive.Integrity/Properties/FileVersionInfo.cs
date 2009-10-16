578b4c5b438d1291b6d00f0c224ed5d7219a16ce
Alex Yakunin <alex.yakunin@gmail.com>
2009-10-16 08:01:12 +0000

    Bugfix in CopyIndirectDependencies.targets (rebuild didn't work).

--- Xtensive.Integrity/Xtensive.Integrity/Properties/FileVersionInfo.cs
+++ Xtensive.Integrity/Xtensive.Integrity/Properties/FileVersionInfo.cs
@@ -7,4 +7,4 @@
 using System.Reflection;
 
 
-[assembly: AssemblyFileVersion("1.0.0.12783")]
+[assembly: AssemblyFileVersion("1.0.0.12818")]
--- Xtensive.Storage/Xtensive.Storage.Model/Properties/FileVersionInfo.cs
+++ Xtensive.Storage/Xtensive.Storage.Model/Properties/FileVersionInfo.cs
@@ -7,4 +7,4 @@
 using System.Reflection;
 
 
-[assembly: AssemblyFileVersion("1.0.0.12792")]
+[assembly: AssemblyFileVersion("1.0.0.12824")]
