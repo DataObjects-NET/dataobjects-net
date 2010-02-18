a78a7a7645ec3f87ad069ac33b945daf2bc6dccc
Alexis Kochetov <alexis.kochetov@gmail.com>
2010-02-18 12:45:41 +0000

    No comment.

---
+++ Xtensive.Storage/Xtensive.Storage.Tests/Linq/SelectDTOTest.cs
@@ -0,0 +1,61 @@
+// Copyright (C) 2010 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexis Kochetov
+// Created:    2010.02.18
+
+using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using Xtensive.Storage.Configuration;
+using Xtensive.Storage.Tests.Linq.Dto;
+using System.Linq;
+
+namespace Xtensive.Storage.Tests.Linq
+{
+  namespace Dto
+  {
+    [HierarchyRoot]
+    public class Person
+     : Entity
+    {
+      [Field, Key]
+      public int ID { get; private set; }
+
+      [Field]
+      public string Name { get; set; }
+    }
+
+    public class PersonDto
+    {
+      public string Name { get; set; }
+    }
+  }
+
+  [Serializable]
+  public class SelectDTOTest : AutoBuildTest
+  {
+    protected override DomainConfiguration BuildConfiguration()
+    {
+      var config = base.BuildConfiguration();
+      config.Types.Register(typeof(Person));
+      return config;
+    }
+
+    [Test]
+    public void MainTest()
+    {
+      using (Session.Open(Domain))
+      using (var t = Transaction.Open()) {
+        var alex = new Person() { Name = "Alex" };
+        var query = Query.All<Person>()
+          .Select(p => new PersonDto {Name = p.Name})
+          .Where(personDto => personDto.Name == "Alex");
+        var result = query.ToList();
+        var firstPerson = query.SingleOrDefault();
+        Assert.IsTrue(firstPerson.Name == "Alex");
+        t.Complete();
+      }
+    }
+  }
+}
