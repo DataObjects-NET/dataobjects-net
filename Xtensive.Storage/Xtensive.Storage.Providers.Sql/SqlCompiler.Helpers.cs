907547074db99d1bd220ba6c6efe189fd34c80a1
Alexis Kochetov <alexis.kochetov@gmail.com>
2010-03-18 20:50:29 +0500

    - Minor fix in SqlCompiler

--- Xtensive.Storage/Xtensive.Storage.Providers.Sql/SqlCompiler.Helpers.cs
+++ Xtensive.Storage/Xtensive.Storage.Providers.Sql/SqlCompiler.Helpers.cs
@@ -92,7 +92,7 @@ namespace Xtensive.Storage.Providers.Sql
       var shouldUseQueryColumns = false;
       if (origin.Sources.Any(i => i.Type == ProviderType.Index))
         shouldUseQueryColumns |= true;
-      shouldUseQueryColumns |= origin.Type.In(ProviderType.Take, ProviderType.Skip, ProviderType.Filter, ProviderType.Index, ProviderType.RowNumber)
+      shouldUseQueryColumns |= origin.Type.In(ProviderType.Take, ProviderType.Skip, ProviderType.Join, ProviderType.Filter, ProviderType.Index, ProviderType.RowNumber)
         && query.Columns.Count != query.From.Columns.Count;
       if (query.Columns.Any(IsColumnStub) || query.GroupBy.Count > 0 || shouldUseQueryColumns) {
         foreach (var column in query.Columns) {
