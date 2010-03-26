9cdef2beb0d2db055f8c99eef59f541deba3d74b
Alexis Kochetov <alexis.kochetov@gmail.com>
2010-03-26 17:46:26 +0500

    - SelectDtoTest has been extended

--- Xtensive.Storage/Xtensive.Storage.Tests/Linq/SelectDtoTest.cs
+++ Xtensive.Storage/Xtensive.Storage.Tests/Linq/SelectDtoTest.cs
@@ -15,20 +15,34 @@ namespace Xtensive.Storage.Tests.Linq
 {
   namespace Dto
   {
+    public enum BudgetType
+    {
+      Default,
+      Regional
+    }
+
     [HierarchyRoot]
     public class Person
      : Entity
     {
       [Field, Key]
-      public int ID { get; private set; }
+      public int Id { get; private set; }
 
       [Field]
       public string Name { get; set; }
+
+      [Field]
+      public int? Tag { get; set; }
+      [Field]
+      public BudgetType? BudgetType { get; set; }
     }
 
     public class PersonDto
     {
+      public int Id { get; set; }
       public string Name { get; set; }
+      public int? Tag { get; set; }
+      public BudgetType? BudgetType { get; set; }
     }
   }
 
@@ -54,6 +68,22 @@ namespace Xtensive.Storage.Tests.Linq
         var result = query.ToList();
         var firstPerson = query.SingleOrDefault();
         Assert.IsTrue(firstPerson.Name == "Alex");
+        t.Complete();
+      }
+    }
+
+    [Test]
+    public void WhereTest()
+    {
+      using (var session = Session.Open(Domain))
+      using (var t = Transaction.Open()) {
+        var person5 = new Person() {Name = "John", Tag = 5, BudgetType = BudgetType.Regional};
+        var personEmpty = new Person() {Name = "John"};
+
+        var count = Query.All<Person>()
+          .Select(p => new PersonDto() { Id = p.Id, Name = p.Name, Tag = p.Tag, BudgetType = p.BudgetType})
+          .Where(x => x.Tag == 5 && x.BudgetType == BudgetType.Regional).OrderBy(dto => dto.Id).Count();
+        Assert.AreEqual(1, count);
         t.Complete();
       }
     }
