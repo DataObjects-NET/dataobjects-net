5de55fcc34e98f43f94365e9d8df3a226ff1e642
Alex Yakunin <alex.yakunin@gmail.com>
2010-04-16 05:16:47 +0600

    - Storage.Tests is splat to Storage.Tests.Core (base types), Storage.Tests and Storage.Tests.Part2. New tests must be implemented in Part2. So speedup the compilation, simply unload Tests project. - VersionInfo comparison is fixed. Now there is ConflictsWith method, which implementation differs from Equals. == and != are relying on ConflictsWith instead of Equals (can't do the same for Equals, since its impl. must be "paired" with GetHashCode). So now there is no conflict, if all mutually available fields in VersionInfo.Value-s are equal, and count of these fields is the same. - Issue 631 is fully closed. - About 4 new tests fail - don't use this build!

--- Xtensive.Storage/Xtensive.Storage.Tests.Core/Properties/AssemblyInfo.cs
+++ Xtensive.Storage/Xtensive.Storage.Tests.Core/Properties/AssemblyInfo.cs
@@ -11,25 +11,22 @@ using Xtensive.Storage.Aspects;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly : AssemblyTitle("Xtensive.Storage.Tests")]
-[assembly : AssemblyDescription("")]
-[assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("Xtensive LLC")]
-[assembly : AssemblyProduct("Xtensive.Storage.Tests")]
-[assembly : AssemblyCopyright("Copyright © Xtensive LLC 2010")]
-[assembly : AssemblyTrademark("")]
-[assembly : AssemblyCulture("")]
+[assembly: AssemblyTitle("Xtensive.Storage.Tests.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Xtensive LLC")]
+[assembly: AssemblyProduct("Xtensive.Storage.Tests.Core")]
+[assembly: AssemblyCopyright("Copyright © Xtensive LLC 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
-[assembly : ComVisible(false)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly : Guid("b667ca2a-e17f-4dbc-bc06-656780d9fac9")]
+[assembly: Guid("7693cfa2-1c6b-41c7-956b-4dbf60324387")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -41,11 +38,10 @@ using Xtensive.Storage.Aspects;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
-[assembly : AssemblyVersion("1.0.0.0")]
-[assembly : AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly : CLSCompliant(true)]
 [assembly : SecurityPermission(SecurityAction.RequestMinimum, Execution = true)]
 
 // This ensures the methods of Persistent & SessionBound will be aspected
-[assembly : Persistent(AttributeTargetAssemblies = "Xtensive.Storage.Tests")]
+[assembly: Persistent(AttributeTargetAssemblies = "Xtensive.Storage.Tests.Core")]
