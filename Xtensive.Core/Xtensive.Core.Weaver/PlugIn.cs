673ebcb7c183c0922a4b91e1bda04138ef9a24af
Alexis Kochetov <alexis.kochetov@gmail.com>
2010-04-30 18:16:20 +0600

    - LicenseManager GUI

--- Xtensive.Core/Xtensive.Core.Weaver/PlugIn.cs
+++ Xtensive.Core/Xtensive.Core.Weaver/PlugIn.cs
@@ -5,7 +5,13 @@
 // Created:    2010.04.07
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using License;
+using LicenseManager;
 using Xtensive.Core.Aspects;
+using Xtensive.Licensing;
 
 namespace Xtensive.Core.Weaver
 {
@@ -14,11 +20,43 @@ namespace Xtensive.Core.Weaver
   /// </summary>
   public sealed class PlugIn : PostSharp.AspectWeaver.PlugIn
   {
-    public PlugIn()
-      : base(Priorities.User)
-    {
-    }
     // $(ProjectDir)..\..\Xtensive.Licensing\Protect.bat "$(TargetPath)" "$(ProjectDir)obj\$(ConfigurationName)\$(TargetFileName)"
+
+    public override bool Execute()
+    {
+      // TODO: Check license
+      var properties = new Dictionary<string, string>();
+      for (int i = 0; i < Status.KeyValueList.Count; i++) {
+        string key = Status.KeyValueList.GetKey(i).ToString();
+        string value = Status.KeyValueList.GetByIndex(i).ToString();
+        properties.Add(key, value);
+      }
+      string licensee;
+      string licenseTypeString;
+      string numberOfDevelopersString;
+      properties.TryGetValue(LicenseInfo.LicenseeKey, out licensee);
+      properties.TryGetValue(LicenseInfo.LicenseTypeKey, out licenseTypeString);
+      properties.TryGetValue(LicenseInfo.NumberOfDevelopersKey, out numberOfDevelopersString);
+      var licenseType = licenseTypeString.IsNullOrEmpty()
+        ? LicenseType.Trial
+        : (LicenseType) Enum.Parse(typeof (LicenseType), licenseTypeString);
+      var numberOfDevelopers = numberOfDevelopersString.IsNullOrEmpty()
+        ? -1
+        : int.Parse(numberOfDevelopersString);
+      var licenseInfo = new LicenseInfo {
+        LicenseType = licenseType,
+        ExpireOn = Status.Expiration_Date,
+        TrialDays = Status.Evaluation_Time,
+        TrialDaysCurrent = Status.Evaluation_Time_Current,
+        Licensee = licensee,
+        NumberOfDevelopers = numberOfDevelopers
+      };
+      RunLicensingAgent(licenseInfo);
+      return base.Execute();
+    }
+    
+    #region Non-public methods
+
     protected override void Initialize()
     {
       AddAspectWeaverFactory<ReplaceAutoProperty, ReplaceAutoPropertyWeaver>();
@@ -27,5 +65,33 @@ namespace Xtensive.Core.Weaver
       AddAspectWeaverFactory<ImplementConstructor, ImplementConstructorWeaver>();
       AddAspectWeaverFactory<ImplementFactoryMethod, ImplementFactoryMethodWeaver>();
     }
+
+    private void RunLicensingAgent(LicenseInfo licenseInfo)
+    {
+      string path = Path.Combine(Path.GetDirectoryName(typeof (PlugIn).Assembly.Location), "LicenseManager.exe");
+      if (!File.Exists(path))
+        throw new FileNotFoundException("LicenseManager.exe");
+
+      if (!Environment.UserInteractive || Environment.OSVersion.Platform!=PlatformID.Win32NT || path.IsNullOrEmpty())
+        return;
+      var startInfo = new ProcessStartInfo(path) {
+        UseShellExecute = false,
+        CreateNoWindow = true
+      };
+      Process.Start(startInfo);
+      using (var client = new PipeClient()) {
+        client.SendLicenseInfo(licenseInfo);
+      }
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public PlugIn()
+      : base(Priorities.User)
+    {}
+
+    #endregion
   }
 }
