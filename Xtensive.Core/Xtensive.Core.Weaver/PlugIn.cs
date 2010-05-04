9f5316d426089cb03229eb7e6be30ad67e917ae8
Alexis Kochetov <alexis.kochetov@gmail.com>
2010-05-04 13:08:26 +0600

    - Root solution refactoring - Licensing in progress

--- Xtensive.Core/Xtensive.Core.Weaver/PlugIn.cs
+++ Xtensive.Core/Xtensive.Core.Weaver/PlugIn.cs
@@ -26,34 +26,35 @@ namespace Xtensive.Core.Weaver
 
     protected override void Initialize()
     {
+      base.Initialize();
       // TODO: Check license
-//      var properties = new Dictionary<string, string>();
-//      for (int i = 0; i < Status.KeyValueList.Count; i++) {
-//        string key = Status.KeyValueList.GetKey(i).ToString();
-//        string value = Status.KeyValueList.GetByIndex(i).ToString();
-//        properties.Add(key, value);
-//      }
-//      string licensee;
-//      string licenseTypeString;
-//      string numberOfDevelopersString;
-//      properties.TryGetValue(LicenseInfo.LicenseeKey, out licensee);
-//      properties.TryGetValue(LicenseInfo.LicenseTypeKey, out licenseTypeString);
-//      properties.TryGetValue(LicenseInfo.NumberOfDevelopersKey, out numberOfDevelopersString);
-//      LicenseType licenseType = licenseTypeString.IsNullOrEmpty()
-//        ? LicenseType.Trial
-//        : (LicenseType) Enum.Parse(typeof (LicenseType), licenseTypeString);
-//      int numberOfDevelopers = numberOfDevelopersString.IsNullOrEmpty()
-//        ? -1
-//        : int.Parse(numberOfDevelopersString);
-//      var licenseInfo = new LicenseInfo {
-//        LicenseType = licenseType,
-//        ExpireOn = Status.Expiration_Date,
-//        TrialDays = Status.Evaluation_Time,
-//        TrialDaysCurrent = Status.Evaluation_Time_Current,
-//        Licensee = licensee,
-//        NumberOfDevelopers = numberOfDevelopers
-//      };
-//      RunLicensingAgent(licenseInfo);
+      var properties = new Dictionary<string, string>();
+      for (int i = 0; i < Status.KeyValueList.Count; i++) {
+        string key = Status.KeyValueList.GetKey(i).ToString();
+        string value = Status.KeyValueList.GetByIndex(i).ToString();
+        properties.Add(key, value);
+      }
+      string licensee;
+      string licenseTypeString;
+      string numberOfDevelopersString;
+      properties.TryGetValue(LicenseInfo.LicenseeKey, out licensee);
+      properties.TryGetValue(LicenseInfo.LicenseTypeKey, out licenseTypeString);
+      properties.TryGetValue(LicenseInfo.NumberOfDevelopersKey, out numberOfDevelopersString);
+      LicenseType licenseType = licenseTypeString.IsNullOrEmpty()
+        ? LicenseType.Trial
+        : (LicenseType) Enum.Parse(typeof (LicenseType), licenseTypeString);
+      int numberOfDevelopers = numberOfDevelopersString.IsNullOrEmpty()
+        ? -1
+        : int.Parse(numberOfDevelopersString);
+      var licenseInfo = new LicenseInfo {
+        LicenseType = licenseType,
+        ExpireOn = Status.Expiration_Date,
+        TrialDays = Status.Evaluation_Time,
+        TrialDaysCurrent = Status.Evaluation_Time_Current,
+        Licensee = licensee,
+        NumberOfDevelopers = numberOfDevelopers
+      };
+      RunLicensingAgent(licenseInfo);
       AddAspectWeaverFactory<ReplaceAutoProperty, ReplaceAutoPropertyWeaver>();
       AddAspectWeaverFactory<ImplementConstructorEpilogue, ConstructorEpilogueWeaver>();
       AddAspectWeaverFactory<NotSupportedAttribute, NotSupportedWeaver>();
@@ -61,22 +62,20 @@ namespace Xtensive.Core.Weaver
       AddAspectWeaverFactory<ImplementFactoryMethod, ImplementFactoryMethodWeaver>();
     }
 
-    private void RunLicensingAgent(LicenseInfo licenseInfo)
+    private static void RunLicensingAgent(LicenseInfo licenseInfo)
     {
-      string path = Path.Combine(Path.GetDirectoryName(typeof (PlugIn).Assembly.Location), "LicenseManager.exe");
-      ErrorLog.Debug(path);
+      var dataObjectsPath = Environment.GetEnvironmentVariable("DataObjectsDotNetPath");
+      if (dataObjectsPath.IsNullOrEmpty() || !Directory.Exists(dataObjectsPath))
+        throw new InvalidOperationException("DataObjects.Net is not installed. Please install.");
+      var path = Path.Combine(dataObjectsPath, "Bin", "Latest", "Xtensive.Licensing.Manager.exe");
       if (!File.Exists(path))
-        throw new FileNotFoundException("LicenseManager.exe");
-
+        throw new FileNotFoundException("Xtensive.Licensing.Manager.exe");
       if (!Environment.UserInteractive || Environment.OSVersion.Platform!=PlatformID.Win32NT || path.IsNullOrEmpty())
         return;
       var startInfo = new ProcessStartInfo(path) {
         UseShellExecute = false
       };
-      ErrorLog.Debug("Starting program...");
-      Console.Out.WriteLine("IDDQD");
       Process.Start(startInfo);
-      ErrorLog.Debug("Program Started...");
       using (var client = new PipeClient())
         client.SendLicenseInfo(licenseInfo);
     }
