23da48e197e707d84c8eaa21b7e20c849c8982b6
Alex Yakunin <alex.yakunin@gmail.com>
2010-05-14 19:52:18 +0600

    Common\FileVersionInfo.tt.original update.

--- Xtensive.Modelling/Xtensive.Modelling/Properties/FileVersionInfo.cs
+++ Xtensive.Modelling/Xtensive.Modelling/Properties/FileVersionInfo.cs
@@ -7,4 +7,4 @@
 using System.Reflection;
 
 
-[assembly: AssemblyFileVersion("1.0.4948.12896")]
+[assembly: AssemblyFileVersion("1.0.4979.60809")]
--- Xtensive.Sql/Xtensive.Sql.Oracle/Properties/FileVersionInfo.cs
+++ Xtensive.Sql/Xtensive.Sql.Oracle/Properties/FileVersionInfo.cs
@@ -7,4 +7,4 @@
 using System.Reflection;
 
 
-[assembly: AssemblyFileVersion("1.0.4948.12896")]
+[assembly: AssemblyFileVersion("1.0.4950.52103")]
