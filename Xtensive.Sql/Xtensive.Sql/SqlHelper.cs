0f893d7f3439a0c02cf7cdd06de20744113ce205
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2010-06-04 13:51:22 +0600

    - Fixed issue 684

--- Xtensive.Sql/Xtensive.Sql/SqlHelper.cs
+++ Xtensive.Sql/Xtensive.Sql/SqlHelper.cs
@@ -171,6 +171,62 @@ namespace Xtensive.Sql
       return new Guid(value);
     }
 
+    /// <summary>
+    /// Converts the specified <see cref="TimeSpan"/> to string using the specified format string.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="format">The format string.</param>
+    /// <returns>Formatted representation of the <paramref name="value"/>.</returns>
+    /// <remarks>
+    /// Format string can contain any of these placeholders:
+    /// <list type="table">
+    /// <item><term>{0}</term><description>negative sign, if argument represents a negative <see cref="TimeSpan"/>; <see cref="string.Empty"/>, otherwise.</description></item>
+    /// <item><term>{1}</term><description>absolute value of <see cref="TimeSpan.Days"/> property.</description></item>
+    /// <item><term>{2}</term><description>absolute value of <see cref="TimeSpan.Hours"/> property.</description></item>
+    /// <item><term>{3}</term><description>absolute value of <see cref="TimeSpan.Minutes"/> property.</description></item>
+    /// <item><term>{4}</term><description>absolute value of <see cref="TimeSpan.Seconds"/> property.</description></item>
+    /// <item><term>{5}</term><description>absolute value of <see cref="TimeSpan.Milliseconds"/> property.</description></item>
+    /// </list>
+    /// </remarks>
+    public static string TimeSpanToString(TimeSpan value, string format)
+    {
+      int days = value.Days;
+      int hours = value.Hours;
+      int minutes = value.Minutes;
+      int seconds = value.Seconds;
+      int milliseconds = value.Milliseconds;
+
+      bool negative = false;
+
+      if (days < 0) {
+        days = -days;
+        negative = true;
+      }
+
+      if (hours < 0) {
+        hours = -hours;
+        negative = true;
+      }
+
+      if (minutes < 0) {
+        minutes = -minutes;
+        negative = true;
+      }
+
+      if (seconds < 0) {
+        seconds = -seconds;
+        negative = true;
+      }
+
+      if (milliseconds < 0) {
+        milliseconds = -milliseconds;
+        negative = true;
+      }
+
+      return String.Format(format, negative ? "-" : string.Empty,
+          days, hours, minutes, seconds, milliseconds);
+    }
+
     public static SqlExpression GenericPad(SqlFunctionCall node)
     {
       string paddingFunction;
