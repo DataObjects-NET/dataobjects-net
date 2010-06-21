fc3ca53dcb52e16665deae64cbb7c0aff78d97e7
Alexis Kochetov <alexis.kochetov@gmail.com>
2010-06-21 18:32:13 +0600

    - Minor fix for SQLCompiler

--- Xtensive.Storage/Xtensive.Storage.Providers.Sql/SqlCompiler.Helpers.cs
+++ Xtensive.Storage/Xtensive.Storage.Providers.Sql/SqlCompiler.Helpers.cs
@@ -90,10 +90,8 @@ namespace Xtensive.Storage.Providers.Sql
     public List<SqlExpression> ExtractColumnExpressions(SqlSelect query, CompilableProvider origin)
     {
       var result = new List<SqlExpression>(query.Columns.Count);
-      var shouldUseQueryColumns = false;
-      if (origin.Sources.Any(i => i.Type == ProviderType.Index))
-        shouldUseQueryColumns |= true;
-      shouldUseQueryColumns |= origin.Type.In(ProviderType.Take, ProviderType.Skip, ProviderType.Join, ProviderType.Filter, ProviderType.Index, ProviderType.RowNumber)
+      bool shouldUseQueryColumns = origin.Sources.Any(i => i.Type == ProviderType.Index);
+      shouldUseQueryColumns |= origin.Type.In(ProviderType.Take, ProviderType.Skip, ProviderType.Join, ProviderType.Sort, ProviderType.Filter, ProviderType.Index, ProviderType.RowNumber)
         && query.Columns.Count != query.From.Columns.Count;
       if (query.Columns.Any(IsColumnStub) || query.GroupBy.Count > 0 || shouldUseQueryColumns) {
         foreach (var column in query.Columns) {
