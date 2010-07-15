dbb4d421fda8c53eaa6d188fa4ed40db3d30711c
Alex Yakunin <alex.yakunin@gmail.com>
2010-07-15 15:52:34 +0600

    .tt update.

--- Xtensive.Storage/Xtensive.Storage.Providers.Sql/Properties/FileVersionInfo.cs
+++ Xtensive.Storage/Xtensive.Storage.Providers.Sql/Properties/FileVersionInfo.cs
@@ -8,5 +8,5 @@ using System.Reflection;
 
 
 [assembly: AssemblyCopyright("Copyright © Xtensive LLC 2003-2010")]
-[assembly: AssemblyFileVersion("1.0.5455.13930")]
-[assembly: AssemblyInformationalVersion("Part of DataObjects.Net v4.3.0 RC6, build 5456 for .NET 4.0, build date: 11.07.2010")]
+[assembly: AssemblyFileVersion("1.0.5499.5443")]
+[assembly: AssemblyInformationalVersion("DataObjects.Net v4.3.0 component, build 5507 for .NET 4.0, build date: 07/15/2010")]
