8f091e3b59f28cd9c1f1126087be8a302a46d368
Alexey Gamzov <alex@gamzov.com>
2010-09-06 16:40:13 +0600

    EntitySet comparison in Linq

--- Xtensive.Storage/Xtensive.Storage.Tests/Issues/Issue0751_EntitySetEquals.cs
+++ Xtensive.Storage/Xtensive.Storage.Tests/Issues/Issue0751_EntitySetEquals.cs
@@ -29,6 +29,9 @@ namespace Xtensive.Storage.Tests.Issues.Issue0751_EntitySetEqualsModel
     public int Id { get; private set; }
 
     [Field]
+    public string Name { get; set; }
+
+    [Field]
     [Association(PairTo = "Company")]
     public EntitySet<Person> Persons { get; private set; }
   }
@@ -59,7 +62,7 @@ namespace Xtensive.Storage.Tests.Issues
           var person22 = new Person {Company = company2};
           var person23 = new Person {Company = company2};
           var query = from company in Query.All<Company>()
-                      where company.Persons==null
+                      where null==company.Persons
                       select company;
           var result = query.ToList();
           Assert.AreEqual(0, result.Count);
@@ -96,54 +99,152 @@ namespace Xtensive.Storage.Tests.Issues
     {
       using (Session.Open(Domain)) {
         using (var t = Transaction.Open()) {
-          var company1 = new Company();
-          var company2 = new Company();
-          var person11 = new Person {Company = company1};
-          var person12 = new Person {Company = company1};
-          var person13 = new Person {Company = company1};
-          var person21 = new Person {Company = company2};
-          var person22 = new Person {Company = company2};
-          var person23 = new Person {Company = company2};
+          var company1 = new Company{Name = "Company"};
+          var company2 = new Company{Name = "Company"};
 
-          var query = from c in
-            from company in Query.All<Company>()
-            where company.Persons==null
-            select new {Company1 = company, Company2 = company}
-                      where c.Company1.Persons==c.Company2.Persons
-                      select c;
+          var query =
+            from x in
+              from c1 in Query.All<Company>()
+              join c2 in Query.All<Company>()
+                on c1.Name equals c2.Name
+              select new {Company1 = c1, Company2 = c2}
+            where x.Company1.Persons==x.Company2.Persons
+            select x;
           var result = query.ToList();
           Assert.AreEqual(2, result.Count);
+          foreach (var pair in result)
+            Assert.AreEqual(pair.Company1, pair.Company2);
+          // Rollback
+        }
+      }
+    }
+  
+    [Test]
+    public void NotEqualTest()
+    {
+      using (Session.Open(Domain)) {
+        using (var t = Transaction.Open()) {
+          var company1 = new Company{Name = "Company"};
+          var company2 = new Company{Name = "Company"};
+
+          var query =
+            from x in
+              from c1 in Query.All<Company>()
+              join c2 in Query.All<Company>()
+                on c1.Name equals c2.Name
+              select new {Company1 = c1, Company2 = c2}
+            where x.Company1.Persons!=x.Company2.Persons
+            select x;
+          var result = query.ToList();
+          Assert.AreEqual(2, result.Count);
+          foreach (var pair in result)
+            Assert.AreNotEqual(pair.Company1, pair.Company2);
+          // Rollback
+        }
+      }
+    }
+  
+    [Test]
+    public void ParameterNullTest()
+    {
+      using (Session.Open(Domain)) {
+        using (var t = Transaction.Open()) {
+          var company1 = new Company {Name = "Company"};
+          var company2 = new Company {Name = "Company"};
+          EntitySet<Person> nullEntitySet = null;
+          var query =
+            from c in Query.All<Company>()
+            where c.Persons==nullEntitySet
+            select c;
+          var result = query.ToList();
+          Assert.AreEqual(0, result.Count);
+          // Rollback
+        }
+      }
+    }
+  
+    [Test]
+    public void ClosureEqual1Test()
+    {
+      using (Session.Open(Domain)) {
+        using (var t = Transaction.Open()) {
+          var company1 = new Company {Name = "Company1"};
+          var company2 = new Company {Name = "Company2"};
+          EntitySet<Person> persons = company1.Persons;
+          var query =
+            from c in Query.All<Company>()
+            where c.Persons==persons
+            select c;
+          var result = query.ToList();
+          Assert.AreEqual(1, result.Count);
+          Assert.AreEqual(company1, result.Single());
+          // Rollback
+        }
+      }
+    }
+  
+    [Test]
+    public void ClosureEqual2Test()
+    {
+      using (Session.Open(Domain)) {
+        using (var t = Transaction.Open()) {
+          var company1 = new Company {Name = "Company1"};
+          var company2 = new Company {Name = "Company2"};
+          var query =
+            from c in Query.All<Company>()
+            where c.Persons==company1.Persons
+            select c;
+          var result = query.ToList();
+          Assert.AreEqual(1, result.Count);
+          Assert.AreEqual(company1, result.Single());
           // Rollback
         }
       }
     }
 
     [Test]
-    public void NotEqualTest()
+    public void ClosureNotEqualTest()
     {
       using (Session.Open(Domain)) {
         using (var t = Transaction.Open()) {
-          var company1 = new Company();
-          var company2 = new Company();
-          var person11 = new Person {Company = company1};
-          var person12 = new Person {Company = company1};
-          var person13 = new Person {Company = company1};
-          var person21 = new Person {Company = company2};
-          var person22 = new Person {Company = company2};
-          var person23 = new Person {Company = company2};
-
-          var query = from c in
-            from company in Query.All<Company>()
-            where company.Persons==null
-            select new {Company1 = company, Company2 = company}
-                      where c.Company1.Persons==c.Company2.Persons
-                      select c;
+          var company1 = new Company {Name = "Company1"};
+          var company2 = new Company {Name = "Company2"};
+          EntitySet<Person> persons = company1.Persons;
+          var query =
+            from c in Query.All<Company>()
+            where c.Persons!=persons
+            select c;
           var result = query.ToList();
-          Assert.AreEqual(2, result.Count);
+          Assert.AreEqual(1, result.Count);
+          Assert.AreEqual(company2, result.Single());
           // Rollback
         }
       }
     }
   
+    [Test]
+    public void ParameterEqualTest()
+    {
+      using (Session.Open(Domain)) {
+        using (var t = Transaction.Open()) {
+          var company1 = new Company {Name = "Company1"};
+          var company2 = new Company {Name = "Company2"};
+          EntitySet<Person> persons = company1.Persons;
+          ParameterEqual(company1, persons);
+          // Rollback
+        }
+      }
+    }
+
+    private void ParameterEqual(Company company1, EntitySet<Person> persons)
+    {
+      var query =
+        from c in Query.All<Company>()
+        where c.Persons==persons
+        select c;
+      var result = query.ToList();
+      Assert.AreEqual(1, result.Count);
+      Assert.AreEqual(company1, result.Single());
+    }
   }
 }
