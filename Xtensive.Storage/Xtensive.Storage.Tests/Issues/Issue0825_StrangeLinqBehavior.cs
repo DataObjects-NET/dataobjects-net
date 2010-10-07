f6fee21c0c0e9594e84a0e7c422b7f6e38e557e9
Alexey Gamzov <alex@gamzov.com>
2010-10-08 00:18:45 +0600

    Issue 825 test

---
+++ Xtensive.Storage/Xtensive.Storage.Tests/Issues/Issue0825_StrangeLinqBehavior.cs
@@ -0,0 +1,17 @@
+// Copyright (C) 2010 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Gamzov
+// Created:    2010.10.07
+
+using System;
+using System.Diagnostics;
+
+namespace Xtensive.Storage.Tests.Issues
+{
+  [Serializable]
+  public class Issue0825_StrangeLinqBehavior
+  {
+    
+  }
+}
