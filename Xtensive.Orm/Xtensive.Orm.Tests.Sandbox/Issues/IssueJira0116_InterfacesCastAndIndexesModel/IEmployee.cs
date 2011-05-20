e1a8108310c4c9344c61f6e4a5cbaeb390b4af1b
Dima Maximov <dmitri.maximov@gmail.com>
2011-05-20 21:07:58 +0600

    - DO-115	Duplicate index registering when using interfaces defined in hierarchy - DO-116	Error when casting interface to parent interface in LINQ

---
+++ Xtensive.Orm/Xtensive.Orm.Tests.Sandbox/Issues/IssueJira0116_InterfacesCastAndIndexesModel/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿namespace Xtensive.Orm.Tests.Issues.IssueJira0116_InterfacesCastAndIndexesModel
+{
+    public interface IEmployee : IPerson
+    {
+        [Field]
+        double EmploymentFraction { get; set; }
+
+        [Field]
+        string PensionNumber { get; set; }
+
+        [Field]
+        string InsuranceNumber { get; set; }
+    }
+}
---
+++ Xtensive.Orm/Xtensive.Orm.Tests.Sandbox/Issues/IssueJira0116_InterfacesCastAndIndexesModel/IPerson.cs
@@ -0,0 +1,14 @@
+﻿namespace Xtensive.Orm.Tests.Issues.IssueJira0116_InterfacesCastAndIndexesModel
+{
+    public interface IPerson : IParty
+    {
+        [Field]
+        string FirstName { get; set; }
+
+        [Field]
+        string LastName { get; set; }
+
+        [Field]
+        string PersonNumber { get; set; }
+    }
+}
