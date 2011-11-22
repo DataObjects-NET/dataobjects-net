8d0ef9a14092dea4300b5cfaf5632cabc7d16ea1
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2011-11-22 19:53:45 +0600

    Improved test for DO-221

--- Xtensive.Storage/Xtensive.Storage.Tests.Sandbox/Issues/IssueJira0221_UnableToTranslateAggregate.cs
+++ Xtensive.Storage/Xtensive.Storage.Tests.Sandbox/Issues/IssueJira0221_UnableToTranslateAggregate.cs
@@ -11,6 +11,12 @@ using Xtensive.Storage.Tests.Issues.IssueJira0221_UnableToTranslateAggregateMode
 
 namespace Xtensive.Storage.Tests.Issues.IssueJira0221_UnableToTranslateAggregateModel
 {
+  public class RowTuple<TValue>
+  {
+    public int FakeKey { get; set; }
+    public TValue Value { get; set; }
+  }
+
   [HierarchyRoot]
   public class Zames : Entity
   {
@@ -43,56 +49,202 @@ namespace Xtensive.Storage.Tests.Issues
       return config;
     }
 
-    [Test]
-    public void AggregateOnFakeGroupTestWithProjectionTest()
+    public override void TestFixtureSetUp()
     {
-      using (Session.Open(Domain)) {
-        using (var t = Transaction.Open()) {
-          var result = Query.All<Zames>()
-            .Select(z => new {FakeKey = 0, Info = Query.All<ZamesInfo>().Where(info => info.Owner==z).FirstOrDefault()})
-            .GroupBy(item => item.FakeKey)
-            .Select(grouping => grouping.Min(item => item.Info.Rank))
-            .First();
-        }
-      }
+      base.TestFixtureSetUp();
+
+      CreateSessionAndTransaction();
+
+      new ZamesInfo {Owner = new Zames(), Rank = 1};
+      new ZamesInfo {Owner = new Zames(), Rank = 3};
+    }
+
+    #region Min()
+
+    [Test]
+    public void MinOnGroupingsTest()
+    {
+      var result = GetGroupingsQuery()
+        .Select(grouping => grouping.Min(item => item.Value.Rank))
+        .FirstOrDefault();
+      Assert.AreEqual(1, result);
     }
 
     [Test]
-    public void AggregateOnFakeGroupTest()
+    public void MinOnEntitiesTest()
     {
-      using (Session.Open(Domain)) {
-        using (var t = Transaction.Open()) {
-          var result = Query.All<Zames>()
-            .Select(z => new {FakeKey = 0, Info = Query.All<ZamesInfo>().Where(info => info.Owner==z).Select(info => info.Rank).FirstOrDefault()})
-            .GroupBy(item => item.FakeKey)
-            .Select(grouping => grouping.Min())
-            .First();
-        }
-      }
+      var result = GetEntitiesQuery().Min(info => info.Rank);
+      Assert.AreEqual(1, result);
     }
 
     [Test]
-    public void AggregateOnAllTestWithProjectionTest()
+    public void MinOnValuesTest()
     {
-      using (Session.Open(Domain)) {
-        using (var t = Transaction.Open()) {
-          var result = Query.All<Zames>()
-            .Select(z => Query.All<ZamesInfo>().Where(info => info.Owner==z).FirstOrDefault())
-            .Min(info => info.Rank);
-        }
-      }
+      var result = GetValuesQuery().Min();
+      Assert.AreEqual(1, result);
+    }
+
+    #endregion
+
+    #region Max()
+
+    [Test]
+    public void MaxOnGroupingsTest()
+    {
+      var result = GetGroupingsQuery()
+        .Select(grouping => grouping.Max(item => item.Value.Rank))
+        .FirstOrDefault();
+      Assert.AreEqual(3, result);
     }
 
     [Test]
-    public void AggregateOnAllTest()
+    public void MaxOnEntitiesTest()
     {
-      using (Session.Open(Domain)) {
-        using (var t = Transaction.Open()) {
-          var result = Query.All<Zames>()
-            .Select(z => Query.All<ZamesInfo>().Where(info => info.Owner==z).Select(info => info.Rank).FirstOrDefault())
-            .Min();
-        }
-      }
+      var result = GetEntitiesQuery().Max(info => info.Rank);
+      Assert.AreEqual(3, result);
     }
+
+    [Test]
+    public void MaxOnValuesTest()
+    {
+      var result = GetValuesQuery().Max();
+      Assert.AreEqual(3, result);
+    }
+
+    #endregion
+
+    #region Average()
+
+    [Test]
+    public void AverageOnGroupingsTest()
+    {
+      var result = GetGroupingsQuery()
+        .Select(grouping => grouping.Average(item => item.Value.Rank))
+        .FirstOrDefault();
+      Assert.AreEqual(2, result);
+    }
+
+    [Test]
+    public void AverageOnEntitiesTest()
+    {
+      var result = GetEntitiesQuery().Average(info => info.Rank);
+      Assert.AreEqual(2, result);
+    }
+
+    [Test]
+    public void AverageOnValuesTest()
+    {
+      var result = GetValuesQuery().Average();
+      Assert.AreEqual(2, result);
+    }
+
+    #endregion
+
+    #region Sum()
+
+    [Test]
+    public void SumOnGroupingsTest()
+    {
+      var result = GetGroupingsQuery()
+        .Select(grouping => grouping.Sum(item => item.Value.Rank))
+        .FirstOrDefault();
+      Assert.AreEqual(4, result);
+    }
+
+    [Test]
+    public void SumOnEntitiesTest()
+    {
+      var result = GetEntitiesQuery().Sum(info => info.Rank);
+      Assert.AreEqual(4, result);
+    }
+
+    [Test]
+    public void SumOnValuesTest()
+    {
+      var result = GetValuesQuery().Sum();
+      Assert.AreEqual(4, result);
+    }
+
+    #endregion
+
+    #region Count()
+
+    [Test]
+    public void CountOnGroupingsTest()
+    {
+      var result = GetGroupingsQuery()
+        .Select(grouping => grouping.Count())
+        .FirstOrDefault();
+      Assert.AreEqual(2, result);
+    }
+
+    [Test]
+    public void CountOnEntitiesTest()
+    {
+      var result = GetEntitiesQuery().Count();
+      Assert.AreEqual(2, result);
+    }
+
+    [Test]
+    public void CountOnValuesTest()
+    {
+      var result = GetValuesQuery().Count();
+      Assert.AreEqual(2, result);
+    }
+
+    #endregion
+
+    #region LongCount()
+
+    [Test]
+    public void LongCountOnGroupingsTest()
+    {
+      var result = GetGroupingsQuery()
+        .Select(grouping => grouping.LongCount())
+        .FirstOrDefault();
+      Assert.AreEqual(2, result);
+    }
+
+    [Test]
+    public void LongCountOnEntitiesTest()
+    {
+      var result = GetEntitiesQuery().LongCount();
+      Assert.AreEqual(2, result);
+    }
+
+    [Test]
+    public void LongCountOnValuesTest()
+    {
+      var result = GetValuesQuery().LongCount();
+      Assert.AreEqual(2, result);
+    }
+
+    #endregion
+
+    #region Helpers
+
+    private static IQueryable<int> GetValuesQuery()
+    {
+      return Query.All<Zames>()
+        .Select(z => Query.All<ZamesInfo>().Where(info => info.Owner==z).Select(info => info.Rank).FirstOrDefault());
+    }
+
+    private static IQueryable<ZamesInfo> GetEntitiesQuery()
+    {
+      return Query.All<Zames>()
+        .Select(z => Query.All<ZamesInfo>().Where(info => info.Owner==z).FirstOrDefault());
+    }
+
+    private static IQueryable<IGrouping<int, RowTuple<ZamesInfo>>> GetGroupingsQuery()
+    {
+      return Query.All<Zames>()
+        .Select(z => new RowTuple<ZamesInfo> {
+          FakeKey = 0,
+          Value = Query.All<ZamesInfo>().Where(info => info.Owner==z).FirstOrDefault()
+        })
+        .GroupBy(item => item.FakeKey);
+    }
+
+    #endregion
   }
 }
