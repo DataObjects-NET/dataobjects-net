f8448963ff29da7f23ac017289de190cc3802ac7
Dima Maximov <dmitri.maximov@gmail.com>
2011-12-28 15:15:58 +0600

    - Support for DELETE FROM operation is added to ORM level - Minor fix in comment

--- Xtensive.Core/Xtensive.Core/Linq/Interfaces/IMemberCompilerProvider.cs
+++ Xtensive.Core/Xtensive.Core/Linq/Interfaces/IMemberCompilerProvider.cs
@@ -25,7 +25,7 @@ namespace Xtensive.Core.Linq
     /// Gets untyped compiler for <see paramref="target"/>.
     /// </summary>
     /// <param name="target">Member to get compiler for.</param>
-    /// <returns>Compiler for <see cref="target"/></returns>
+    /// <returns>Compiler for <see paramref="target"/></returns>
     Delegate GetUntypedCompiler(MemberInfo target);
 
     /// <summary>
