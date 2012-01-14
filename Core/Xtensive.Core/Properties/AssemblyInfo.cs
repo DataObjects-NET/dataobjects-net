1d66f969a052361b4cfc5e27e5869dff45e115e4
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-01-14 18:17:18 +0600

    AssemblyInfo.cs clean up

--- Core/Xtensive.Core/Properties/AssemblyInfo.cs
+++ Core/Xtensive.Core/Properties/AssemblyInfo.cs
@@ -2,43 +2,8 @@
 // All rights reserved.
 // For conditions of distribution and use, see license.
 
-using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
-using System.Security.Permissions;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Xtensive.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Xtensive LLC")]
-[assembly: AssemblyProduct("Xtensive.Core")]
-
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b57222bf-029f-44b9-86ae-1ee141b27508")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-#if !NET40
-[assembly: AllowPartiallyTrustedCallers]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution = true)]
-#endif
