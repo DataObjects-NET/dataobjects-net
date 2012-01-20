61696874fe1155199fe5299b0b356de34e2c8c4c
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-01-20 16:50:33 +0600

    RecordQuery: remove unused code

--- Orm/Xtensive.Orm/Orm/Rse/RecordQuery.cs
+++ Orm/Xtensive.Orm/Orm/Rse/RecordQuery.cs
@@ -27,64 +27,12 @@ namespace Xtensive.Orm.Rse
     /// <summary>
     /// Gets the header of the <see cref="RecordQuery"/>.
     /// </summary>
-    public RecordSetHeader Header
-    {
-      get { return Provider.Header; }
-    }
+    public RecordSetHeader Header { get { return Provider.Header; } }
 
     /// <summary>
     /// Gets the provider this <see cref="RecordQuery"/> is bound to.
     /// </summary>
     public CompilableProvider Provider { get; private set; }
-
-    /// <summary>
-    /// Creates <see cref="StoreProvider"/> with specified <see cref="RecordSetHeader"/>
-    /// and name for saved context data .
-    /// </summary>
-    /// <param name="header">The result header.</param>
-    /// <param name="scope">The result scope.</param>
-    /// <param name="name">The result name.</param>
-    public static RecordQuery Load(RecordSetHeader header, TemporaryDataScope scope, string name)
-    {
-      return new StoreProvider(header, scope, name).Result;
-    }
-
-//    #region IEnumerable<...> methods
-//
-//    /// <inheritdoc/>
-//    public IEnumerator<Tuple> GetEnumerator()
-//    {
-//      EnumerationContext ctx;
-//      ExecutableProvider compiled;
-//      using (EnumerationScope.Open()) {
-//        ctx = EnumerationContext.Current;
-//        var compilationContext = CompilationContext.Current;
-//        if (compilationContext==null)
-//          throw new InvalidOperationException();
-//        compiled = compilationContext.Compile(Provider);
-//        if (ctx.CheckOptions(EnumerationContextOptions.GreedyEnumerator)) {
-          // Way 1: preloading all the data into memory and returning it inside this scope
-//          foreach (var tuple in compiled.ToList())
-//            yield return tuple;
-//          yield break;
-//        }
-//      }
-      // Way 2: batched enumeration with periodical context activation
-//      EnumerationScope currentScope = null;
-//      var batched = compiled.Batch(2).ApplyBeforeAndAfter(
-//        () => currentScope = ctx.Activate(), () => currentScope.DisposeSafely());
-//      foreach (var batch in batched)
-//        foreach (var tuple in batch)
-//          yield return tuple;
-//    }
-//
-//    /// <inheritdoc/>
-//    IEnumerator IEnumerable.GetEnumerator()
-//    {
-//      return GetEnumerator();
-//    }
-//
-//    #endregion
 
     /// <inheritdoc/>
     public override string ToString()
