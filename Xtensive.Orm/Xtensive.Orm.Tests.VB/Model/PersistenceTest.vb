dfc5a46cd269e29b8bac66cf5f604fa2246ed80b
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-01-23 13:29:06 +0600

    Fix warnings about obsolete members (for VB project)

--- Xtensive.Orm/Xtensive.Orm.Tests.VB/Model/PersistenceTest.vb
+++ Xtensive.Orm/Xtensive.Orm.Tests.VB/Model/PersistenceTest.vb
@@ -18,19 +18,18 @@ Namespace Linq
 
     <Test()>
     Public Sub MainTest()
-      Using Session as Session = Session.Open(Domain)
-        Using Scope as TransactionScope = Transaction.Open (Session)
-          Dim Author = New Author With {.Name = "Vasya"}
-          Dim Book = New Book With {.Name = "The Book", .Author = Author}
-          Assert.AreEqual(1, Author.Books.Count)
-          Dim result = From c In Query.All(Of Author)
-                       Select c
+      Using session As Session = Domain.OpenSession()
+        Using scope As TransactionScope = session.OpenTransaction()
+          Dim author = New Author With {.Name = "Vasya"}
+          Dim book = New Book With {.Name = "The Book", .Author = author}
+          Assert.AreEqual(1, author.Books.Count)
+          Dim result = From c In Query.All (Of Author)()
+                Select c
           Dim list = result.ToList()
           Dim loaded = result.Single()
           Assert.AreEqual("Vasya", loaded.Name)
         End Using
       End Using
     End Sub
-
   End Class
 End Namespace
