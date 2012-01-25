32d0606f7f2ebfa77056cc851d46e37c288963d0
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-01-25 12:43:13 +0600

    Test case for DO-213 is heavily improved

--- Xtensive.Storage/Xtensive.Storage.Tests.Sandbox/Issues/IssueJira0213_GroupByNewDate.cs
+++ Xtensive.Storage/Xtensive.Storage.Tests.Sandbox/Issues/IssueJira0213_GroupByNewDate.cs
@@ -7,6 +7,7 @@
 using System;
 using System.Linq;
 using NUnit.Framework;
+using Xtensive.Storage.Configuration;
 using Xtensive.Storage.Tests.Issues.IssueJira0213_GroupByNewDateModel;
 
 namespace Xtensive.Storage.Tests.Issues.IssueJira0213_GroupByNewDateModel
@@ -19,39 +20,103 @@ namespace Xtensive.Storage.Tests.Issues.IssueJira0213_GroupByNewDateModel
 
     [Field]
     public DateTime Date { get; set; }
-  }
 
+    public override string ToString()
+    {
+      return Date.ToString("yyyy.MM.dd");
+    }
+  }
 }
 
 namespace Xtensive.Storage.Tests.Issues
 {
   public class IssueJira0213_GroupByNewDate : AutoBuildTest
   {
-
-    protected override global::Xtensive.Storage.Configuration.DomainConfiguration BuildConfiguration()
+    protected override DomainConfiguration BuildConfiguration()
     {
       var configuration = base.BuildConfiguration();
       configuration.Types.Register(typeof (EntityWithDate).Assembly, typeof (EntityWithDate).Namespace);
       return configuration;
     }
 
+    protected override void PopulateData()
+    {
+      using (Session.Open(Domain))
+      using (var tx = Transaction.Open()) {
+        var now = DateTime.Now;
+        for (int i = 1; i <= 10; i++)
+          new EntityWithDate {Date = new DateTime(now.Year, now.Month, i)};
+        tx.Complete();
+      }
+    }
+
     [Test]
-    public void MainTest()
+    public void GroupByNewDateDirectly()
     {
       using (Session.Open(Domain))
       using (Transaction.Open()) {
-        var now = DateTime.Now;
-        for (int i = 0; i < 50; i++)
-          new EntityWithDate {Date = new DateTime(now.Year, now.Month, now.Day, 12, i, 0)};
-
-        var query =
+        var q =
           from i in Query.All<EntityWithDate>()
-          let startMonth = new DateTime(i.Date.Year, i.Date.Month, 1)
-          group i by startMonth
-          into g
+          group i by new DateTime(i.Date.Year, i.Date.Month, 1) into g
           select g;
+        Assert.That(q.Count(), Is.EqualTo(1));
+      }
+    }
 
-        Assert.That(query.Count(), Is.EqualTo(1));
+    [Test]
+    public void GroupByNewDateWithIntermediateProjection()
+    {
+      using (Session.Open(Domain))
+      using (Transaction.Open()) {
+        var q =
+          from i in Query.All<EntityWithDate>()
+          let x = new DateTime(i.Date.Year, i.Date.Month, 1)
+          group i by x into g
+          select g;
+        Assert.That(q.Count(), Is.EqualTo(1));
+      }
+    }
+
+    [Test]
+    public void OrderByNewDateWithIntermediateProjection()
+    {
+      using (Session.Open(Domain))
+      using (Transaction.Open()) {
+
+        var expected = Query.All<EntityWithDate>()
+          .OrderBy(e => e.Date)
+          .AsEnumerable()
+          .Reverse()
+          .ToList();
+
+        var q =
+          from i in Query.All<EntityWithDate>()
+          let x = new DateTime(i.Date.Year, i.Date.Month, 11 - i.Date.Day) 
+          orderby x
+          select i;
+
+        Assert.That(q.ToList(), Is.EqualTo(expected));
+      }
+    }
+
+    [Test]
+    public void OrderByNewDateDirectly()
+    {
+      using (Session.Open(Domain))
+      using (Transaction.Open()) {
+
+        var expected = Query.All<EntityWithDate>()
+          .OrderBy(e => e.Date)
+          .AsEnumerable()
+          .Reverse()
+          .ToList();
+
+        var q =
+          from i in Query.All<EntityWithDate>() 
+          orderby new DateTime(i.Date.Year, i.Date.Month, 11 - i.Date.Day)
+          select i;
+
+        Assert.That(q.ToList(), Is.EqualTo(expected));
       }
     }
   }
