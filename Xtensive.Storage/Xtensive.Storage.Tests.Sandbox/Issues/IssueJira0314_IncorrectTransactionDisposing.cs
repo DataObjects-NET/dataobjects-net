f8644bc3ae305b1caa99f92e2f49143e1fe6c1d7
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-02-09 16:21:59 +0600

    Improved formatting of IssueJira0314_IncorrectTransactionDisposing

--- Xtensive.Storage/Xtensive.Storage.Tests.Sandbox/Issues/IssueJira0314_IncorrectTransactionDisposing.cs
+++ Xtensive.Storage/Xtensive.Storage.Tests.Sandbox/Issues/IssueJira0314_IncorrectTransactionDisposing.cs
@@ -9,78 +9,73 @@ using Xtensive.Storage.Tests.Issues.IssueJira0314_IncorrectTransactionDisposingM
 
 namespace Xtensive.Storage.Tests.Issues.IssueJira0314_IncorrectTransactionDisposingModel
 {
-    [HierarchyRoot]
-    public sealed class Entity1 : Entity
-    {
-        [Field]
-        [Key]
-        public int Id { get; private set; }
-    }
+  [HierarchyRoot]
+  public sealed class Entity1 : Entity
+  {
+    [Field]
+    [Key]
+    public int Id { get; private set; }
+  }
 
-    [HierarchyRoot]
-    public sealed class Entity2 : Entity
-    {
-        [Field]
-        [Key]
-        public int Id { get; private set; }
-    }
+  [HierarchyRoot]
+  public sealed class Entity2 : Entity
+  {
+    [Field]
+    [Key]
+    public int Id { get; private set; }
+  }
 }
 
 namespace Xtensive.Storage.Tests.Issues
 {
-    public class IssueJira0314_IncorrectTransactionDisposing : AutoBuildTest
+  public class IssueJira0314_IncorrectTransactionDisposing : AutoBuildTest
+  {
+    protected override DomainConfiguration BuildConfiguration()
     {
-        protected override DomainConfiguration BuildConfiguration()
-        {
-            DomainConfiguration config = base.BuildConfiguration();
-            config.Types.Register(typeof (Entity1).Assembly, typeof (Entity1).Namespace);
-            return config;
-        }
-
-        protected override void PopulateData()
-        {
-            using (Session.Open(Domain))
-            using (Transaction.Open())
-            {
-                new Entity1();
-                new Entity2();
-            }
-        }
-
-        [Test]
-        public void Test()
-        {
-            int i = 0;
-            var wait1 = new ManualResetEventSlim();
-            var wait2 = new ManualResetEventSlim();
-            Parallel.Invoke(
-                () =>
-                    {
-                        using (Session.Open(Domain))
-                        using (Transaction.Open(TransactionOpenMode.New, IsolationLevel.Serializable))
-                        using (Transaction.Open(TransactionOpenMode.New, IsolationLevel.Serializable))
-                        {
-                            i++;
-                            Query.All<Entity1>().Lock(LockMode.Exclusive, LockBehavior.Wait).ToArray();
-                            wait1.Set();
-                            wait2.Wait();
-                            Query.All<Entity2>().Lock(LockMode.Exclusive, LockBehavior.Wait).ToArray();
-                        }
-                    },
-            () =>
-                {
-                    using (Session.Open(Domain))
-                    using (Transaction.Open(TransactionOpenMode.New, IsolationLevel.Serializable))
-                    using (Transaction.Open(TransactionOpenMode.New, IsolationLevel.Serializable))
-                    {
-                        i++;
-                        Query.All<Entity2>().Lock(LockMode.Exclusive, LockBehavior.Wait).ToArray();
-                        wait2.Set();
-                        wait1.Wait();
-                        Query.All<Entity1>().Lock(LockMode.Exclusive, LockBehavior.Wait).ToArray();
-                    }
-                });
-            Assert.That(i, Is.EqualTo(3));
-        }
+      DomainConfiguration config = base.BuildConfiguration();
+      config.Types.Register(typeof (Entity1).Assembly, typeof (Entity1).Namespace);
+      return config;
     }
+
+    protected override void PopulateData()
+    {
+      using (Session.Open(Domain))
+      using (Transaction.Open()) {
+        new Entity1();
+        new Entity2();
+      }
+    }
+
+    [Test]
+    public void Test()
+    {
+      int i = 0;
+      var wait1 = new ManualResetEventSlim();
+      var wait2 = new ManualResetEventSlim();
+      Parallel.Invoke(
+        () => {
+          using (Session.Open(Domain))
+          using (Transaction.Open(TransactionOpenMode.New, IsolationLevel.Serializable))
+          using (Transaction.Open(TransactionOpenMode.New, IsolationLevel.Serializable)) {
+            i++;
+            Query.All<Entity1>().Lock(LockMode.Exclusive, LockBehavior.Wait).ToArray();
+            wait1.Set();
+            wait2.Wait();
+            Query.All<Entity2>().Lock(LockMode.Exclusive, LockBehavior.Wait).ToArray();
+          }
+        },
+        () => {
+          using (Session.Open(Domain))
+          using (Transaction.Open(TransactionOpenMode.New, IsolationLevel.Serializable))
+          using (Transaction.Open(TransactionOpenMode.New, IsolationLevel.Serializable)) {
+            i++;
+            Query.All<Entity2>().Lock(LockMode.Exclusive, LockBehavior.Wait).ToArray();
+            wait2.Set();
+            wait1.Wait();
+            Query.All<Entity1>().Lock(LockMode.Exclusive, LockBehavior.Wait).ToArray();
+          }
+        });
+      Assert.That(i, Is.EqualTo(3));
+    }
+  }
 }
