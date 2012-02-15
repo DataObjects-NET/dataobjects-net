aa017860aff227529e5ad2d8dce8df87d7441bcf
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-02-15 11:57:07 +0600

    Tests: add tests for cycle detection and base tests for multimapping

---
+++ Orm/Xtensive.Orm.Tests.Sandbox/Storage/Multimapping/MultidatabaseTest.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (C) 2012 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Denis Krjuchkov
+// Created:    2012.02.14
+
+using Xtensive.Orm.Providers;
+
+namespace Xtensive.Orm.Tests.Storage.Multimapping
+{
+  public abstract class MultidatabaseTest : MultimappingTest
+  {
+    protected const string DefaultDatabaseName = "default";
+
+    protected override Configuration.DomainConfiguration BuildConfiguration()
+    {
+      var configuration = base.BuildConfiguration();
+      configuration.DefaultDatabase = DefaultDatabaseName;
+      return configuration;
+    }
+
+    protected override void CheckRequirements()
+    {
+      Require.AllFeaturesSupported(ProviderFeatures.Multidatabase);
+    }
+  }
+}
---
+++ Orm/Xtensive.Orm.Tests.Sandbox/Storage/Multimapping/MultischemaTest.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (C) 2012 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Denis Krjuchkov
+// Created:    2012.02.14
+
+using Xtensive.Orm.Providers;
+
+namespace Xtensive.Orm.Tests.Storage.Multimapping
+{
+  public abstract class MultischemaTest : MultimappingTest
+  {
+    protected override void CheckRequirements()
+    {
+      Require.AllFeaturesSupported(ProviderFeatures.Multischema);
+    }
+  }
+}
