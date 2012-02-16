4be0faa03eb7ae34d85330f40b1dc26fe270c0cd
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-02-16 20:23:45 +0600

    Added non-persistent metadata classes, added stub for MetadataAccessor

---
+++ Orm/Xtensive.Orm/Orm/Metadata/TypeMetadata.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (C) 2012 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Denis Krjuchkov
+// Created:    2012.02.16
+
+using Xtensive.Core;
+using Xtensive.Internals.DocTemplates;
+using Xtensive.Reflection;
+
+namespace Xtensive.Orm.Metadata
+{
+  /// <summary>
+  /// Non-persistent type metadata.
+  /// </summary>
+  public class TypeMetadata
+  {
+    /// <summary>
+    /// Gets the type identifier.
+    /// </summary>
+    public int Id { get; private set; }
+
+    /// <summary>
+    /// Gets the full type name.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+      return string.Format(Strings.MetadataTypeFormat, Name, Id);
+    }
+
+    // Constructors
+
+    /// <summary>
+    ///   <see cref="ClassDocTemplate.Ctor" copy="true"/>
+    /// </summary>
+    /// <param name="id">The type identifier.</param>
+    /// <param name="name">The name of the type.</param>
+    public TypeMetadata(int id, string name)
+    {
+      ArgumentValidator.EnsureArgumentNotNull(name, "name");
+
+      Id = id;
+      Name = name;
+    }
+
+    /// <summary>
+    ///   <see cref="ClassDocTemplate.Ctor" copy="true"/>
+    /// </summary>
+    /// <param name="id">The type identifier.</param>
+    /// <param name="type">The type.</param>
+    public TypeMetadata(int id, System.Type type)
+    {
+      Id = id;
+      Name = type.GetFullName();
+    }
+  }
+}
---
+++ Orm/Xtensive.Orm/Orm/Providers/Sql/MetadataAccessor.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (C) 2012 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Denis Krjuchkov
+// Created:    2012.02.16
+
+using System.Collections.Generic;
+using Xtensive.Core;
+using Xtensive.Orm.Building;
+using Xtensive.Orm.Metadata;
+
+namespace Xtensive.Orm.Providers.Sql
+{
+  /// <summary>
+  /// <see cref="MetadataAccessor"/> for SQL storages.
+  /// </summary>
+  public class MetadataAccessor : Providers.MetadataAccessor
+  {
+    /// <inheritdoc/>
+    public override IEnumerable<AssemblyMetadata> GetAssemblies(string database, string schema)
+    {
+      return null;
+    }
+
+    /// <inheritdoc/>
+    public override IEnumerable<TypeMetadata> GetTypes(string database, string schema)
+    {
+      return null;
+    }
+
+    /// <inheritdoc/>
+    public override IEnumerable<ExtensionMetadata> GetExtension(string database, string schema, string name)
+    {
+      return null;
+    }
+
+    private IQueryExecutor GetQueryExecutor()
+    {
+      return BuildingContext.Demand().SystemSessionHandler.GetService<IQueryExecutor>(true);
+    }
+  }
+}
