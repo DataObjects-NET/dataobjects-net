f1c015d4e3c2f528c04864431771b8ffd9256386
Dima Maximov <dmitri.maximov@gmail.com>
2012-03-05 17:43:16 +0600

    Resolve Issue DO-355 : Fix length of columns in metadata tables in runtime

---
+++ Orm/Xtensive.Orm/Orm/Metadata/ConfigurationModule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xtensive.Orm.Building;
+using Xtensive.Orm.Building.Definitions;
+
+namespace Xtensive.Orm.Metadata
+{
+  public class ConfigurationModule : IModule
+  {
+    public void OnBuilt(Domain domain)
+    {
+    }
+
+    public void OnDefinitionsBuilt(BuildingContext context, DomainModelDef model)
+    {
+      if (context.Configuration.ConnectionInfo.Provider.ToLower() != WellKnown.Provider.MySql)
+        return;
+
+      Log.Info("Applying changes to Metadata-related types for MySQL");
+
+      // Fixing length of Assembly.Name field
+      TypeDef type = model.Types.TryGetValue(typeof(Assembly));
+      FieldDef field;
+      if (type != null && type.Fields.TryGetValue("Name", out field))
+        field.Length = 255;
+      
+      // Fixing length of Extension.Name field
+      type = model.Types.TryGetValue(typeof(Extension));
+      if (type != null && type.Fields.TryGetValue("Name", out field))
+        field.Length = 255;
+
+      // Removing index on Type.Name field
+      type = model.Types.TryGetValue(typeof(Type));
+      if (type != null && type.Indexes.Count > 0) {
+        var indexes = type.Indexes.Where(i => i.KeyFields.ContainsKey("Name")).ToList();
+        foreach (var index in indexes)
+          type.Indexes.Remove(index);
+      }
+    }
+  }
+}
