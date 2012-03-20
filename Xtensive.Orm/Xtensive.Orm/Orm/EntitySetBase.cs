4a35ce005ab2e82b92f1f885e124cb64e54c120d
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-03-20 12:08:27 +0600

    EntitySet: fix memory leak in GetItemCountQueryDelegate

--- Xtensive.Orm/Xtensive.Orm/Orm/EntitySetBase.cs
+++ Xtensive.Orm/Xtensive.Orm/Orm/EntitySetBase.cs
@@ -879,9 +879,10 @@ namespace Xtensive.Orm
 
       bool foundInDatabase;
       using (new ParameterContext().Activate()) {
-        keyParameter.Value = GetEntitySetTypeState().SeekTransform
+        var entitySetTypeState = GetEntitySetTypeState();
+        keyParameter.Value = entitySetTypeState.SeekTransform
           .Apply(TupleTransformType.TransformedTuple, Owner.Key.Value, key.Value);
-        foundInDatabase = GetEntitySetTypeState().GetSeekRecordSet(Session.Handler).FirstOrDefault()!=null;
+        foundInDatabase = entitySetTypeState.GetSeekRecordSet(Session.Handler).FirstOrDefault()!=null;
       }
       if (foundInDatabase)
         State.Register(key);
