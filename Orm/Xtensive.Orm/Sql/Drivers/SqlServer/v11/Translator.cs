f51fe2388316176553289df59773b3f29479d716
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-04-06 14:34:14 +0600

    Fix database qualified create/drop/alter sequence in SQL Server 2012

--- Orm/Xtensive.Orm/Sql/Drivers/SqlServer/v11/Translator.cs
+++ Orm/Xtensive.Orm/Sql/Drivers/SqlServer/v11/Translator.cs
@@ -5,6 +5,7 @@
 // Created:    2012.04.02
 
 using System;
+using System.Text;
 using Xtensive.Sql.Compiler;
 using Xtensive.Sql.Ddl;
 using Xtensive.Sql.Dml;
@@ -16,7 +17,21 @@ namespace Xtensive.Sql.Drivers.SqlServer.v11
   {
     public override string Translate(SqlCompilerContext context, SqlDropSequence node)
     {
-      return "DROP SEQUENCE " + Translate(context, node.Sequence);
+      return TranslateSequenceStatement(context, node.Sequence, "DROP");
+    }
+
+    public override string Translate(SqlCompilerContext context, SqlAlterSequence node, NodeSection section)
+    {
+      if (section==NodeSection.Entry)
+        return TranslateSequenceStatement(context, node.Sequence, "ALTER");
+      return string.Empty;
+    }
+
+    public override string Translate(SqlCompilerContext context, SqlCreateSequence node, NodeSection section)
+    {
+      if (section==NodeSection.Entry)
+        return TranslateSequenceStatement(context, node.Sequence, "CREATE");
+      return string.Empty;
     }
 
     public override string Translate(SqlCompilerContext context, SequenceDescriptor descriptor, SequenceDescriptorSection section)
@@ -42,6 +57,20 @@ namespace Xtensive.Sql.Drivers.SqlServer.v11
         default:
           return base.Translate(context, node, section);
       }
+    }
+
+    private string TranslateSequenceStatement(SqlCompilerContext context, Sequence sequence, string action)
+    {
+      // SQL Server does not support database qualification in create/drop/alter sequence.
+      // We add explicit "use" statement before such statements.
+      // This changes current database as side effect,
+      // but it's OK because we always use database qualified objects in all other statements.
+
+      var result = new StringBuilder();
+      if (context.HasOptions(SqlCompilerNamingOptions.DatabaseQualifiedObjects))
+        result.AppendFormat("USE {0}; ", QuoteIdentifier(sequence.Schema.Catalog.Name));
+      result.AppendFormat("{0} SEQUENCE {1}", action, QuoteIdentifier(sequence.Schema.Name, sequence.Name));
+      return result.ToString();
     }
 
     public Translator(SqlDriver driver)
