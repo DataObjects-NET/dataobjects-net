902ea6e66b032c8deac5b6c0347e90f3a6279a87
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-05-17 17:15:57 +0600

    Implement per-session key caching for SQLite

--- Orm/Xtensive.Orm/Orm/Internals/KeyGenerators/StorageSequentalGenerator.cs
+++ Orm/Xtensive.Orm/Orm/Internals/KeyGenerators/StorageSequentalGenerator.cs
@@ -4,9 +4,7 @@
 // Created by: Denis Krjuchkov
 // Created:    2012.03.08
 
-using System;
-using System.Collections.Generic;
-using Xtensive.Arithmetic;
+using Xtensive.Core;
 using Xtensive.Orm.Model;
 using Xtensive.Orm.Providers;
 using Xtensive.Tuples;
@@ -16,43 +14,28 @@ namespace Xtensive.Orm.Internals.KeyGenerators
 {
   public sealed class StorageSequentalGenerator<TValue> : IKeyGenerator
   {
-    private static readonly Arithmetic<TValue> ValueArithmetic = Arithmetic<TValue>.Default;
-
-    private readonly object syncRoot = new object(); // guard access to cachedValues
-
-    private Domain domain;
     private Tuple prototype;
-    private Queue<TValue> cachedValues;
+    private ICachingSequenceProvider<TValue> sequenceProvider;
 
     public void Initialize(Domain ownerDomain, TupleDescriptor keyTupleDescriptor)
     {
-      domain = ownerDomain;
       prototype = Tuple.Create(keyTupleDescriptor);
-      cachedValues = new Queue<TValue>();
+
+      var accessor = ownerDomain.Services.Demand<IStorageSequenceAccessor>();
+
+      if (ownerDomain.StorageProviderInfo.Supports(ProviderFeatures.TransactionalKeyGenerators))
+        sequenceProvider = new SessionCachingSequenceProvider<TValue>(accessor);
+      else
+        sequenceProvider = new DomainCachingSequenceProvider<TValue>(accessor);
     }
 
     public Tuple GenerateKey(KeyInfo keyInfo, Session session)
     {
-      TValue keyValue;
-      lock (syncRoot) {
-        if (cachedValues.Count==0)
-          CacheValues(keyInfo.Sequence, session);
-        keyValue = cachedValues.Dequeue();
-      }
+      var sequence = sequenceProvider.GetSequence(session);
+      var keyValue = sequence.GetNextValue(keyInfo.Sequence, session);
       var keyTuple = prototype.CreateNew();
       keyTuple.SetValue(0, keyValue);
       return keyTuple;
-    }
-
-    private void CacheValues(SequenceInfo sequenceInfo, Session session)
-    {
-      var accessor = domain.Services.Get<IStorageSequenceAccessor>();
-      var values = accessor.NextBulk(sequenceInfo, session);
-      var current = (TValue) Convert.ChangeType(values.Offset, typeof (TValue));
-      for (int i = 0; i < values.Length; i++) {
-        cachedValues.Enqueue(current);
-        current = ValueArithmetic.Add(current, ValueArithmetic.One);
-      }
     }
   }
 }
