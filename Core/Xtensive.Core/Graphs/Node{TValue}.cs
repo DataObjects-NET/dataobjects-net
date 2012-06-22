6d055ea2ce0ecb3e46255ee3a92211f3f2829782
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-06-22 19:27:26 +0600

    Core/Graphs: fix formatting and xmldocs

--- Core/Xtensive.Core/Graphs/Node{TValue}.cs
+++ Core/Xtensive.Core/Graphs/Node{TValue}.cs
@@ -5,47 +5,44 @@
 // Created:    2012.02.22
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Xtensive.Internals.DocTemplates;
 
 namespace Xtensive.Graphs
 {
+  /// <summary>
+  /// Graph node with value. 
+  /// </summary>
+  /// <typeparam name="TValue">Node value.</typeparam>
+  [Serializable]
+  public class Node<TValue> : Node
+  {
     /// <summary>
-    /// Graph node with value. 
+    /// Node value.
     /// </summary>
-    /// <typeparam name="TValue">Node value.</typeparam>
-    [Serializable]
-    public class Node<TValue> : Node
+    public TValue Value { get; set; }
+
+    /// <summary>
+    /// Gets node value.
+    /// </summary>
+    public override object UntypedValue { get { return Value; } }
+
+
+    // Constructors
+
+    /// <summary>
+    /// <see cref="ClassDocTemplate.Ctor" copy="true"/>
+    /// </summary>
+    public Node()
     {
-        /// <summary>
-        /// Node value.
-        /// </summary>
-        public TValue Value { get; set; }
-
-        /// <summary>
-        /// Gets node value.
-        /// </summary>
-        public override object UntypedValue { get { return Value; } }
-
-
-        // Constructors
-
-        /// <summary>
-        /// <see cref="ClassDocTemplate.Ctor" copy="true"/>
-        /// </summary>
-        public Node()
-        {
-        }
-
-        /// <summary>
-        /// <see cref="ClassDocTemplate.Ctor" copy="true"/>
-        /// </summary>
-        /// <param name="value">Node value.</param>
-        public Node(TValue value)
-        {
-            Value = value;
-        }
     }
+
+    /// <summary>
+    /// <see cref="ClassDocTemplate.Ctor" copy="true"/>
+    /// </summary>
+    /// <param name="value">Node value.</param>
+    public Node(TValue value)
+    {
+      Value = value;
+    }
+  }
 }
