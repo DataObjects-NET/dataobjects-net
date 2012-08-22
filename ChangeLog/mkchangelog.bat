a8faa3c4d0cab2695e579f0586bbe34996b67f71
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-08-22 13:43:09 +0600

    Generate ReleaseNotes.txt as part of build process

--- ChangeLog/mkchangelog.bat
+++ ChangeLog/mkchangelog.bat
@@ -4,10 +4,12 @@
 
 @set input_dir=%cd%\ChangeLog
 @set output_dir=%cd%\_Build\ProductInfo
-@set output_file=%output_dir%\ChangeLog.txt
+@set change_log_file=%output_dir%\ChangeLog.txt
+@set notes_file=%output_dir%\ReleaseNotes.txt
 
 @if not exist "%output_dir%" mkdir "%output_dir%"
-@if exist "%output_file%" del "%output_file%"
+@if exist "%change_log_file%" del "%change_log_file%"
+@if exist "%notes_file%" del "%notes_file%"
 
 @cd "%input_dir%"
 
@@ -15,12 +17,13 @@
 @goto :end
 
 :process
+@if not exist "%notes_file%" copy "%1" "%notes_file%"
 @set version=%~n1:
 @set version=%version:_Z=%
 @set version=%version:_= %
-@echo Changes in %version%>> "%output_file%"
-@echo.>> "%output_file%"
-@for /F "usebackq delims==" %%I in ("%1") do @echo %%I>> "%output_file%"
-@echo.>> "%output_file%"
+@echo Changes in %version%>> "%change_log_file%"
+@echo.>> "%change_log_file%"
+@for /F "usebackq delims==" %%I in ("%1") do @echo %%I>> "%change_log_file%"
+@echo.>> "%change_log_file%"
 
 :end
