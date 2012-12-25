b23f103f1bfdcf1c831d0c7820e442258738e143
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-12-25 14:52:25 +0600

    IncludeFilterMappingGatherer: fix formatting

--- Orm/Xtensive.Orm/Orm/Linq/Expressions/Visitors/IncludeFilterMappingGatherer.cs
+++ Orm/Xtensive.Orm/Orm/Linq/Expressions/Visitors/IncludeFilterMappingGatherer.cs
@@ -5,37 +5,35 @@
 // Created:    2009.11.16
 
 using System;
-using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using Xtensive.Core;
-using Xtensive.Linq;
 
 namespace Xtensive.Orm.Linq.Expressions.Visitors
 {
-  internal class IncludeFilterMappingGatherer :ExtendedExpressionVisitor
+  internal class IncludeFilterMappingGatherer : ExtendedExpressionVisitor
   {
-    private int tupleIndex = -1;
-    private int providerIndex = -1;
     private readonly int[] mapping;
     private readonly Expression tupleExpression;
 
+    private int tupleIndex = -1;
+    private int providerIndex = -1;
 
     public static int[] Visit(Expression tupleExpression, int length, Expression expression)
     {
       var mapping = Enumerable.Repeat(-1, length).ToArray();
       new IncludeFilterMappingGatherer(tupleExpression, mapping).Visit(expression);
-      if (mapping.Contains(-1)) 
+      if (mapping.Contains(-1))
         throw new InvalidOperationException();
       return mapping;
     }
 
     protected override Expression VisitBinary(BinaryExpression b)
     {
-      if (tupleIndex!=-1 || providerIndex!=-1) 
+      if (tupleIndex!=-1 || providerIndex!=-1)
         throw new InvalidOperationException();
       var result = base.VisitBinary(b);
-      if (tupleIndex*providerIndex<0) 
+      if (tupleIndex * providerIndex < 0)
         throw new InvalidOperationException();
       if (tupleIndex >= 0)
         mapping[tupleIndex] = providerIndex;
@@ -46,9 +44,8 @@ namespace Xtensive.Orm.Linq.Expressions.Visitors
 
     protected override Expression VisitMethodCall(MethodCallExpression mc)
     {
-      if (mc.Method.IsGenericMethod 
-        && mc.Method.GetGenericMethodDefinition()==WellKnownMembers.Tuple.GenericAccessor 
-        && mc.Arguments[0].StripCasts().NodeType==ExpressionType.Constant) {
+      var isTupleAccess = mc.Method.IsGenericMethod && mc.Method.GetGenericMethodDefinition()==WellKnownMembers.Tuple.GenericAccessor;
+      if (isTupleAccess && mc.Arguments[0].StripCasts().NodeType==ExpressionType.Constant) {
         if (mc.Object==tupleExpression)
           tupleIndex = (int) ((ConstantExpression) mc.Arguments[0].StripCasts()).Value;
         else
