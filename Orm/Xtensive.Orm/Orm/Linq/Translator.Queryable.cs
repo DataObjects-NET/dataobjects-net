21c10733e29eca19f745fa7e45050a52b154c067
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2012-12-25 14:02:33 +0600

    Extract VisitExistsAsInclude method for more readability

--- Orm/Xtensive.Orm/Orm/Linq/Translator.Queryable.cs
+++ Orm/Xtensive.Orm/Orm/Linq/Translator.Queryable.cs
@@ -1163,52 +1163,8 @@ namespace Xtensive.Orm.Linq
 
     private Expression VisitExists(Expression source, LambdaExpression predicate, bool notExists)
     {
-      if (source.IsLocalCollection(context) && predicate!=null && predicate.Body.NodeType==ExpressionType.Equal) {
-        var parameter = predicate.Parameters[0];
-        ProjectionExpression visitedSource;
-        using (state.CreateScope()) {
-          state.IncludeAlgorithm = IncludeAlgorithm.Auto;
-          visitedSource = VisitSequence(source);
-        }
-
-        ParameterExpression outerParameter = state.Parameters[0];
-        var outerResult = context.Bindings[outerParameter];
-
-
-        using (context.Bindings.Add(parameter, visitedSource))
-        using (state.CreateScope()) {
-          state.CalculateExpressions = false;
-          state.CurrentLambda = predicate;
-          ItemProjectorExpression predicateExpression;
-          using (state.CreateScope()) {
-            state.IncludeAlgorithm = IncludeAlgorithm.Auto;
-            predicateExpression = (ItemProjectorExpression) VisitLambda(predicate);
-          }
-          var predicateLambda = predicateExpression.ToLambda(context);
-
-          var parameterSource = context.Bindings[parameter];
-          var parameterRecordSet = parameterSource.ItemProjector.DataSource;
-          var rawProvider = ((RawProvider) ((StoreProvider) visitedSource.ItemProjector.DataSource).Source);
-          var tuples = rawProvider.Source;
-          var mapping = IncludeFilterMappingGatherer.Visit(predicateLambda.Parameters[0], rawProvider.Header.Length, predicateLambda.Body);
-
-          var columnIndex = outerResult.ItemProjector.DataSource.Header.Length;
-          var newDataSource = outerResult.ItemProjector.DataSource
-            .Include(state.IncludeAlgorithm, true, tuples, context.GetNextAlias(), mapping);
-
-          var newItemProjector = outerResult.ItemProjector.Remap(newDataSource, 0);
-          var newOuterResult = new ProjectionExpression(
-            outerResult.Type, 
-            newItemProjector, 
-            outerResult.TupleParameterBindings, 
-            outerResult.ResultType);
-          context.Bindings.ReplaceBound(outerParameter, newOuterResult);
-          Expression columnExpression = ColumnExpression.Create(typeof (bool), columnIndex);
-          if (notExists)
-            columnExpression = Expression.Not(columnExpression);
-          return columnExpression;
-        }
-      }
+      if (source.IsLocalCollection(context) && predicate!=null && predicate.Body.NodeType==ExpressionType.Equal)
+        return VisitExistsAsInclude(source, predicate, notExists);
 
       ProjectionExpression subquery;
       using (state.CreateScope()) {
@@ -1227,6 +1183,57 @@ namespace Xtensive.Orm.Linq
       if (notExists)
         filter = Expression.Not(filter);
       return filter;
+    }
+
+    private Expression VisitExistsAsInclude(Expression source, LambdaExpression predicate, bool notExists)
+    {
+      // Translate localCollection.Any(item => item==outer) as outer.In(localCollection)
+
+      var parameter = predicate.Parameters[0];
+      ProjectionExpression visitedSource;
+      using (state.CreateScope()) {
+        state.IncludeAlgorithm = IncludeAlgorithm.Auto;
+        visitedSource = VisitSequence(source);
+      }
+
+      ParameterExpression outerParameter = state.Parameters[0];
+      var outerResult = context.Bindings[outerParameter];
+
+
+      using (context.Bindings.Add(parameter, visitedSource))
+      using (state.CreateScope()) {
+        state.CalculateExpressions = false;
+        state.CurrentLambda = predicate;
+        ItemProjectorExpression predicateExpression;
+        using (state.CreateScope()) {
+          state.IncludeAlgorithm = IncludeAlgorithm.Auto;
+          state.CalculateExpressions = true;
+          predicateExpression = (ItemProjectorExpression) VisitLambda(predicate);
+        }
+        var predicateLambda = predicateExpression.ToLambda(context);
+
+        var parameterSource = context.Bindings[parameter];
+        var parameterRecordSet = parameterSource.ItemProjector.DataSource;
+        var rawProvider = ((RawProvider) ((StoreProvider) visitedSource.ItemProjector.DataSource).Source);
+        var tuples = rawProvider.Source;
+        var mapping = IncludeFilterMappingGatherer.Visit(predicateLambda.Parameters[0], rawProvider.Header.Length, predicateLambda.Body);
+
+        var columnIndex = outerResult.ItemProjector.DataSource.Header.Length;
+        var newDataSource = outerResult.ItemProjector.DataSource
+          .Include(state.IncludeAlgorithm, true, tuples, context.GetNextAlias(), mapping);
+
+        var newItemProjector = outerResult.ItemProjector.Remap(newDataSource, 0);
+        var newOuterResult = new ProjectionExpression(
+          outerResult.Type,
+          newItemProjector,
+          outerResult.TupleParameterBindings,
+          outerResult.ResultType);
+        context.Bindings.ReplaceBound(outerParameter, newOuterResult);
+        Expression columnExpression = ColumnExpression.Create(typeof (bool), columnIndex);
+        if (notExists)
+          columnExpression = Expression.Not(columnExpression);
+        return columnExpression;
+      }
     }
 
     private Expression VisitIn(MethodCallExpression mc)
