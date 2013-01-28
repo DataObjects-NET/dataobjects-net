4a16bdfddabf379ccd081515a1105b6f833ce10b
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2013-01-28 15:16:11 +0600

    Merge TupleFieldAccessor into PackedFieldAccessor

--- Core/Xtensive.Core/Tuples/Packed/PackedFieldAccessor.cs
+++ Core/Xtensive.Core/Tuples/Packed/PackedFieldAccessor.cs
@@ -8,8 +8,50 @@ using System;
 
 namespace Xtensive.Tuples.Packed
 {
-  internal abstract class PackedFieldAccessor : TupleFieldAccessor
+  internal abstract class PackedFieldAccessor
   {
+    /// <summary>
+    /// Getter delegate.
+    /// </summary>
+    protected Delegate Getter;
+
+    /// <summary>
+    /// Setter delegate.
+    /// </summary>
+    protected Delegate Setter;
+
+    /// <summary>
+    /// Nullable getter delegate.
+    /// </summary>
+    protected Delegate NullableGetter;
+
+    /// <summary>
+    /// Nullable setter delegate.
+    /// </summary>
+    protected Delegate NullableSetter;
+
+    /// <summary>
+    /// Gets setter for the field.
+    /// </summary>
+    /// <typeparam name="T">Field type.</typeparam>
+    /// <param name="isNullable">Flag indicating if field type is nullable.</param>
+    /// <returns><see cref="GetValueDelegate{TValue}"/> for the field.</returns>
+    public GetValueDelegate<T> GetGetter<T>(bool isNullable)
+    {
+      return (isNullable ? NullableGetter : Getter) as GetValueDelegate<T>;
+    }
+
+    /// <summary>
+    /// Gets setter for the field.
+    /// </summary>
+    /// <typeparam name="T">Field type.</typeparam>
+    /// <param name="isNullable">Flag indicating if field type is nullable.</param>
+    /// <returns><see cref="SetValueDelegate{TValue}"/> for the field.</returns>
+    public SetValueDelegate<T> GetSetter<T>(bool isNullable)
+    {
+      return (isNullable ? NullableSetter : Setter) as SetValueDelegate<T>;
+    }
+
     public abstract object GetUntypedValue(PackedTuple tuple, PackedFieldDescriptor descriptor);
 
     public abstract void SetUntypedValue(PackedTuple tuple, PackedFieldDescriptor descriptor, object value);
