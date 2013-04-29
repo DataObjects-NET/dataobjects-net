d4bd6839412f794497881e9dc4203e3d8d15915a
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2013-04-29 16:33:28 +0600

    QueryableVisitor: added GetQueryableMethod(), improved implementation

--- Core/Xtensive.Core/Linq/QueryableVisitor.cs
+++ Core/Xtensive.Core/Linq/QueryableVisitor.cs
@@ -21,19 +21,14 @@ namespace Xtensive.Linq
     /// <inheritdoc/>
     protected override Expression VisitMethodCall(MethodCallExpression mc)
     {
-      if (mc.Method.DeclaringType==typeof (Queryable) || mc.Method.DeclaringType==typeof (Enumerable)) {
-        QueryableMethodKind kind;
-        try {
-          kind = (QueryableMethodKind) Enum.Parse(typeof (QueryableMethodKind), mc.Method.Name);
-        }
-        catch(ArgumentException) {
-          return base.VisitMethodCall(mc);
-        }
-        if (mc.Arguments.Count > 0 && mc.Arguments[0].Type == typeof(string))
-          return base.VisitMethodCall(mc);
-        return VisitQueryableMethod(mc, kind);
-      }
-      return base.VisitMethodCall(mc);
+      if (mc.Arguments.Count > 0 && mc.Arguments[0].Type==typeof (string))
+        return base.VisitMethodCall(mc);
+
+      var method = GetQueryableMethod(mc);
+      if (method==null)
+        return base.VisitMethodCall(mc);
+
+      return VisitQueryableMethod(mc, method.Value);
     }
 
     /// <summary>
@@ -42,5 +37,38 @@ namespace Xtensive.Linq
     /// <param name="mc">The method call expression.</param>
     /// <param name="methodKind">Kind of the method.</param>
     protected abstract Expression VisitQueryableMethod(MethodCallExpression mc, QueryableMethodKind methodKind);
+
+    /// <summary>
+    /// Parses <see cref="QueryableMethodKind"/> for the specified expression.
+    /// </summary>
+    /// <param name="call">A call to process.</param>
+    /// <returns><see cref="QueryableMethodKind"/> for the specified expression,
+    /// or null if method is not a LINQ method.</returns>
+    public static QueryableMethodKind? GetQueryableMethod(MethodCallExpression call)
+    {
+      if (call==null)
+        return null;
+      var declaringType = call.Method.DeclaringType;
+      if (declaringType==typeof (Queryable) || declaringType==typeof (Enumerable))
+        return ParseQueryableMethodKind(call.Method.Name);
+      return null;
+    }
+
+    private static QueryableMethodKind? ParseQueryableMethodKind(string methodName)
+    {
+#if NET40
+      QueryableMethodKind result;
+      if (Enum.TryParse(methodName, out result))
+        return result;
+      return null;
+#else
+      try {
+        return (QueryableMethodKind) Enum.Parse(typeof (QueryableMethodKind), methodName);
+      }
+      catch {
+        return null;
+      }
+#endif
+    }
   }
 }
