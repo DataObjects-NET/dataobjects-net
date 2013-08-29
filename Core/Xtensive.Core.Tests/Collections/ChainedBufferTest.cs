a5f6c90cdcf8100c9c9a0f460ad44388f282b152
Alena Mikshina <info@x-tensive.com>
2013-08-29 16:01:48 +0600

    Implemented ChainedBuffer

---
+++ Core/Xtensive.Core.Tests/Collections/ChainedBufferTest.cs
@@ -0,0 +1,214 @@
+﻿// Copyright (C) 2013 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alena Mikshina
+// Created:    2013.08.19
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+using Xtensive.Collections;
+
+namespace Xtensive.Tests.Collections
+{
+  [TestFixture]
+  public class ChainedBufferTest
+  {
+    [Test]
+    public void EmptyTest()
+    {
+      var buffer = new ChainedBuffer<int>();
+      Assert.That(buffer.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void AddTest()
+    {
+      int count = 40;
+      int index = 0;
+      var buffer1 = new ChainedBuffer<int>();
+      var buffer2 = new ChainedBuffer<string>();
+
+      for (int i = 0; i < count; i++) {
+        buffer1.Add(i);
+        buffer2.Add(null);
+      }
+        
+      Assert.That(buffer1.Count, Is.EqualTo(count));
+      Assert.That(buffer2.Count, Is.EqualTo(count));
+
+      foreach (var b in buffer1) {
+        Assert.That(buffer1.Contains(index));
+        ++index;
+      }
+
+       foreach (var b in buffer1) {
+        Assert.That(buffer2.Contains(null));
+        ++index;
+      }
+    }
+
+
+    [Test]
+    public void ClearTest()
+    {
+      int count = 40;
+      var buffer1 = new ChainedBuffer<string>();
+      for (int i = 0; i < count; i++)
+        buffer1.Add(i.ToString());
+
+      buffer1.Clear();
+
+      Assert.That(buffer1.Count, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void ContainsTest()
+    {
+      int count = 40;
+      int index = 0;
+
+      var buffer1 = new ChainedBuffer<int>();
+      var buffer2 = new ChainedBuffer<string>();
+
+      for (int i = 0; i < count; i++) {
+        buffer1.Add(i);
+        buffer2.Add(null);
+      }
+
+      foreach (var buf in buffer1) {
+        Assert.That(buffer1.Contains(index));
+        ++index;
+      }
+
+      foreach (var buf in buffer2) {
+        Assert.That(buffer2.Contains(null));
+        ++index;
+      }
+    }
+
+    [Test]
+    public void CopyToTest()
+    {
+      int count = 40;
+      int arrayIndex = 10;
+      int[] intArray = new int[count+arrayIndex];
+      var buffer1 = new ChainedBuffer<int>();
+      bool flag = true;
+
+      for (int i = 0; i < count; i++)
+        buffer1.Add(i);
+      
+      buffer1.CopyTo(intArray, arrayIndex);
+
+      for (int i = 0; i < count; i++)
+        if (intArray[i + arrayIndex] != i)
+          flag = false;
+      
+      Assert.That(buffer1.Count + arrayIndex, Is.EqualTo(intArray.Length));
+      Assert.That(flag);
+    }
+
+    [Test]
+    public void AddRangeTest()
+    {
+      int count = 40;
+      int arrayCount = 10;
+      int index = 0;
+      int[] intArray = new int[arrayCount];
+      var buffer1 = new ChainedBuffer<int>();
+
+      for (int i = 0; i < arrayCount; i++)
+        intArray[i] = i + count;
+      
+      for (int i = 0; i < count; i++)
+        buffer1.Add(i);
+
+      buffer1.AddRange(intArray);
+
+      foreach (var buf in buffer1) {
+        Assert.That(buffer1.Contains(index));
+        ++index;
+      }
+    }
+
+    [Test]
+    public void GetEnumeratorTest()
+    {
+      int count = 40;
+      int index = 0;
+      var buffer1 = new ChainedBuffer<int>();
+
+      for (int i = 0; i < count; i++)
+        buffer1.Add(i);
+
+      Assert.AreEqual(count, buffer1.Count);
+      
+      foreach (var buf in buffer1) {
+        Assert.That(buf, Is.EqualTo(index));
+        ++index;
+      }
+    }
+
+    [Test]
+    public void ConstructorIntArrayTest()
+    {
+      int index = 0;
+      int arrayCount = 40;
+      int[] intArray = new int[arrayCount];
+      
+      for (int i = 0; i < arrayCount; i++)
+        intArray[i] = i;
+
+      var buffer1 = new ChainedBuffer<int>(intArray);
+
+      Assert.AreEqual(arrayCount, buffer1.Count);
+
+      foreach (var buf in buffer1) {
+        Assert.That(buf, Is.EqualTo(index));
+        ++index;
+      }
+    }
+
+    [Test]
+    public void ConstructorIntArrayAndMaxNodeSizeTest()
+    {
+      int index = 0;
+      int arrayCount = 40;
+      int maxNodeSize = 20;
+      int[] intArray = new int[arrayCount];
+
+      for (int i = 0; i < arrayCount; i++)
+        intArray[i] = i;
+
+      var buffer1 = new ChainedBuffer<int>(intArray, maxNodeSize);
+
+      Assert.AreEqual(arrayCount, buffer1.Count);
+
+      foreach (var buf in buffer1) {
+        Assert.That(buf, Is.EqualTo(index));
+        ++index;
+      }
+    }
+
+    [Test]
+    public void ConstructorMaxNodeSizeTest()
+    {
+      int count = 40;
+      int index = 0;
+      int maxNodeSize = 10;
+      var buffer1 = new ChainedBuffer<int>(maxNodeSize);
+
+      for (int i = 0; i < count; i++)
+        buffer1.Add(i);
+
+      Assert.AreEqual(count, buffer1.Count);
+
+      foreach (var buf in buffer1) {
+        Assert.That(buf, Is.EqualTo(index));
+        ++index;
+      }
+    }
+  }
+}
---
+++ Core/Xtensive.Core/Collections/ChainedBuffer.cs
@@ -0,0 +1,184 @@
+﻿// Copyright (C) 2013 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alena Mikshina
+// Created:    2013.08.19
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xtensive.Core;
+using Xtensive.Resources;
+using System.Linq;
+
+namespace Xtensive.Collections
+{
+  /// <summary>
+  /// ChainedBuffer(<see cref="ICollection{T}"/>)
+  /// </summary>
+  /// <typeparam name="T">The type of collection items.</typeparam>
+
+  public class ChainedBuffer<T> : ICollection<T>
+  {
+    private sealed class Node<T>
+    {
+      public readonly T[] Items;
+      public Node<T> Next;
+
+      // Constructors
+
+      public Node(int size)
+      {
+        Items = new T[size];
+      }
+    }
+
+    private static readonly IEqualityComparer<T> Comparer = EqualityComparer<T>.Default; 
+
+    private int totalSize;
+    private int maxNodeSize;
+    private int tailNodeSize;   
+    private Node<T> headNode;
+    private Node<T> tailNode;
+
+    /// <inheritdoc/>
+    public int Count { get { return totalSize; } }
+
+    /// <inheritdoc/>
+    public bool IsReadOnly { get  { return false; } }
+
+    #region GetEnumerator<...> methods
+
+    /// <inheritdoc/>
+    public IEnumerator<T> GetEnumerator()
+    {
+      if (headNode==null)
+        yield break;
+
+      var currentNode = headNode;
+
+      while (currentNode!=tailNode) {
+        foreach (var item in currentNode.Items)
+          yield return item;
+        currentNode = currentNode.Next;
+      }
+
+      for (int i = 0; i < tailNodeSize; ++i)
+        yield return currentNode.Items[i];
+    }
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+
+    #endregion
+
+    #region Modification methods: Add, Clear, Contains, Remove, AddRange
+
+    /// <inheritdoc/>
+    public void Add(T item)
+    {
+      if (headNode==null) {
+        headNode = tailNode = new Node<T>(maxNodeSize);
+        tailNodeSize = 0;
+      }
+      else if (tailNodeSize==maxNodeSize) {
+        var newTailNode = new Node<T>(maxNodeSize);
+        tailNodeSize = 0;
+        tailNode.Next = newTailNode;
+        tailNode = newTailNode;
+      }
+      tailNode.Items[tailNodeSize++] = item;
+      ++totalSize;
+    }
+
+    /// <inheritdoc/>
+    public void Clear()
+    {
+      headNode = null;
+      tailNode = null;
+      totalSize = 0;
+      maxNodeSize = 0;
+      tailNodeSize = 0;
+    }
+
+    /// <inheritdoc/>
+    public bool Contains(T item)
+    {
+      return this.Any(itemNode => Comparer.Equals(itemNode, item));
+    }
+
+    /// <inheritdoc/>
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+      ArgumentValidator.EnsureArgumentNotNull(array, "array");
+      ArgumentValidator.EnsureArgumentIsInRange(arrayIndex, 0, int.MaxValue, "arrayIndex");
+
+      if (array.Length < totalSize + arrayIndex)
+        throw new ArgumentException();
+
+      foreach (var item in this)
+        array[arrayIndex++] = item;
+    }
+
+    /// <inheritdoc/>
+    public bool Remove(T item)
+    {
+      throw new NotSupportedException(Strings.ExChainedBufferRemoveMethodIsNotSupported);
+    }
+
+    /// <inheritdoc/>
+    public void AddRange(IEnumerable<T> items)
+    {
+      ArgumentValidator.EnsureArgumentNotNull(items, "items");
+      foreach (var item in items)
+        Add(item);
+    }
+
+    #endregion
+
+    // Constructors
+
+    /// <summary>
+    /// Initializes new instance of this type.
+    /// </summary>
+    public ChainedBuffer()
+    {
+      const int largeObjectHeapItemSize = 85000;
+      maxNodeSize = largeObjectHeapItemSize / IntPtr.Size;
+    }
+
+    /// <summary>
+    /// Initializes new instance of this type.
+    /// </summary>
+    /// <param name="maxNodeSize">The maximal node size.</param>
+    public ChainedBuffer(int maxNodeSize)
+    {
+      ArgumentValidator.EnsureArgumentIsInRange(maxNodeSize, 1, int.MaxValue, "maxNodeSize");
+      this.maxNodeSize = maxNodeSize;
+    }
+
+    /// <summary>
+    /// Initializes new instance of this type.
+    /// </summary>
+    /// <param name="items">The items to add to this collection.</param>   
+    public ChainedBuffer(IEnumerable<T> items)
+      : this()
+    {
+      AddRange(items);
+    }
+
+    /// <summary>
+    /// Initializes new instance of this type.
+    /// </summary>
+    /// <param name="items">The items to add to this collection.</param>      
+    /// <param name="maxNodeSize">The maximal node size.</param>
+    public ChainedBuffer(IEnumerable<T> items, int maxNodeSize)
+      : this(maxNodeSize)
+    {
+      AddRange(items);
+    }
+  }
+}
