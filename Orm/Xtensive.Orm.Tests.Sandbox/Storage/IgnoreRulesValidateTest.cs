6303820337e97aa7a2225e1e30ec71298ce93a0f
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2013-08-30 12:02:12 +0600

    Changed namespaces of models

--- Orm/Xtensive.Orm.Tests.Sandbox/Storage/IgnoreRulesValidateTest.cs
+++ Orm/Xtensive.Orm.Tests.Sandbox/Storage/IgnoreRulesValidateTest.cs
@@ -8,10 +8,10 @@ using System;
 using System.Linq;
 using NUnit.Framework;
 using Xtensive.Orm.Configuration;
-using Model_1 = Xtensive.Orm.Tests.Storage.IgnoreRulesValidateModel_1;
-using Model_2 = Xtensive.Orm.Tests.Storage.IgnoreRulesValidateModel_2;
+using Model1 = Xtensive.Orm.Tests.Storage.IgnoreRulesValidateModel1;
+using Model2 = Xtensive.Orm.Tests.Storage.IgnoreRulesValidateModel2;
 
-namespace Xtensive.Orm.Tests.Storage.IgnoreRulesValidateModel_1
+namespace Xtensive.Orm.Tests.Storage.IgnoreRulesValidateModel1
 {
   public abstract class Person : Entity
   {
@@ -112,7 +112,7 @@ namespace Xtensive.Orm.Tests.Storage.IgnoreRulesValidateModel_1
   }
 }
 
-namespace Xtensive.Orm.Tests.Storage.IgnoreRulesValidateModel_2
+namespace Xtensive.Orm.Tests.Storage.IgnoreRulesValidateModel2
 {
   public abstract class Person : Entity
   {
@@ -220,13 +220,13 @@ namespace Xtensive.Orm.Tests.Storage
       var configuration = DomainConfigurationFactory.Create();
       configuration.UpgradeMode = mode;
       if (mode != DomainUpgradeMode.Recreate) {
-        configuration.Types.Register(typeof (Model_2.Customer).Assembly, typeof (Model_2.Customer).Namespace);
+        configuration.Types.Register(typeof (Model2.Customer).Assembly, typeof (Model2.Customer).Namespace);
         configuration.IgnoreRules.IgnoreTable("IgnoredTable");
         configuration.IgnoreRules.IgnoreColumn("SomeIgnoredField").WhenTable("Order");
         configuration.IgnoreRules.IgnoreColumn("IgnoredColumn.Id").WhenTable("Author");
       }
       else
-        configuration.Types.Register(typeof (Model_1.Customer).Assembly, typeof (Model_1.Customer).Namespace);
+        configuration.Types.Register(typeof (Model1.Customer).Assembly, typeof (Model1.Customer).Namespace);
       return Domain.Build(configuration);
     }
 
@@ -235,11 +235,11 @@ namespace Xtensive.Orm.Tests.Storage
       using (var domain = BuildDomain(DomainUpgradeMode.Recreate))
       using (var session = domain.OpenSession())
       using (var transaction = session.OpenTransaction()) {
-        var author = new Model_1.Author { FirstName = "Иван", LastName = "Гончаров", Birthday = new DateTime(1812, 6, 18) };
-        var book = new Model_1.Book { ISBN = "9780140440409", Title = "Обломов" };
+        var author = new Model1.Author { FirstName = "Иван", LastName = "Гончаров", Birthday = new DateTime(1812, 6, 18) };
+        var book = new Model1.Book { ISBN = "9780140440409", Title = "Обломов" };
         book.Authors.Add(author);
-        var customer = new Model_1.Customer { FirstName = "Алексей", LastName = "Кулаков", Birthday = new DateTime(1988, 8, 31) };
-        var order = new Model_1.Order { Book = book, Customer = customer, SomeIgnoredField = "Secret information for FBI :)" };
+        var customer = new Model1.Customer { FirstName = "Алексей", LastName = "Кулаков", Birthday = new DateTime(1988, 8, 31) };
+        var order = new Model1.Order { Book = book, Customer = customer, SomeIgnoredField = "Secret information for FBI :)" };
         transaction.Complete();
       }
     }
@@ -249,9 +249,9 @@ namespace Xtensive.Orm.Tests.Storage
       using (var domain = BuildDomain(DomainUpgradeMode.Perform))
       using (var session = domain.OpenSession())
       using (var transaction = session.OpenTransaction()) {
-        var currentCustomer = session.Query.All<Model_2.Customer>().First(c => c.LastName=="Кулаков");
-        var order = session.Query.All<Model_2.Order>().First(o => o.Customer.LastName==currentCustomer.LastName);
-        var newCustomer = new Model_2.Customer { FirstName = "Fred", LastName = "Smith", Birthday = new DateTime(1998, 7, 9) };
+        var currentCustomer = session.Query.All<Model2.Customer>().First(c => c.LastName=="Кулаков");
+        var order = session.Query.All<Model2.Order>().First(o => o.Customer.LastName==currentCustomer.LastName);
+        var newCustomer = new Model2.Customer { FirstName = "Fred", LastName = "Smith", Birthday = new DateTime(1998, 7, 9) };
         order.Customer = newCustomer;
         changedOrderKey = order.Key;
         transaction.Complete();
@@ -263,9 +263,9 @@ namespace Xtensive.Orm.Tests.Storage
       using (var domain = BuildDomain(DomainUpgradeMode.PerformSafely))
       using (var session = domain.OpenSession())
       using (var transaction = session.OpenTransaction()) {
-        var currentCustomer = session.Query.All<Model_2.Customer>().First(c => c.LastName=="Кулаков");
-        var order = session.Query.All<Model_2.Order>().First(o => o.Customer.LastName==currentCustomer.LastName);
-        var newCustomer = new Model_2.Customer { FirstName = "Fred", LastName = "Smith", Birthday = new DateTime(1998, 7, 9) };
+        var currentCustomer = session.Query.All<Model2.Customer>().First(c => c.LastName=="Кулаков");
+        var order = session.Query.All<Model2.Order>().First(o => o.Customer.LastName==currentCustomer.LastName);
+        var newCustomer = new Model2.Customer { FirstName = "Fred", LastName = "Smith", Birthday = new DateTime(1998, 7, 9) };
         order.Customer = newCustomer;
         changedOrderKey = order.Key;
         transaction.Complete();
@@ -275,12 +275,12 @@ namespace Xtensive.Orm.Tests.Storage
     {
       var configuration = DomainConfigurationFactory.Create();
       configuration.UpgradeMode = DomainUpgradeMode.Validate;
-      configuration.Types.Register(typeof (Model_1.Customer).Assembly, typeof (Model_1.Customer).Namespace);
+      configuration.Types.Register(typeof (Model1.Customer).Assembly, typeof (Model1.Customer).Namespace);
 
       using (var domain = Domain.Build(configuration))
       using (var session = domain.OpenSession())
       using (var transaction = session.OpenTransaction()) {
-        var result = session.Query.All<Model_1.Order>().First(o => o.Key==changedOrderKey);
+        var result = session.Query.All<Model1.Order>().First(o => o.Key==changedOrderKey);
         Assert.That(result.SomeIgnoredField, Is.EqualTo("Secret information for FBI :)"));
       }
     }
