461e834a5d816555875fc59cac6d4296d0185c8d
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2013-09-11 13:18:21 +0600

    Added throwing exception when table's or column's name contains special symbols

--- Orm/Xtensive.Orm/Orm/Configuration/DomainConfiguration.cs
+++ Orm/Xtensive.Orm/Orm/Configuration/DomainConfiguration.cs
@@ -607,9 +607,21 @@ namespace Xtensive.Orm.Configuration
     private void ValidateIgnoreConfiguration()
     {
       foreach (var ignoreRule in IgnoreRules) {
-        if(string.IsNullOrEmpty(ignoreRule.Table) && string.IsNullOrEmpty(ignoreRule.Column))
-          throw new InvalidOperationException(Strings.ExIgnoreRuleCantBeAppliedToDatabaseOrSchemaOnly);
+        if (string.IsNullOrEmpty(ignoreRule.Table) && string.IsNullOrEmpty(ignoreRule.Column))
+          throw new InvalidOperationException(string.Format(Strings.ExIgnoreRuleXMustBeAppliedToColumnOrTable, ignoreRule));
+        if (IsContainsRegExSpecialChars(ignoreRule.Table))
+          throw new InvalidOperationException(string.Format(Strings.ExIgnoreRuleXContainsInvalidTableNameY, ignoreRule, ignoreRule.Table));
+        if (IsContainsRegExSpecialChars(ignoreRule.Column))
+          throw new InvalidOperationException(string.Format(Strings.ExIgnoreRuleXContainsInvalidColumnNameY, ignoreRule, ignoreRule.Column));
       }
+    }
+
+    private bool IsContainsRegExSpecialChars(string valueToValidate)
+    {
+      const string specialChars = @"[]\/^$|?+(){}";
+      if (valueToValidate.IsNullOrEmpty())
+        return false;
+      return valueToValidate.Any(symbol => specialChars.Contains(symbol));
     }
     /// <inheritdoc/>
     protected override ConfigurationBase CreateClone()
