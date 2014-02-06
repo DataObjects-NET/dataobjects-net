e32d2d2463e248bc03d368a522080be7b1a5d7fb
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2014-02-06 16:26:29 +0600

    Fixed information about copyrights, creation date and creator

--- Orm/Xtensive.Orm/Orm/Linq/LocalCollectionKeyTypeExtractor.cs
+++ Orm/Xtensive.Orm/Orm/Linq/LocalCollectionKeyTypeExtractor.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Copyright (C) 2013 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Kulakov
+// Created:    2013.12.30
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
