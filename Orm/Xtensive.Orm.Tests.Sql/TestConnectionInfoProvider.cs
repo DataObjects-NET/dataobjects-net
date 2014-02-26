dc29f1c1e7f3580bacf4f0e44e66dbf6a4b27f59
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2014-02-26 13:53:19 +0600

    Added provider of connection information for tests

---
+++ Orm/Xtensive.Orm.Tests.Sql/TestConnectionInfoProvider.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (C) 2014 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Kulakov
+// Created:    2014.02.25
+
+namespace Xtensive.Orm.Tests.Sql
+{
+  public sealed class TestConnectionInfoProvider
+  {
+    public static string GetCurrentConnectionUrl()
+    {
+      return GetCurrentConnectionInfo().ConnectionUrl.Url;
+    }
+
+    public static string GetCurrentConnectionProvider()
+    {
+      return GetCurrentConnectionInfo().Provider;
+    }
+
+    public static string GetCurrentConnectionString()
+    {
+      return TestConfiguration.Instance.GetConnectionInfo(TestConfiguration.Instance.Storage+"cs").ConnectionString; 
+    }
+
+    public static ConnectionInfo GetCurrentConnectionInfo()
+    {
+      return TestConfiguration.Instance.GetConnectionInfo(TestConfiguration.Instance.Storage);
+    }
+  }
+}
