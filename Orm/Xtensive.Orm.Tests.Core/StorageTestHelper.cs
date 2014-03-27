b76097a62bb9a5ee2c3a12545eb822e486cfccf1
Denis Krjuchkov <denis.krjuchkov@gmail.com>
2014-03-27 18:50:59 +0600

    StorageTestHelper: Oracle compatibility

--- Orm/Xtensive.Orm.Tests.Core/StorageTestHelper.cs
+++ Orm/Xtensive.Orm.Tests.Core/StorageTestHelper.cs
@@ -5,8 +5,8 @@
 // Created:    2009.12.17
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using Xtensive.Core;
 using Xtensive.Orm.Providers;
 using Xtensive.Sql;
 using Xtensive.Sql.Model;
@@ -41,17 +41,49 @@ namespace Xtensive.Orm.Tests
       var driver = TestSqlDriver.Create(connectionInfo);
       using (var connection = driver.CreateConnection()) {
         connection.Open();
+
         var extractionTask = new SqlExtractionTask(driver.CoreServerInfo.DatabaseName);
         var extractionResult = driver.Extract(connection, new[] {extractionTask});
         var catalog = extractionResult.Catalogs.Single();
         var existingSchemas = catalog.Schemas.Select(s => s.Name);
-        foreach (var schema in schemas.Except(existingSchemas)) {
-          var query = SqlDdl.Create(catalog.CreateSchema(schema));
-          using (var command = connection.CreateCommand(query)) {
-            command.ExecuteNonQuery();
-          }
-        }
+        var schemasToCreate = schemas.Except(existingSchemas, StringComparer.OrdinalIgnoreCase);
+
+        // Oracle does not support creating schemas
+        // Instead user should be create, corresponding schema is added automatically.
+        if (connectionInfo.Provider==WellKnown.Provider.Oracle)
+          CreateUsers(connection, schemasToCreate);
+        else
+          CreateSchemas(connection, catalog, schemasToCreate);
+
+        connection.Close();
       }
+    }
+
+    private static void CreateUsers(SqlConnection connection, IEnumerable<string> schemasToCreate)
+    {
+      foreach (var schema in schemasToCreate) {
+        ExecuteQuery(connection, string.Format("create user {0} identified by {0}", schema));
+        ExecuteQuery(connection, string.Format("alter user {0} quota unlimited on system", schema));
+      }
+    }
+
+    private static void CreateSchemas(SqlConnection connection, Catalog catalog, IEnumerable<string> schemasToCreate)
+    {
+      foreach (var schema in schemasToCreate) {
+        ExecuteQuery(connection, SqlDdl.Create(catalog.CreateSchema(schema)));
+      }
+    }
+
+    private static void ExecuteQuery(SqlConnection connection, ISqlCompileUnit query)
+    {
+      using (var command = connection.CreateCommand(query))
+        command.ExecuteNonQuery();
+    }
+
+    private static void ExecuteQuery(SqlConnection connection, string query)
+    {
+      using (var command = connection.CreateCommand(query))
+        command.ExecuteNonQuery();
     }
   }
 }
