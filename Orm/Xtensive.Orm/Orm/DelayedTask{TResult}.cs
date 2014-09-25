f1b93aff5561d13ceb929d7fdaff86884d6de170
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2014-09-25 16:32:51 +0600

    Added delayed task

---
+++ Orm/Xtensive.Orm/Orm/DelayedTask{TResult}.cs
@@ -0,0 +1,139 @@
+﻿// Copyright (C) 2012 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Kulakov
+// Created:    2014.08.29
+
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xtensive.Orm.Internals;
+
+namespace Xtensive.Orm
+{
+#if NET45
+  /// <summary>
+  /// Represents delayed query task.
+  /// </summary>
+  public class DelayedTask<TResult> : SessionBound
+  {
+    private enum DelayedTaskStatus
+    {
+      Created,
+      Started,
+      Completed,
+      Failed,
+      Canceled
+    }
+
+    private readonly TaskCompletionSource<TResult> source;
+    private readonly DelayedQueryResult<TResult> delayedResult;
+    private Session session;
+    private DelayedTaskStatus internalStatus;
+
+    internal static DelayedTaskFactory<TResult> Factory { get; private set; }
+
+    public TaskStatus Status
+    {
+      get { return source.Task.Status; }
+    }
+
+    /// <summary>
+    /// Gets awaiter for this delayed task.
+    /// </summary>
+    /// <returns>Returns awaiter of <see cref="TResult"/></returns>
+    public TaskAwaiter<TResult> GetAwaiter()
+    {
+      var awaiter = source.Task.GetAwaiter();
+      switch (internalStatus) {
+      case DelayedTaskStatus.Canceled:
+      case DelayedTaskStatus.Failed:
+      case DelayedTaskStatus.Started:
+      case DelayedTaskStatus.Completed:
+        return awaiter;
+      case DelayedTaskStatus.Created: {
+        session.ExecuteDelayedQueriesAsync(false).ContinueWith(
+          task => {
+            if (task.IsFaulted) {
+              source.SetException(task.Exception.InnerException);
+              internalStatus = DelayedTaskStatus.Failed;
+              return;
+            }
+            if (task.IsCanceled) {
+              source.SetCanceled();
+              internalStatus = DelayedTaskStatus.Canceled;
+              return;
+            }
+            var delayedResultAsDelayed = delayedResult as Delayed<TResult>;
+            if (delayedResultAsDelayed!=null) {
+              source.SetResult(delayedResultAsDelayed.Value);
+              internalStatus = DelayedTaskStatus.Completed;
+              return;
+            }
+            object aa = delayedResult;
+            source.SetResult((TResult) aa);
+            internalStatus = DelayedTaskStatus.Completed;
+          });
+        return awaiter;
+      }
+      default:
+        return awaiter;
+      }
+    }
+
+    /// <summary>
+    /// Convert this instance to Task of <see cref="TResult"/>.
+    /// </summary>
+    /// <returns>Task of <see cref="TResult"/>.</returns>
+    public Task<TResult> ToTask()
+    {
+      switch (internalStatus) {
+      case DelayedTaskStatus.Canceled:
+      case DelayedTaskStatus.Failed:
+      case DelayedTaskStatus.Started:
+      case DelayedTaskStatus.Completed:
+        return source.Task;
+      case DelayedTaskStatus.Created: {
+        Session.ExecuteDelayedQueriesAsync(false).ContinueWith(
+          task => {
+            if (task.IsFaulted) {
+              source.SetException(task.Exception.InnerException);
+              internalStatus = DelayedTaskStatus.Failed;
+              return;
+            }
+            if (task.IsCanceled) {
+              source.SetCanceled();
+              internalStatus = DelayedTaskStatus.Canceled;
+              return;
+            }
+            var delayedResultAsDelayed = delayedResult as Delayed<TResult>;
+            if (delayedResultAsDelayed!=null) {
+              source.SetResult(delayedResultAsDelayed.Value);
+              internalStatus = DelayedTaskStatus.Completed;
+              return;
+            }
+            object aa = delayedResult;
+            source.SetResult((TResult) aa);
+            internalStatus = DelayedTaskStatus.Completed;
+          });
+        return source.Task;
+      }
+      default:
+        return source.Task;
+      }
+    }
+
+    static DelayedTask()
+    {
+      Factory = new DelayedTaskFactory<TResult>();
+    }
+
+    internal DelayedTask(Session session, DelayedQueryResult<TResult> delayed) //TResult
+      : base(session)
+    {
+      source = new TaskCompletionSource<TResult>();
+      delayedResult = delayed;
+      internalStatus = DelayedTaskStatus.Created;
+    }
+  }
+#endif
+}
---
+++ Orm/Xtensive.Orm/Orm/Internals/DelayedTaskFactory{T}.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (C) 2012 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Kulakov
+// Created:    2014.08.29
+
+namespace Xtensive.Orm.Internals
+{
+#if NET45
+  internal class DelayedTaskFactory<T>
+  {
+    public DelayedTask<T> CreateNew<T>(Session session, DelayedQueryResult<T> delayedResult)
+    {
+      return new DelayedTask<T>(session, delayedResult);
+    }
+
+    public DelayedTask<T> CreateNew<T>(DelayedQueryResult<T> delayedResult)
+    {
+      var session = Session.Demand();
+      return new DelayedTask<T>(session, delayedResult);
+    }
+  }
+#endif
+}
