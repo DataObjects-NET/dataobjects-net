73920aef89d090f9aea79b24d887cc5cf1dae410
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2015-09-03 23:13:50 +0500

    Added schema cache manager

---
+++ Orm/Xtensive.Orm/Orm/Internals/SchemaCacheManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xtensive.Core;
+using Xtensive.Orm.Configuration;
+using Xtensive.Orm.Upgrade;
+using Xtensive.Orm.Upgrade.Internals;
+using Xtensive.Sql;
+using Xtensive.Sql.Info;
+
+namespace Xtensive.Orm.Internals
+{
+  internal class SchemaCacheManager
+  {
+    private readonly NodeSchemaCache cache;
+
+    public SchemaExtractionResult TryGetExtractionResult(NodeConfiguration nodeConfiguration)
+    {
+      var catalogs = cache.GetNodeSchema(nodeConfiguration);
+      if (catalogs==null)
+        return null;
+      var extractionResult = new SchemaExtractionResult();
+      foreach (var catalog in catalogs)
+        extractionResult.Catalogs.Add(catalog);
+      return extractionResult;
+    }
+
+    public void CacheExtractionResult(SchemaExtractionResult extractionResult, NodeConfiguration node)
+    {
+      foreach (var catalog in extractionResult.Catalogs)
+        cache.Add(catalog, node);
+    }
+
+    public SchemaCacheManager(DomainConfiguration configuration, DefaultSchemaInfo defaultSchemaInfo)
+    {
+      cache = new NodeSchemaCache(configuration, defaultSchemaInfo);
+    }
+  }
+}
