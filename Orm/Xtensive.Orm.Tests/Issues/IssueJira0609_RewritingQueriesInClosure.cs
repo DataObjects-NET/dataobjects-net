72e9750ed288c08443979faa70db3d4823c20f43
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2015-11-03 18:14:11 +0500

    Issue #609: Improved tests

--- Orm/Xtensive.Orm.Tests/Issues/IssueJira0609_RewritingQueriesInClosure.cs
+++ Orm/Xtensive.Orm.Tests/Issues/IssueJira0609_RewritingQueriesInClosure.cs
@@ -4,8 +4,11 @@
 // Created by: Alexey Kulakov
 // Created:    2009.11.02
 
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using NUnit.Framework;
+using Xtensive.Core;
 using Xtensive.Orm.Configuration;
 using Xtensive.Orm.Tests.Issues.Xtensive.Orm.Tests.Issues.IssueJira0609_RewritingQueriesInClosureModel;
 
@@ -13,31 +16,57 @@ namespace Xtensive.Orm.Tests.Issues
 {
   public class IssueJira0609_RewritingQueriesInClosure : AutoBuildTest
   {
+    private List<string> uniqueIdentifiers;
+
     [Test]
-    public void DirectInTest()
+    public void DirectInParametersTest()
     {
       using (var session = Domain.OpenSession())
       using (var transaction = session.OpenTransaction()) {
         var q = Query.All<StoredMaterialLot>().Where(c => c.IsAvailable);
-        var pallets = Query.All<BegroPallet>().Where(p => p.UniqueIdentifier.In("dfjghkdfjhg", "jdfhgkjfhg"));
+        var pallets = Query.All<BegroPallet>().Where(p => p.UniqueIdentifier.In("2","4","6","8","10"));
         var storedContainers = Query.All<StoredContainer>().Where(sc => sc.Container.Id.In(pallets.Select(p => p.Id)));
         q = q.Where(sml => sml.StoredContainer.Id.In(storedContainers.Select(sc => sc.Id)));
-        Assert.DoesNotThrow(()=>q.Count());
+        Assert.DoesNotThrow(()=>q.Run());
+        var expectedCountOfElemetns = uniqueIdentifiers.Where((s,i)=> i % 2==0).Count();
+        Assert.That(q.Count(), Is.EqualTo(expectedCountOfElemetns));
       }
     }
 
     [Test]
-    public void IndirectInTest()
+    public void ArrayAsParameterForInTest()
     {
       using (var session = Domain.OpenSession())
       using (var transaction = session.OpenTransaction()) {
         var q = Query.All<StoredMaterialLot>().Where(c => c.IsAvailable);
-        var a = new string[] {"dfjhgkjdfgj", "jdfgkjfd"};
-        var pallets = Query.All<BegroPallet>().Where(p => p.UniqueIdentifier.In(a));
+        var requiredIdentifiers = uniqueIdentifiers.Where((s, i) => i % 2==0).ToArray();
+        var pallets = Query.All<BegroPallet>().Where(p => p.UniqueIdentifier.In(requiredIdentifiers));
         var storedContainers = Query.All<StoredContainer>().Where(sc => sc.Container.Id.In(pallets.Select(p => p.Id)));
         q = q.Where(sml => sml.StoredContainer.Id.In(storedContainers.Select(sc => sc.Id)));
-        Assert.DoesNotThrow(() => q.Count());
+        Assert.DoesNotThrow(() => q.Run());
+        Assert.That(q.Count(), Is.EqualTo(requiredIdentifiers.Length));
       }
+    }
+
+    [Test]
+    public void EnumerableAsParameterForInTest()
+    {
+      using (var session = Domain.OpenSession())
+      using (var transaction = session.OpenTransaction()) {
+        var q = Query.All<StoredMaterialLot>().Where(c => c.IsAvailable);
+        var requiredIdentifiers = uniqueIdentifiers.Where((s, i) => i % 2==0);
+        var pallets = Query.All<BegroPallet>().Where(p => p.UniqueIdentifier.In(requiredIdentifiers));
+        var storedContainers = Query.All<StoredContainer>().Where(sc => sc.Container.Id.In(pallets.Select(p => p.Id)));
+        q = q.Where(sml => sml.StoredContainer.Id.In(storedContainers.Select(sc => sc.Id)));
+        Assert.DoesNotThrow(() => q.Run());
+        Assert.That(q.Count(), Is.EqualTo(requiredIdentifiers.Count()));
+      }
+    }
+
+    protected override void PopulateData()
+    {
+      PopulateIdentifiers();
+      PopulateEntities();
     }
 
     protected override DomainConfiguration BuildConfiguration()
@@ -46,6 +75,27 @@ namespace Xtensive.Orm.Tests.Issues
       configuration.UpgradeMode = DomainUpgradeMode.Recreate;
       configuration.Types.Register(typeof(StoredMaterialLot));
       return configuration;
+    }
+
+    private void PopulateIdentifiers()
+    {
+      uniqueIdentifiers = new List<string>(Enumerable.Range(1, 10).Select(el => el.ToString(CultureInfo.InvariantCulture)));
+    }
+
+    private void PopulateEntities()
+    {
+      using (var session = Domain.OpenSession())
+      using (var transaction = session.OpenTransaction()) {
+        foreach (var uniqueIdentifier in uniqueIdentifiers) {
+          new StoredMaterialLot {
+            IsAvailable = true,
+            StoredContainer = new StoredContainer {
+              Container = new BegroPallet {UniqueIdentifier = uniqueIdentifier}
+            }
+          };
+        }
+        transaction.Complete();
+      }
     }
   }
 }
