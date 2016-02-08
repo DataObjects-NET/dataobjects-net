a50c411fd2b3d8044f4c5fa16a3149399fe7ae7f
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2016-02-08 20:48:05 +0500

    Added bunch of tests

---
+++ Orm/Xtensive.Orm.Tests/Upgrade/UpgradeAndNamingRulesAdvanced/UpperCaseNamesTest.cs
@@ -0,0 +1,21 @@
+// Copyright (C) 2016 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Kulakov
+// Created:    2016.02.05
+
+using Xtensive.Orm.Configuration;
+
+namespace Xtensive.Orm.Tests.Upgrade.UpgradeAndNamingRulesAdvanced
+{
+  public sealed class UpperCaseNamesTest : TestBase
+  {
+    protected override NamingConvention CreateNamingConvention()
+    {
+      var namingConvension = new NamingConvention {
+        LetterCasePolicy = LetterCasePolicy.Uppercase
+      };
+      return namingConvension;
+    }
+  }
+}
