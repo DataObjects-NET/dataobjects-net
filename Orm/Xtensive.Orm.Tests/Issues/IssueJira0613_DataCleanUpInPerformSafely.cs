a33348b286efea0f5425cd5ac8784c6cdccbba52
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2016-05-17 18:19:38 +0500

    Fixed summary

--- Orm/Xtensive.Orm.Tests/Issues/IssueJira0613_DataCleanUpInPerformSafely.cs
+++ Orm/Xtensive.Orm.Tests/Issues/IssueJira0613_DataCleanUpInPerformSafely.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2015 Xtensive LLC.
+﻿// Copyright (C) 2016 Xtensive LLC.
 // All rights reserved.
 // For conditions of distribution and use, see license.
 // Created by: Alexey Kulakov
