6866fe77f72d857a1e0f28b0e7be81825988c84d
Alexey Groznov <alex.groznov@gmail.com>
2016-06-06 18:39:13 +0500

    new DateTime and DateTimeOffset tests

---
+++ Orm/Xtensive.Orm.Tests/Linq/DateTimeAndDateTimeOffset/BaseDateTimeAndDateTimeOffsetTest.cs
@@ -0,0 +1,225 @@
+﻿// Copyright (C) 2016 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alex Groznov
+// Created:    2016.05.30
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using NUnit.Framework;
+using Xtensive.Core;
+
+namespace Xtensive.Orm.Tests.Linq.DateTimeAndDateTimeOffset
+{
+  public abstract class BaseDateTimeAndDateTimeOffsetTest
+    : AutoBuildTest
+  {
+    [Test]
+    public void OrderByTest()
+    {
+      OpenSessionAndAction(OrderByProtected);
+    }
+
+    [Test]
+    public void DistinctTest()
+    {
+      OpenSessionAndAction(DistinctProtected);
+    }
+
+    [Test]
+    public void MinMaxTest()
+    {
+      OpenSessionAndAction(MinMaxProtected);
+    }
+
+    [Test]
+    public void GroupByTest()
+    {
+      OpenSessionAndAction(GroupByProtected);
+    }
+
+    [Test]
+    public void JoinTest()
+    {
+      OpenSessionAndAction(JoinProtected);
+    }
+
+    [Test]
+    public void SkipTakeTest()
+    {
+      OpenSessionAndAction(SkipTakeProtected);
+    }
+
+    [Test]
+    public void EqualsTest()
+    {
+      OpenSessionAndAction(EqualsProtected);
+    }
+
+    [Test]
+    public void DifferentCulturesTest()
+    {
+      var oldCulture = Thread.CurrentThread.CurrentCulture;
+      using (new Disposable(c => Thread.CurrentThread.CurrentCulture = oldCulture)) {
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+        EqualsTest();
+      }
+      using (new Disposable(c => Thread.CurrentThread.CurrentCulture = oldCulture)) {
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+        EqualsTest();
+      }
+
+      using (new Disposable(c => Thread.CurrentThread.CurrentCulture = oldCulture)) {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        EqualsTest();
+      }
+    }
+
+    protected abstract void PopulateDataProtected();
+
+    protected abstract void OrderByProtected();
+    protected abstract void DistinctProtected();
+    protected abstract void MinMaxProtected();
+    protected abstract void GroupByProtected();
+    protected abstract void JoinProtected();
+    protected abstract void SkipTakeProtected();
+    protected abstract void EqualsProtected();
+
+    protected override void PopulateData()
+    {
+      OpenSessionAndAction(PopulateDataProtected, true);
+    }
+
+    #region Implementation of base tests
+
+    protected void RunTest<T>(Expression<Func<T, bool>> filter, int rightCount = 1)
+      where T : Entity
+    {
+      var count = Query.All<T>().Count(filter);
+      Assert.AreEqual(count, rightCount);
+    }
+
+    protected void RunWrongTest<T>(Expression<Func<T, bool>> filter)
+      where T : Entity
+    {
+      RunTest(filter, 0);
+    }
+
+    protected void OpenSessionAndAction(Action action, bool commitTransaction = false)
+    {
+      using (var session = Domain.OpenSession())
+      using (var tx = session.OpenTransaction()) {
+        action();
+        if (commitTransaction)
+          tx.Complete();
+      }
+    }
+
+    protected void OrderByProtected<T, TK1, TK2>(Expression<Func<T, TK1>> orderByExpression, Expression<Func<T, TK2>> thenByExpression)
+      where T : Entity
+    {
+      var compiledOrderByExpression = orderByExpression.Compile();
+      var compiledThenByExpression = thenByExpression.Compile();
+      var notOrderedLocal = Query.All<T>().ToArray();
+      var orderedLocal = notOrderedLocal.OrderBy(compiledOrderByExpression).ThenBy(compiledThenByExpression);
+      var orderedLocalDescending = notOrderedLocal.OrderByDescending(compiledOrderByExpression).ThenBy(compiledThenByExpression);
+      var orderedByServer = Query.All<T>().OrderBy(orderByExpression).ThenBy(thenByExpression);
+      var orderedByServerDescending = Query.All<T>().OrderByDescending(orderByExpression).ThenBy(thenByExpression);
+
+      Assert.IsFalse(notOrderedLocal.SequenceEqual(orderedLocal));
+      Assert.IsFalse(notOrderedLocal.SequenceEqual(orderedByServer));
+      Assert.IsTrue(orderedLocal.SequenceEqual(orderedByServer));
+      Assert.IsTrue(orderedLocalDescending.SequenceEqual(orderedByServerDescending));
+      Assert.IsFalse(orderedLocal.SequenceEqual(orderedByServerDescending));
+      Assert.IsFalse(orderedLocalDescending.SequenceEqual(orderedByServer));
+    }
+
+    protected void DistinctProtected<T, TK>(Expression<Func<T, TK>> selectExpression)
+      where T : Entity
+    {
+      var compiledSelectExpression = selectExpression.Compile();
+      var distinctLocal = Query.All<T>().ToArray().Select(compiledSelectExpression).Distinct().OrderBy(c => c);
+      var distinctByServer = Query.All<T>().Select(selectExpression).Distinct().OrderBy(c => c);
+      Assert.IsTrue(distinctLocal.SequenceEqual(distinctByServer));
+
+      distinctByServer = Query.All<T>().Select(selectExpression).Distinct().OrderByDescending(c => c);
+      Assert.IsFalse(distinctLocal.SequenceEqual(distinctByServer));
+    }
+
+    protected void MinMaxProtected<T, TK>(Expression<Func<T, TK>> selectExpression)
+      where T : Entity
+    {
+      var compiledSelectExpression = selectExpression.Compile();
+      var minLocal = Query.All<T>().ToArray().Min(compiledSelectExpression);
+      var maxLocal = Query.All<T>().ToArray().Max(compiledSelectExpression);
+      var minServer = Query.All<T>().Min(selectExpression);
+      var maxServer = Query.All<T>().Max(selectExpression);
+
+      Assert.AreEqual(minLocal, minServer);
+      Assert.AreEqual(maxLocal, maxServer);
+      Assert.AreNotEqual(minLocal, maxServer);
+      Assert.AreNotEqual(maxLocal, minServer);
+    }
+
+    protected void GroupByProtected<T, TK1, TK2>(Expression<Func<T, TK1>> groupByExpression, Expression<Func<T, TK2>> orderByExpression)
+      where T : Entity
+    {
+      var compiledGroupByExpression = groupByExpression.Compile();
+      var compiledOrderByExpression = orderByExpression.Compile();
+      var groupByLocal = Query.All<T>().ToArray().GroupBy(compiledGroupByExpression).ToArray();
+      var groupByServer = Query.All<T>().GroupBy(groupByExpression);
+      foreach (var group in groupByServer) {
+        Assert.Contains(group, groupByLocal);
+        var localGroup = groupByLocal.Single(c => c.Key.Equals(group.Key));
+        Assert.IsTrue(group.OrderBy(compiledOrderByExpression).SequenceEqual(localGroup.OrderBy(compiledOrderByExpression)));
+      }
+    }
+
+    protected void SkipTakeProctected<T, TK1, TK2>(Expression<Func<T, TK1>> orderByExpression, Expression<Func<T, TK2>> thenByExpression, int skipCount, int takeCount)
+      where T : Entity
+    {
+      var compiledOrderByExpression = orderByExpression.Compile();
+      var compiledThenByExpression = thenByExpression.Compile();
+      var skipTakeLocal = Query.All<T>().ToArray().OrderBy(compiledOrderByExpression).ThenBy(compiledThenByExpression).Skip(skipCount).Take(takeCount).ToArray();
+      var skipTakeServer = Query.All<T>().OrderBy(orderByExpression).ThenBy(thenByExpression).Skip(skipCount).Take(takeCount);
+      Assert.IsTrue(skipTakeLocal.SequenceEqual(skipTakeServer));
+
+      skipTakeServer = Query.All<T>().OrderByDescending(orderByExpression).Skip(skipCount).Take(takeCount);
+      Assert.IsFalse(skipTakeLocal.SequenceEqual(skipTakeServer));
+    }
+
+    protected void JoinProtected<T1, T2, T3, TK1, TK3>(Expression<Func<T1, TK1>> leftJoinExpression, Expression<Func<T2, TK1>> rightJoinExpression,
+      Expression<Func<T1, T2, T3>> joinResultExpression, Expression<Func<T3, TK3>> orderByExpression, Expression<Func<T3, TK3>> thenByExpression)
+      where T1 : Entity
+      where T2 : Entity
+    {
+      var compiledLeftJoinExpression = leftJoinExpression.Compile();
+      var compiledRightJoinExpression = rightJoinExpression.Compile();
+      var compiledJoinResultExpression = joinResultExpression.Compile();
+      var compiledOrderByExpression = orderByExpression.Compile();
+      var compiledThenByExpression = thenByExpression.Compile();
+      var joinLocal = Query.All<T1>().ToArray()
+        .Join(Query.All<T2>().ToArray(), compiledLeftJoinExpression, compiledRightJoinExpression, compiledJoinResultExpression)
+        .OrderBy(compiledOrderByExpression)
+        .ThenBy(compiledThenByExpression);
+
+      var joinServer = Query.All<T1>()
+        .Join(Query.All<T2>(), leftJoinExpression, rightJoinExpression, joinResultExpression)
+        .OrderBy(orderByExpression)
+        .ThenBy(thenByExpression);
+
+      Assert.IsTrue(joinLocal.SequenceEqual(joinServer));
+
+      joinServer = Query.All<T1>()
+        .Join(Query.All<T2>(), leftJoinExpression, rightJoinExpression, joinResultExpression)
+        .OrderByDescending(orderByExpression)
+        .ThenBy(thenByExpression);
+      Assert.IsFalse(joinLocal.SequenceEqual(joinServer));
+    }
+
+    #endregion
+  }
+}
---
+++ Orm/Xtensive.Orm.Tests/Linq/DateTimeAndDateTimeOffset/DateTimeTest.cs
@@ -0,0 +1,303 @@
+﻿// Copyright (C) 2016 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alex Groznov
+// Created:    2016.05.26
+
+using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using Xtensive.Orm.Configuration;
+using Xtensive.Orm.Tests.Linq.DateTimeAndDateTimeOffset.DateTimeTestModels;
+
+namespace Xtensive.Orm.Tests.Linq.DateTimeAndDateTimeOffset
+{
+  namespace DateTimeTestModels
+  {
+    [HierarchyRoot]
+    public class SingleEntity : Entity
+    {
+      [Field, Key]
+      public int Id { get; private set; }
+
+      [Field]
+      public DateTime DateTime { get; set; }
+    }
+
+    [HierarchyRoot]
+    public class MultipleEntity : Entity
+    {
+      [Field, Key]
+      public int Id { get; private set; }
+
+      [Field]
+      public DateTime DateTime { get; set; }
+    }
+  }
+
+  public class DateTimeTest : BaseDateTimeAndDateTimeOffsetTest
+  {
+    private class JoinResult
+    {
+      public int LeftId { get; set; }
+      public int RightId { get; set; }
+      public DateTime LeftDateTime { get; set; }
+      public DateTime RightDateTime { get; set; }
+
+      public override bool Equals(object obj)
+      {
+        var equalTo = obj as JoinResult;
+        if (equalTo==null)
+          return false;
+        return LeftId==equalTo.LeftId && RightId==equalTo.RightId && LeftDateTime==equalTo.LeftDateTime && RightDateTime==equalTo.RightDateTime;
+      }
+    }
+
+    #region Default values
+
+    public virtual DateTime DefaultDateTime
+    {
+      get { return new DateTime(2016, 01, 02, 03, 04, 05, DateTimeKind.Unspecified); }
+    }
+
+    public virtual DateTime WrongDateTime
+    {
+      get { return new DateTime(2017, 12, 11, 10, 09, 08, DateTimeKind.Unspecified); }
+    }
+
+    public virtual DateTime DateTimeForSubstract
+    {
+      get { return new DateTime(2015, 12, 11, 10, 9, 8, DateTimeKind.Unspecified); }
+    }
+
+    public virtual TimeSpan DefaultTimeSpan
+    {
+      get { return new TimeSpan(5, 4, 3, 2); }
+    }
+
+    public virtual TimeSpan MultipleEntityDateStep
+    {
+      get { return TimeSpan.FromSeconds(1); }
+    }
+
+    public virtual int MultipleEntityCount
+    {
+      get { return 20; }
+    }
+
+    #endregion
+
+    #region Build config and populate data
+
+    protected override DomainConfiguration BuildConfiguration()
+    {
+      var configuration = base.BuildConfiguration();
+      configuration.Types.Register(typeof (SingleEntity));
+      configuration.Types.Register(typeof (MultipleEntity));
+      configuration.UpgradeMode = DomainUpgradeMode.Recreate;
+      return configuration;
+    }
+
+    protected override void PopulateDataProtected()
+    {
+      new SingleEntity { DateTime = DefaultDateTime };
+
+      new MultipleEntity { DateTime = DefaultDateTime };
+      new MultipleEntity { DateTime = DefaultDateTime };
+      new MultipleEntity { DateTime = DefaultDateTime.Date };
+      new MultipleEntity { DateTime = new DateTime(DefaultDateTime.Ticks, DateTimeKind.Local) };
+      new MultipleEntity { DateTime = new DateTime(DefaultDateTime.Ticks, DateTimeKind.Utc) };
+      new MultipleEntity { DateTime = DefaultDateTime.AddYears(1) };
+      new MultipleEntity { DateTime = DefaultDateTime.AddYears(-2) };
+      new MultipleEntity { DateTime = DefaultDateTime.AddMonths(44) };
+      new MultipleEntity { DateTime = DefaultDateTime.AddMonths(-55) };
+      new MultipleEntity { DateTime = DefaultDateTime.AddHours(5) };
+      new MultipleEntity { DateTime = DefaultDateTime.AddHours(-5) };
+      new MultipleEntity { DateTime = DefaultDateTime.AddMinutes(59) };
+      new MultipleEntity { DateTime = DefaultDateTime.AddMinutes(-49) };
+      new MultipleEntity { DateTime = DefaultDateTime.AddSeconds(1) };
+      new MultipleEntity { DateTime = DefaultDateTime.AddMinutes(-2) };
+
+      var dateTime = DefaultDateTime;
+      for (var i = 0; i < MultipleEntityCount; ++i) {
+        new MultipleEntity { DateTime = dateTime };
+        dateTime = dateTime.Add(MultipleEntityDateStep);
+      }
+    }
+
+    #endregion
+
+    #region Helpers
+
+    protected void RunTest(Expression<Func<SingleEntity, bool>> expression)
+    {
+      RunTest<SingleEntity>(expression);
+    }
+
+    protected void RunWrongTest(Expression<Func<SingleEntity, bool>> expression)
+    {
+      RunWrongTest<SingleEntity>(expression);
+    }
+
+    #endregion
+
+    #region Implementation of own tests
+
+    protected virtual void ExtractDateTimeProtected()
+    {
+      RunTest(c => c.DateTime==DefaultDateTime);
+      RunTest(c => c.DateTime.Year==DefaultDateTime.Year);
+      RunTest(c => c.DateTime.Month==DefaultDateTime.Month);
+      RunTest(c => c.DateTime.Day==DefaultDateTime.Day);
+      RunTest(c => c.DateTime.Hour==DefaultDateTime.Hour);
+      RunTest(c => c.DateTime.Minute==DefaultDateTime.Minute);
+      RunTest(c => c.DateTime.Second==DefaultDateTime.Second);
+
+      RunTest(c => c.DateTime.Date==DefaultDateTime.Date);
+      RunTest(c => c.DateTime.TimeOfDay==DefaultDateTime.TimeOfDay);
+      RunTest(c => c.DateTime.DayOfYear==DefaultDateTime.DayOfYear);
+      RunTest(c => c.DateTime.DayOfWeek==DefaultDateTime.DayOfWeek);
+
+      RunWrongTest(c => c.DateTime==WrongDateTime);
+      RunWrongTest(c => c.DateTime.Year==WrongDateTime.Year);
+      RunWrongTest(c => c.DateTime.Month==WrongDateTime.Month);
+      RunWrongTest(c => c.DateTime.Day==WrongDateTime.Day);
+      RunWrongTest(c => c.DateTime.Hour==WrongDateTime.Hour);
+      RunWrongTest(c => c.DateTime.Minute==WrongDateTime.Minute);
+      RunWrongTest(c => c.DateTime.Second==WrongDateTime.Second);
+
+      RunWrongTest(c => c.DateTime.Date==WrongDateTime.Date);
+      RunWrongTest(c => c.DateTime.TimeOfDay==WrongDateTime.TimeOfDay);
+      RunWrongTest(c => c.DateTime.DayOfYear==WrongDateTime.DayOfYear);
+      RunWrongTest(c => c.DateTime.DayOfWeek==WrongDateTime.DayOfWeek);
+    }
+
+    protected virtual void DateTimeOperationsProtected()
+    {
+      RunTest(c => c.DateTime==DefaultDateTime);
+      RunTest(c => c.DateTime.AddYears(1)==DefaultDateTime.AddYears(1));
+      RunTest(c => c.DateTime.AddMonths(1)==DefaultDateTime.AddMonths(1));
+      RunTest(c => c.DateTime.AddDays(1)==DefaultDateTime.AddDays(1));
+      RunTest(c => c.DateTime.AddHours(1)==DefaultDateTime.AddHours(1));
+      RunTest(c => c.DateTime.AddMinutes(1)==DefaultDateTime.AddMinutes(1));
+      RunTest(c => c.DateTime.AddSeconds(1)==DefaultDateTime.AddSeconds(1));
+      RunTest(c => c.DateTime.Add(DefaultTimeSpan)==DefaultDateTime.Add(DefaultTimeSpan));
+      RunTest(c => c.DateTime.Subtract(DefaultTimeSpan)==DefaultDateTime.Subtract(DefaultTimeSpan));
+      RunTest(c => c.DateTime.Subtract(DateTimeForSubstract)==DefaultDateTime.Subtract(DateTimeForSubstract));
+
+      RunTest(c => c.DateTime + DefaultTimeSpan==DefaultDateTime + DefaultTimeSpan);
+      RunTest(c => c.DateTime - DefaultTimeSpan==DefaultDateTime - DefaultTimeSpan);
+      RunTest(c => c.DateTime - DateTimeForSubstract==DefaultDateTime - DateTimeForSubstract);
+
+      RunWrongTest(c => c.DateTime==WrongDateTime);
+      RunWrongTest(c => c.DateTime.AddYears(1)==WrongDateTime.AddYears(1));
+      RunWrongTest(c => c.DateTime.AddMonths(1)==WrongDateTime.AddMonths(1));
+      RunWrongTest(c => c.DateTime.AddDays(1)==WrongDateTime.AddDays(1));
+      RunWrongTest(c => c.DateTime.AddHours(1)==WrongDateTime.AddHours(1));
+      RunWrongTest(c => c.DateTime.AddMinutes(1)==WrongDateTime.AddMinutes(1));
+      RunWrongTest(c => c.DateTime.AddSeconds(1)==WrongDateTime.AddSeconds(1));
+      RunWrongTest(c => c.DateTime.Add(DefaultTimeSpan)==WrongDateTime.Add(DefaultTimeSpan));
+      RunWrongTest(c => c.DateTime.Subtract(DefaultTimeSpan)==WrongDateTime.Subtract(DefaultTimeSpan));
+      RunWrongTest(c => c.DateTime.Subtract(DateTimeForSubstract)==WrongDateTime.Subtract(DateTimeForSubstract));
+
+      RunWrongTest(c => c.DateTime + DefaultTimeSpan==WrongDateTime + DefaultTimeSpan);
+      RunWrongTest(c => c.DateTime - DefaultTimeSpan==WrongDateTime - DefaultTimeSpan);
+      RunWrongTest(c => c.DateTime - DateTimeForSubstract==WrongDateTime - DateTimeForSubstract);
+    }
+
+    protected virtual void ConvertToAnotherKindProtected()
+    {
+      RunTest(c => c.DateTime.ToUniversalTime()==DefaultDateTime.ToUniversalTime());
+      RunTest(c => c.DateTime.ToLocalTime()==DefaultDateTime.ToLocalTime());
+
+      RunWrongTest(c => c.DateTime.ToUniversalTime()==WrongDateTime.ToUniversalTime());
+      RunWrongTest(c => c.DateTime.ToLocalTime()==WrongDateTime.ToLocalTime());
+    }
+
+    #endregion
+
+    #region Implementation of ancestor's tests
+
+    protected override void OrderByProtected()
+    {
+      OrderByProtected<MultipleEntity, DateTime, int>(c => c.DateTime, c => c.Id);
+    }
+
+    protected override void DistinctProtected()
+    {
+      DistinctProtected<MultipleEntity, DateTime>(c => c.DateTime);
+    }
+
+    protected override void MinMaxProtected()
+    {
+      MinMaxProtected<MultipleEntity, DateTime>(c => c.DateTime);
+    }
+
+    protected override void GroupByProtected()
+    {
+      GroupByProtected<MultipleEntity, DateTime, int>(c => c.DateTime, c => c.Id);
+    }
+
+    protected override void JoinProtected()
+    {
+      JoinProtected<MultipleEntity, MultipleEntity, JoinResult, DateTime, int>(
+        left => left.DateTime,
+        right => right.DateTime,
+        (left, right) => new JoinResult { LeftId = left.Id, RightId = right.Id, LeftDateTime = left.DateTime, RightDateTime = right.DateTime },
+        c => c.LeftId,
+        c => c.RightId
+        );
+    }
+
+    protected override void EqualsProtected()
+    {
+      RunTest(c => c.DateTime==DefaultDateTime);
+      RunWrongTest(c => c.DateTime==WrongDateTime);
+    }
+
+    protected override void SkipTakeProtected()
+    {
+      SkipTakeProctected<MultipleEntity, DateTimeOffset, int>(c => c.DateTime, c => c.Id, 3, 5);
+    }
+
+    #endregion
+
+    [Test]
+    public void ExtractDateTimeTest()
+    {
+      OpenSessionAndAction(ExtractDateTimeProtected);
+    }
+
+    [Test]
+    public void DateTimeOperationsTest()
+    {
+      OpenSessionAndAction(DateTimeOperationsProtected);
+    }
+
+    [Test]
+    public void DateTimeCompareWithDateTimeTest()
+    {
+      OpenSessionAndAction(() => {
+        RunTest(c => c.DateTime==DefaultDateTime);
+        RunTest(c => c.DateTime.Date==DefaultDateTime.Date);
+        RunTest(c => c.DateTime > DefaultDateTime.AddMinutes(-1));
+        RunWrongTest(c => c.DateTime < DefaultDateTime.AddMinutes(-1));
+      });
+    }
+
+    [Test]
+    public void ToIsoStringTest()
+    {
+      OpenSessionAndAction(() => {
+        RunTest(c => c.DateTime.ToString("s")==DefaultDateTime.ToString("s"));
+        RunWrongTest(c => c.DateTime.ToString("s")==DefaultDateTime.AddMinutes(1).ToString("s"));
+      });
+    }
+
+    //    [Test] Not supported
+    public void ConvertToAnotherKindTest()
+    {
+      OpenSessionAndAction(ConvertToAnotherKindProtected);
+    }
+  }
+}
