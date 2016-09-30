920863a9091467b030ceaf9a8096635f30cb1e32
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2016-09-30 16:12:11 +0500

    Added new test case

--- Orm/Xtensive.Orm.Tests/Issues/IssueJira0674_ReferenceFinderBug.cs
+++ Orm/Xtensive.Orm.Tests/Issues/IssueJira0674_ReferenceFinderBug.cs
@@ -70,6 +70,55 @@ namespace Xtensive.Orm.Tests.Issues
       }
     }
 
+    [Test]
+    public void RemoveSavedReferenceClientTest()
+    {
+      Key keyA2, keyB1, keyB2;
+      using (var session = Domain.OpenSession(new SessionConfiguration(SessionOptions.ServerProfile)))
+      using (var transaction = session.OpenTransaction()) {
+        var testA1 = new TestA(session) {Text = "A1"};
+        keyA2 = new TestA(session) {Text = "A2"}.Key;
+        keyB1 = new TestB(session) {Text = "B1", TestA = testA1}.Key;
+        keyB2 = new TestB(session) {Text = "B2", TestA = testA1}.Key;
+        transaction.Complete();
+      }
+
+      using (var session = Domain.OpenSession(new SessionConfiguration(SessionOptions.ClientProfile))) {
+        var testA2 = session.Query.Single<TestA>(keyA2);
+        var testB1 = session.Query.Single<TestB>(keyB1);
+        var testB2 = session.Query.Single<TestB>(keyB2);
+        testB2.Remove();
+        var testA1 = testB1.TestA;
+        testB1.TestA = testA2;
+        // Exception with TestB2!
+        testA1.Remove();
+      }
+    }
+
+    [Test]
+    public void RemoveSavedReferenceServerTest()
+    {
+      Key keyA2, keyB1, keyB2;
+      using (var session = Domain.OpenSession(new SessionConfiguration(SessionOptions.ServerProfile)))
+      using (var transaction = session.OpenTransaction()) {
+        var testA1 = new TestA(session) {Text = "A1"};
+        keyA2 = new TestA(session) {Text = "A2"}.Key;
+        keyB1 = new TestB(session) {Text = "B1", TestA = testA1}.Key;
+        keyB2 = new TestB(session) {Text = "B2", TestA = testA1}.Key;
+        transaction.Complete();
+      }
+
+      using (var session = Domain.OpenSession(new SessionConfiguration(SessionOptions.ServerProfile)))
+      using (var transaction = session.OpenTransaction()) {
+        var testA2 = session.Query.Single<TestA>(keyA2);
+        var testB1 = session.Query.Single<TestB>(keyB1);
+        var testB2 = session.Query.Single<TestB>(keyB2);
+        testB2.Remove();
+        var testA1 = testB1.TestA;
+        testB1.TestA = testA2;
+        testA1.Remove();
+      }
+    }
 
     protected override DomainConfiguration BuildConfiguration()
     {
