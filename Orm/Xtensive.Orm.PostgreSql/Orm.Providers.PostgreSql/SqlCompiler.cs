c4c5c5c54b552cfa23d0f5231fc37413b2feba4d
Julian Mamokin <julian1990@mail.ru>
2016-10-17 12:12:22 +0500

    PostgreSql SqlCompiler:
     - Added int TypeMapping
     - Renamed binding into criteriaBinding
     - Added topNBinding
     - Bindings are now passed to CreateProvider as an array
     - Modified method logic to support topN by rank queries

--- Orm/Xtensive.Orm.PostgreSql/Orm.Providers.PostgreSql/SqlCompiler.cs
+++ Orm/Xtensive.Orm.PostgreSql/Orm.Providers.PostgreSql/SqlCompiler.cs
@@ -19,25 +19,38 @@ namespace Xtensive.Orm.Providers.PostgreSql
   {
     protected override SqlProvider VisitFreeText(FreeTextProvider provider)
     {
-      var domainHandler = DomainHandler;
-      var rankColumnName = provider.Header.Columns.Last().Name;
+      SqlFreeTextTable fromTable;
+      QueryParameterBinding [] bindings;
 
       var stringTypeMapping = Driver.GetTypeMapping(typeof (string));
-      var binding = new QueryParameterBinding(stringTypeMapping,
+      var criteriaBinding = new QueryParameterBinding(stringTypeMapping,
         provider.SearchCriteria.Invoke, QueryParameterBindingType.Regular);
-
-      SqlSelect select = SqlDml.Select();
       var realPrimaryIndex = provider.PrimaryIndex.Resolve(Handlers.Domain.Model);
       var index = realPrimaryIndex.ReflectedType.Indexes.PrimaryIndex;
       var query = BuildProviderQuery(index);
       var table = Mapping[realPrimaryIndex.ReflectedType];
-      var fromTable = SqlDml.FreeTextTable(table, binding.ParameterReference, table.Columns.Select(column => column.Name).AddOne(rankColumnName).ToList());
+      var rankColumnName = provider.Header.Columns.Last().Name;
+
+      if (provider.TopN==null) {
+        fromTable = SqlDml.FreeTextTable(
+          table, criteriaBinding.ParameterReference, table.Columns.Select(column => column.Name).AddOne(rankColumnName).ToList());
+        bindings = new[] { criteriaBinding };
+      }
+      else {
+        var intTypeMapping = Driver.GetTypeMapping(typeof(int));
+        var topNBinding = new QueryParameterBinding(intTypeMapping,
+          () => provider.TopN.Invoke(), QueryParameterBindingType.Regular);
+        fromTable = SqlDml.FreeTextTable(
+          table, criteriaBinding.ParameterReference, table.Columns.Select(column => column.Name).AddOne(rankColumnName).ToList(), topNBinding.ParameterReference);
+        bindings = new[] { criteriaBinding, topNBinding };
+      }
       var fromTableRef = SqlDml.QueryRef(fromTable);
+      SqlSelect select = SqlDml.Select();
       foreach (var column in query.Columns)
         select.Columns.Add(fromTableRef.Columns[column.Name] ?? column);
       select.Columns.Add(SqlDml.Cast(fromTableRef.Columns[rankColumnName], SqlType.Double), rankColumnName);
       select.From = fromTableRef;
-      return CreateProvider(select, binding, provider);
+      return CreateProvider(select, bindings, provider);
     }
 
     protected override SqlExpression ProcessAggregate(SqlProvider source, List<SqlExpression> sourceColumns, AggregateColumn aggregateColumn)
