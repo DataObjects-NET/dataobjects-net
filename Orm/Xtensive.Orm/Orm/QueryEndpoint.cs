908052c1ac1c4a45e808df9bae787ea934ed2ba9
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2016-11-18 15:57:19 +0500

    Used WellKnownMembers.QueryEndpoing instead of WellKnowsMembers.Query

--- Orm/Xtensive.Orm/Orm/QueryEndpoint.cs
+++ Orm/Xtensive.Orm/Orm/QueryEndpoint.cs
@@ -163,7 +163,7 @@ namespace Xtensive.Orm
       where T : Entity
     {
       ArgumentValidator.EnsureArgumentNotNull(searchCriteria, "searchCriteria");
-      var method = WellKnownMembers.Query.ContainsTableString.MakeGenericMethod(typeof(T));
+      var method = WellKnownMembers.QueryEndpoint.ContainsTableString.MakeGenericMethod(typeof(T));
       var expression = Expression.Call(method, Expression.Constant(searchCriteria));
       return Provider.CreateQuery<FullTextMatch<T>>(expression);
     }
@@ -182,7 +182,7 @@ namespace Xtensive.Orm
       where T : Entity
     {
       ArgumentValidator.EnsureArgumentNotNull(searchCriteria, "searchCriteria");
-      var method = WellKnownMembers.Query.ContainsTableString.MakeGenericMethod(typeof(T));
+      var method = WellKnownMembers.QueryEndpoint.ContainsTableStringWithColumns.MakeGenericMethod(typeof(T));
       var expression = Expression.Call(method, Expression.Constant(searchCriteria), Expression.Constant(targetColumnNames));
       return Provider.CreateQuery<FullTextMatch<T>>(expression);
     }
@@ -200,7 +200,7 @@ namespace Xtensive.Orm
       where T : Entity
     {
       ArgumentValidator.EnsureArgumentNotNull(searchCriteria, "searchCriteria");
-      var method = WellKnownMembers.Query.ContainsTableExpression.MakeGenericMethod(typeof (T));
+      var method = WellKnownMembers.QueryEndpoint.ContainsTableExpression.MakeGenericMethod(typeof (T));
       var expression = Expression.Call(null, method, new []{searchCriteria});
       return Provider.CreateQuery<FullTextMatch<T>>(expression);
     }
@@ -219,7 +219,7 @@ namespace Xtensive.Orm
       where T : Entity
     {
       ArgumentValidator.EnsureArgumentNotNull(searchCriteria, "searchCriteria");
-      var method = WellKnownMembers.Query.ContainsTableExpression.MakeGenericMethod(typeof(T));
+      var method = WellKnownMembers.QueryEndpoint.ContainsTableExpressionWithColumns.MakeGenericMethod(typeof(T));
       var expression = Expression.Call(null, method, searchCriteria, Expression.Constant(targetColumnNames));
       return Provider.CreateQuery<FullTextMatch<T>>(expression);
     }
