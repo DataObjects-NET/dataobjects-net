a62f955ea4a4fc6ec8229ccdb6067f1730ae5dd6
Julian Mamokin <julian1990@mail.ru>
2016-11-23 15:53:23 +0500

    Changed namespace

--- Orm/Xtensive.Orm.Tests/Model/FullTextIndexOnDynamicallyDefinedFieldsTest.cs
+++ Orm/Xtensive.Orm.Tests/Model/FullTextIndexOnDynamicallyDefinedFieldsTest.cs
@@ -10,9 +10,9 @@ using Xtensive.Orm.Building;
 using Xtensive.Orm.Building.Definitions;
 using Xtensive.Orm.Configuration;
 using Xtensive.Orm.Model;
-using Xtensive.Orm.Tests.Model.FullTextIndexOnDynamicStructureFieldTestModel;
+using Xtensive.Orm.Tests.Model.FullTextIndexOnDynamicallyDefinedFieldsTestModel;
 
-namespace Xtensive.Orm.Tests.Model.FullTextIndexOnDynamicStructureFieldTestModel
+namespace Xtensive.Orm.Tests.Model.FullTextIndexOnDynamicallyDefinedFieldsTestModel
 {
   [HierarchyRoot]
   public class EntityWithDynamicallyDefinedStructureField : Entity
