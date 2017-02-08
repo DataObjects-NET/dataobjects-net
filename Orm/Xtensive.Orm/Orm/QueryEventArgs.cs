5d4688fb4cecb740912c74e33ec96b8cbf962ff1
Julian Mamokin <julian1990@mail.ru>
2017-02-08 20:09:04 +0500

    Added Exception field to QueryEventArgs

--- Orm/Xtensive.Orm/Orm/QueryEventArgs.cs
+++ Orm/Xtensive.Orm/Orm/QueryEventArgs.cs
@@ -15,15 +15,21 @@ namespace Xtensive.Orm
     /// </summary>
     public Expression Expression { get; set; }
 
+    /// <summary>
+    /// Gets exception, if any, thrown durring query execution
+    /// </summary>
+    public Exception Exception { get; private set; }
+
     // Constructors
 
     /// <summary>
     /// Initializes a new instance of this class.
     /// <param name="expression">Executed expression.</param>
     /// </summary>
-    public QueryEventArgs(Expression expression)
+    public QueryEventArgs(Expression expression, Exception exception=null)
     {
       Expression = expression;
+      Exception = exception;
     }
   }
 }
