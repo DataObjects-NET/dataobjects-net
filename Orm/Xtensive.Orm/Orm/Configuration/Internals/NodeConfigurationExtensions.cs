fa73b6156393f2e0b97fd5f70d094ed2703786b0
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2017-03-03 19:41:30 +0500

    Fixed Metadata update

---
+++ Orm/Xtensive.Orm/Orm/Configuration/Internals/NodeConfigurationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xtensive.Sql.Model;
+
+namespace Xtensive.Orm.Configuration
+{
+  internal static class NodeConfigurationExtensions
+  {
+    public static string GetActualNameFor(this NodeConfiguration nodeConfiguration, Catalog catalog)
+    {
+      return nodeConfiguration.DatabaseMapping.Apply(catalog.GetNameInternal());
+    }
+
+    public static string GetActualNameFor(this NodeConfiguration nodeConfiguration, Schema schema)
+    {
+      return nodeConfiguration.SchemaMapping.Apply(schema.GetNameInternal());
+    }
+  }
+}
