93f7e3259116097a6dce94adfbc5de4a3a117387
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2017-04-17 17:44:39 +0500

    Key generators tests under SchemaSharingOverNodes option improved
    - created test for "single-schema" databases
    - multischema and multidatabase tests were inherited from simple
    - multischema and multidatabase tests were renamed and moved to particular folder
    - model was moved to separate file

---
+++ Orm/Xtensive.Orm.Tests/Storage/SchemaSharing/KeyGenerator/MultidatabaseTest.cs
@@ -0,0 +1,53 @@
+// Copyright (C) 2017 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Kulakov
+// Created:    2017.03.06
+
+using System.Collections.Generic;
+using NUnit.Framework;
+using Xtensive.Orm.Configuration;
+using Xtensive.Orm.Providers;
+using model = Xtensive.Orm.Tests.Storage.SchemaSharing.KeyGenerator.Model;
+
+namespace Xtensive.Orm.Tests.Storage.SchemaSharing.KeyGenerator
+{
+  public class MultidatabaseTest : SimpleTest
+  {
+    private const string AdditionalNodeId = "Additional";
+
+    protected override void CheckRequirements()
+    {
+      Require.AllFeaturesSupported(ProviderFeatures.Multidatabase);
+    }
+
+    protected override DomainConfiguration BuildConfiguration()
+    {
+      var configuration = base.BuildConfiguration();
+
+      configuration.MappingRules.Map(typeof (model.Part1.TestEntity1).Assembly, typeof (model.Part1.TestEntity1).Namespace).To("DO-Tests-1", "Model1");
+      configuration.MappingRules.Map(typeof (model.Part2.TestEntity3).Assembly, typeof (model.Part2.TestEntity3).Namespace).To("DO-Tests-1", "Model2");
+      configuration.MappingRules.Map(typeof (model.Part3.TestEntity5).Assembly, typeof (model.Part3.TestEntity5).Namespace).To("DO-Tests-2", "Model1");
+      configuration.MappingRules.Map(typeof (model.Part4.TestEntity7).Assembly, typeof (model.Part4.TestEntity7).Namespace).To("DO-Tests-2", "Model2");
+
+      configuration.DefaultDatabase = "DO-Tests-1";
+      configuration.DefaultSchema = "Model1";
+      return configuration;
+    }
+
+    protected override void FillInSkipParameters(Dictionary<NodeConfiguration, int> dictionary)
+    {
+      base.FillInSkipParameters(dictionary);
+      var additionalNode = new NodeConfiguration(AdditionalNodeId);
+      additionalNode.UpgradeMode = DomainUpgradeMode.Recreate;
+
+      additionalNode.DatabaseMapping.Add("DO-Tests-1", "DO-Tests-3");
+      additionalNode.DatabaseMapping.Add("DO-Tests-2", "DO-Tests-4");
+
+      additionalNode.SchemaMapping.Add("Model1", "Model3");
+      additionalNode.SchemaMapping.Add("Model2", "Model4");
+
+      dictionary.Add(additionalNode, 7);
+    }
+  }
+}
---
+++ Orm/Xtensive.Orm.Tests/Storage/SchemaSharing/KeyGenerator/SimpleTest.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Xtensive.Orm.Configuration;
+using Xtensive.Orm.Providers;
+using Xtensive.Orm.Tests.ObjectModel.Interfaces.Alphabet;
+using model = Xtensive.Orm.Tests.Storage.SchemaSharing.KeyGenerator.Model;
+
+namespace Xtensive.Orm.Tests.Storage.SchemaSharing.KeyGenerator
+{
+  public class SimpleTest : AutoBuildTest
+  {
+    private Dictionary<NodeConfiguration, int> skipStepsPerNode = new Dictionary<NodeConfiguration, int>();
+
+    [TestFixtureSetUp]
+    public void TestFixureSetUp()
+    {
+      CheckRequirements();
+      FillInSkipParameters(skipStepsPerNode);
+    }
+
+    [Test]
+    public void MainTest()
+    {
+      using (var referenceDomain = BuildDomain(BuildConfiguration().UseRecreate())) {
+
+        foreach (var node in skipStepsPerNode.Keys.Where(n => n.NodeId!=WellKnown.DefaultNodeId))
+          referenceDomain.StorageNodeManager.AddNode(node.UseRecreate());//node is in recreate;
+
+        var sequenceAccessor = referenceDomain.Services.Get<IStorageSequenceAccessor>();
+
+        var intSequence = referenceDomain.Model.Types[typeof (model.Part1.TestEntity1)].Hierarchy.Key.Sequence;
+        var longSequence = referenceDomain.Model.Types[typeof (model.Part1.TestEntity2)].Hierarchy.Key.Sequence;
+
+        foreach (var node in skipStepsPerNode) {
+          using (var session = referenceDomain.OpenSession()) {
+            session.SelectStorageNode(node.Key.NodeId);
+            var skipCount = node.Value;
+            for (int i = 0; i < skipCount; i++) {
+              sequenceAccessor.NextBulk(intSequence, session);
+              sequenceAccessor.NextBulk(longSequence, session);
+            }
+          }
+        }
+      }
+
+      using (var testDomain = BuildDomain(BuildConfiguration().UsePerformSafely().MakeNodesShareSchema())) {
+        foreach (var node in skipStepsPerNode.Keys.Where(n => n.NodeId!=WellKnown.DefaultNodeId))
+          testDomain.StorageNodeManager.AddNode(node.UsePerformSafely());
+
+        var sequenceAccessor = testDomain.Services.Get<IStorageSequenceAccessor>();
+
+        var intSequence = testDomain.Model.Types[typeof (model.Part1.TestEntity1)].Hierarchy.Key.Sequence;
+        var longSequence = testDomain.Model.Types[typeof (model.Part1.TestEntity2)].Hierarchy.Key.Sequence;
+
+        foreach (var node in skipStepsPerNode) {
+          using (var session = testDomain.OpenSession()) {
+            session.SelectStorageNode(node.Key.NodeId);
+            var expectedIntOffset = node.Value * 128 + 1;
+            var expectedLongOffset = node.Value * 128 + 1;
+
+            var actualIntOffest = sequenceAccessor.NextBulk(intSequence, session).Offset;
+            var actualLongOffset = sequenceAccessor.NextBulk(longSequence, session).Offset;
+
+            Assert.That(actualIntOffest, Is.EqualTo(expectedIntOffset));
+            Assert.That(actualLongOffset, Is.EqualTo(expectedLongOffset));
+          }
+        }
+      }
+    }
+
+    protected override DomainConfiguration BuildConfiguration()
+    {
+      var configuration = base.BuildConfiguration();
+
+      configuration.UpgradeMode = DomainUpgradeMode.Recreate;
+      configuration.Types.Register(typeof (model.Part1.TestEntity1).Assembly, typeof (model.Part1.TestEntity1).Namespace);
+      configuration.Types.Register(typeof (model.Part2.TestEntity3).Assembly, typeof (model.Part2.TestEntity3).Namespace);
+      configuration.Types.Register(typeof (model.Part3.TestEntity5).Assembly, typeof (model.Part3.TestEntity5).Namespace);
+      configuration.Types.Register(typeof (model.Part4.TestEntity7).Assembly, typeof (model.Part4.TestEntity7).Namespace);
+
+      return configuration;
+    }
+
+    protected virtual void FillInSkipParameters(Dictionary<NodeConfiguration, int> dictionary)
+    {
+      dictionary.Add(new NodeConfiguration(WellKnown.DefaultNodeId), 3);
+    }
+  }
+}
