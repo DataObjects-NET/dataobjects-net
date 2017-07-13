7ff3b69d315f73dd9256263aa7d97ccc392f1986
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2017-07-13 22:02:03 +0500

    Improved API of IFulltextCatalogResolver

--- Orm/Xtensive.Orm/Orm/Upgrade/FullTextCatalogResolver.cs
+++ Orm/Xtensive.Orm/Orm/Upgrade/FullTextCatalogResolver.cs
@@ -5,18 +5,72 @@
 // Created:    2017.06.12
 
 
+using Xtensive.Orm.Configuration;
+using Xtensive.Orm.Upgrade.Model;
+
 namespace Xtensive.Orm.Upgrade
 {
+  /// <summary>
+  /// Default <see cref="IFulltextCatalogResolver"/> implementation.
+  /// </summary>
   public class FullTextCatalogResolver : IFulltextCatalogResolver
   {
+    private const char NamePartsDelimeter = ':';
+
+    private readonly string defaultDatabase;
+    private readonly string defaultSchema;
+
+    public DomainConfiguration DomainConfiguration { get; private set; }
+
+    public NodeConfiguration NodeConfiguration { get; private set; }
+
+    /// <inheritdoc />
     public virtual bool IsEnabled
     {
       get { return true; }
     }
 
-    public virtual string Resolve()
+    /// <inheritdoc />
+    public string Resolve(Orm.Model.TypeInfo typeInfo, TableInfo tableInfo)
     {
-      return "Default";
+      var nameParts = GetNameParts(tableInfo.Name);
+      return Resolve(typeInfo, nameParts[0], nameParts[1], nameParts[2]);
+    }
+
+    protected virtual string Resolve(Orm.Model.TypeInfo typeInfo, string databaseName, string schemaName, string typeName)
+    {
+      return null;
+    }
+
+    private string[] GetNameParts(string complexName)
+    {
+      var tableName = string.Empty;
+      var schemaName = defaultSchema;
+      var databaseName = defaultDatabase;
+
+      var nameParts = complexName.Split(NamePartsDelimeter);
+      if (nameParts.Length == 3) {
+        tableName = nameParts[2];
+        schemaName = nameParts[1];
+        databaseName = nameParts[0];
+      }
+      else if (nameParts.Length==2) {
+        tableName = nameParts[1];
+        schemaName = nameParts[0];
+      }
+      else {
+        tableName = nameParts[0];
+      }
+      return new[] { databaseName, schemaName, tableName };
+    }
+
+    public FullTextCatalogResolver()
+    {
+      var context = UpgradeContext.Demand();
+      DomainConfiguration = context.Configuration;
+      NodeConfiguration = context.NodeConfiguration;
+      defaultDatabase = context.DefaultSchemaInfo.Database;
+      defaultSchema = context.DefaultSchemaInfo.Schema;
     }
   }
 }
--- Orm/Xtensive.Orm/Orm/Upgrade/Interfaces/IFulltextCatalogResolver.cs
+++ Orm/Xtensive.Orm/Orm/Upgrade/Interfaces/IFulltextCatalogResolver.cs
@@ -5,12 +5,27 @@
 // Created:    2017.06.12
 
 
+using Xtensive.Orm.Model;
+using Xtensive.Orm.Upgrade.Model;
+
 namespace Xtensive.Orm.Upgrade
 {
+  /// <summary>
+  /// A resolver responsible for name of full-text catalog.
+  /// </summary>
   public interface IFulltextCatalogResolver
   {
+    /// <summary>
+    /// Determines whether resolver is enabled.
+    /// </summary>
     bool IsEnabled { get; }
 
-    string Resolve();
+    /// <summary>
+    /// Resolves full-text catalog name.
+    /// </summary>
+    /// <param name="typeInfo"><see cref="TypeInfo">Domain model type.</see></param>
+    /// <param name="fulltextTable">Table with full-text index.</param>
+    /// <returns></returns>
+    string Resolve(TypeInfo typeInfo, TableInfo fulltextTable);
   }
 }
