5a5ec1fbc6685369435ec66797ef97b388e622f2
Julian Mamokin <julian1990@mail.ru>
2017-09-22 15:39:26 +0500

    Fixed indexes extraction query

--- Orm/Xtensive.Orm/Sql/Drivers/SqlServer/v09/Extractor.cs
+++ Orm/Xtensive.Orm/Sql/Drivers/SqlServer/v09/Extractor.cs
@@ -386,7 +386,7 @@ namespace Xtensive.Sql.Drivers.SqlServer.v09
   INNER JOIN {CATALOG}.sys.index_columns ic
     ON i.object_id = ic.object_id
       AND i.index_id = ic.index_id
-  WHERE i.type <> 3
+  WHERE i.type IN(1, 2, 4)
     AND schema_id {SCHEMA_FILTER}
   ORDER BY
     t.schema_id,
