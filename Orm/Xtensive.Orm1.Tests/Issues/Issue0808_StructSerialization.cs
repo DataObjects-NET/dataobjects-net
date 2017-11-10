3c67c2b824791913d44be48a680dc250ad49eb59
Julian Mamokin <julian1990@mail.ru>
2017-11-10 17:41:24 +0500

    Modified test to work with .net core

--- Orm/Xtensive.Orm1.Tests/Issues/Issue0808_StructSerialization.cs
+++ Orm/Xtensive.Orm1.Tests/Issues/Issue0808_StructSerialization.cs
@@ -22,11 +22,30 @@ public class Issue0808_StructSerialization
     var t = typeof (UnifiedCustomerID);
     Expression<Func<int, UnifiedCustomerID>> ex = a => new UnifiedCustomerID {Id = 2};
     var serializableExpression = ex.ToSerializableExpression();
-    var serializer = new NetDataContractSerializer();
     var memoryStream = new MemoryStream();
+
+#if NETCOREAPP
+      var serializer = new DataContractSerializer(typeof (SerializableLambdaExpression), new [] {
+      typeof (SerializableExpression),
+      typeof (SerializableMemberInitExpression),
+      typeof (SerializableMemberAssignment),
+      typeof (SerializableConstantExpression)
+    });
+
+    serializer.WriteObject(memoryStream, serializableExpression);
+#else
+    var serializer = new NetDataContractSerializer();
     serializer.Serialize(memoryStream, serializableExpression);
+#endif
+
     memoryStream.Position = 0;
+
+#if NETCOREAPP
+    var deserializedExpression = (SerializableLambdaExpression)serializer.ReadObject(memoryStream);
+#else
     var deserializedExpression = (SerializableExpression) serializer.Deserialize(memoryStream);
+#endif
+
     var ex2 = deserializedExpression.ToExpression();
   }
 }
