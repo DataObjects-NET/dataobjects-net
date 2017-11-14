2ebd71b41ab4f3cc41546c0c40af589389beca40
Julian Mamokin <julian1990@mail.ru>
2017-11-14 13:27:41 +0500

    Added provider files

---
+++ Orm/Xtensive.Orm1.Oracle/Orm.Providers.Oracle/DomainHandler.cs
@@ -0,0 +1,21 @@
+// Copyright (C) 2003-2010 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Denis Krjuchkov
+// Created:    2009.08.04
+
+using Xtensive.Orm.Rse.Compilation;
+
+namespace Xtensive.Orm.Providers.Oracle
+{
+  /// <summary>
+  /// A domain handler for Oracle RDBMS.
+  /// </summary>
+  public class DomainHandler : Providers.DomainHandler
+  {
+    protected override ICompiler CreateCompiler(CompilerConfiguration configuration)
+    {
+      return new SqlCompiler(Handlers, configuration);
+    }
+  }
+}
---
+++ Orm/Xtensive.Orm1.Oracle/Sql.Drivers.Oracle/v10/TypeMapper.cs
@@ -0,0 +1,38 @@
+// Copyright (C) 2010 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Denis Krjuchkov
+// Created:    2010.02.18
+
+using System;
+using System.Data.Common;
+using System.Diagnostics;
+using Oracle.DataAccess.Client;
+
+namespace Xtensive.Sql.Drivers.Oracle.v10
+{
+  internal class TypeMapper : v09.TypeMapper
+  {
+    public override void BindFloat(DbParameter parameter, object value)
+    {
+      var nativeParameter = (OracleParameter) parameter;
+      nativeParameter.OracleDbType = OracleDbType.BinaryFloat;
+      nativeParameter.Value = value ?? DBNull.Value;
+    }
+
+    public override void BindDouble(DbParameter parameter, object value)
+    {
+      var nativeParameter = (OracleParameter) parameter;
+      nativeParameter.OracleDbType = OracleDbType.BinaryDouble;
+      nativeParameter.Value = value ?? DBNull.Value;
+    }
+
+
+    // Constructor
+
+    public TypeMapper(SqlDriver driver)
+      : base(driver)
+    {
+    }
+  }
+}
