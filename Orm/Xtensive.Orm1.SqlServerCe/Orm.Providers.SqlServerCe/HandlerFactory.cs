60dc2deb26d46c3d844cca1c3cc2fdeb03c015d4
Julian Mamokin <julian1990@mail.ru>
2017-11-14 13:07:45 +0500

    Added project files

---
+++ Orm/Xtensive.Orm1.SqlServerCe/Orm.Providers.SqlServerCe/HandlerFactory.cs
@@ -0,0 +1,18 @@
+// Copyright (C) 2003-2010 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Gamzov
+// Created:    2008.07.04
+
+using Xtensive.Orm;
+
+namespace Xtensive.Orm.Providers.SqlServerCe
+{
+  /// <summary>
+  /// Storage provider for Microsoft SQL Server CE.
+  /// </summary>
+  [Provider(WellKnown.Provider.SqlServerCe, typeof (Xtensive.Sql.Drivers.SqlServerCe.DriverFactory))]
+  public class HandlerFactory : Providers.HandlerFactory
+  {
+  }
+}
