fba2d8baf3424a5979a119d5651db94114d3212d
Julian Mamokin <julian1990@mail.ru>
2017-11-14 14:06:52 +0500

    Modified Strings.Designer

--- Orm/Xtensive.Orm/Strings.Designer.cs
+++ Orm/Xtensive.Orm/Strings.Designer.cs
@@ -39,7 +39,7 @@ namespace Xtensive {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Xtensive.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Xtensive.Orm1.Strings", typeof(Strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
