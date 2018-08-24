1490f2a02ffb6758db594a4b01d9c7f545b4651e
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2018-08-24 18:42:31 +0500

    Removed temporary projects used for migration

--- Orm/Xtensive.Orm1.Tests/Issues/IssueJira0116_InterfacesCastAndIndexesModel/ICountry.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Xtensive.Orm.Tests.Issues.IssueJira0116_InterfacesCastAndIndexesModel
-{
-    public interface ICountry : IRecord
-    {
-        [Field(Length = 512)]
-        string Name{ get; set;}
-    }
-}
