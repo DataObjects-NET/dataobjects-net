5c81d644c676e5d613fc30030b7b159444b3628f
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2019-04-03 14:41:08 +0500

    Test improved
    - one-time database initialization
    - strings we search through and the ones we search for are moved to separate arrays
    - database-related tests are executed in one session and transaction
    - more cases of testing Like(pattern) and Like(pattern, escapeChar) for cosistency

--- Orm/Xtensive.Orm.Tests/Issues/IssueJira0746_LikeBehaviorDifferentOnClientSide.cs
+++ Orm/Xtensive.Orm.Tests/Issues/IssueJira0746_LikeBehaviorDifferentOnClientSide.cs
@@ -11,78 +11,160 @@ using Xtensive.Core;
 using Xtensive.Orm.Configuration;
 using Xtensive.Orm.Tests.Issues.IssueJira0746_MethodLikeDifferentBehaviorInSQLAndMemoryModel;
 
+namespace Xtensive.Orm.Tests.Issues.IssueJira0746_MethodLikeDifferentBehaviorInSQLAndMemoryModel
+{
+  [HierarchyRoot]
+  public class SimpleEntity : Entity
+  {
+    [Field, Key]
+    public int Id { get; private set; }
+
+    [Field]
+    public string Name { get; set; }
+  }
+}
+
 namespace Xtensive.Orm.Tests.Issues
 {
   public class IssueJira0746_LikeBehaviorDifferentOnClientSide : AutoBuildTest
   {
+    #region Test cases and searching phrases
+
+    public readonly string[] TestCases =
+    {
+      "&%nameazazazaza",
+      "azazazaza&%name",
+      "azazazaza&%nameazazazaza",
+      "azazazaza&&%name",
+      "&&%nameazazazaza",
+      "azazazaza&&%nameazazazaza",
+      "&&%name",
+      "&%name",
+      "%name",
+      "name%",
+      "&name",
+      "name&",
+      "name",
+    };
+
+    public readonly string[] HitSearchStrings = new[]
+    {
+      "&&&%name",
+      "&&%name",
+      "name",
+
+      "%&&&%name",
+      "%&&%name",
+      "%name",
+
+      "&&&%name%",
+      "&&%name%",
+      "name%",
+
+      "%&&&%name%",
+      "%&&%name%",
+      "%name%",
+    };
+
+    public readonly string[] MissSearchStrings = new[]
+    {
+      "&&&%abc",
+      "&&%abc",
+      "abc",
+      "%&&&%abc",
+      "%&&%abc",
+      "%abc",
+      "&&&%abc%",
+      "&&%abc%",
+      "abc%",
+      "%&&&%abc%",
+      "%&&%abc%",
+      "%abc%",
+    };
+
+    #endregion
+
+    public override void TestFixtureSetUp()
+    {
+      base.TestFixtureSetUp();
+      CreateSessionAndTransaction();
+    }
+
     protected override DomainConfiguration BuildConfiguration()
     {
       var configuration = base.BuildConfiguration();
-      configuration.Types.Register(typeof(SimpleEntity).Assembly, typeof(SimpleEntity).Namespace);
+      configuration.Types.Register(typeof (SimpleEntity).Assembly, typeof (SimpleEntity).Namespace);
+      configuration.UpgradeMode = DomainUpgradeMode.Recreate;
       return configuration;
     }
 
-    [Test]
-    public void MainTest()
+    protected override void PopulateData()
     {
       using (var session = Domain.OpenSession())
       using (var transaction = session.OpenTransaction()) {
-        new SimpleEntity {Name = "&&%name"};
-        new SimpleEntity {Name = "&%name"};
+        foreach (var testCase in TestCases)
+          new SimpleEntity {Name = testCase};
 
-        AreEqual(session, q => q.Single(z => z.Name.Like("&&&%name", '&')));
+        transaction.Complete();
       }
     }
 
     [Test]
-    public void Case1Test()
+    [TestCaseSource("HitSearchStrings")]
+    public void HitTest(string searchPattern)
     {
-      using (var session = Domain.OpenSession())
-      using (var transaction = session.OpenTransaction()) {
-        new SimpleEntity {Name = "azaza&%nameazaza"};
-        new SimpleEntity {Name = "&%name"};
-        new SimpleEntity {Name = "%name"};
+      var session = Session.Current;
+      var local = session.Query.All<SimpleEntity>().ToArray().Where(z => z.Name.Like(searchPattern, '&')).OrderBy(e=> e.Id).ToArray();
+      var server = session.Query.All<SimpleEntity>().Where(z => z.Name.Like(searchPattern, '&')).OrderBy(e => e.Id).ToArray();
 
-        AreEqual(session, q => q.Single(z => z.Name.Like("&&%name", '&')));
-        AreEqual(session, q => q.Single(z => z.Name.Like("&%name%", '&')));
+      Assert.That(server.Length, Is.Not.EqualTo(0));
+      Assert.That(local.Length, Is.EqualTo(server.Length));
+      Assert.That(local.SequenceEqual(server), Is.True);
+      
+    }
+
+    [Test]
+    [TestCaseSource("MissSearchStrings")]
+    public void MissTest(string searchPattern)
+    {
+      var session = Session.Current;
+      var local = session.Query.All<SimpleEntity>().ToArray().Where(z => z.Name.Like(searchPattern, '&')).OrderBy(e => e.Id).ToArray();
+      var server = session.Query.All<SimpleEntity>().Where(z => z.Name.Like(searchPattern, '&')).OrderBy(e => e.Id).ToArray();
+
+      Assert.That(server.Length, Is.EqualTo(0));
+      Assert.That(local.Length, Is.EqualTo(server.Length));
+    }
+
+    [Test]
+    public void LocalLikeConsistencyTest01()
+    {
+      var searchingWord = "name";
+      var positiveTestPhrases = new[] {
+        "name",
+      };
+
+      var negativeTestPhrases = new[] {
+        "This is how we do",
+        "There is no searching phrase in the phrase",
+        string.Empty,
+        "name is only what she knew about him",
+        "his name was realy strage so it popped out",
+        "And he asked for her name",
+      };
+
+      foreach (var positiveTestPhrase in positiveTestPhrases) {
+        Assert.That(positiveTestPhrase.Like(searchingWord), Is.True);
+        Assert.That(positiveTestPhrase.Like(searchingWord, '&'), Is.True);
+      }
+
+      foreach (var negativeTestPhrase in negativeTestPhrases) {
+        Assert.That(negativeTestPhrase.Like(searchingWord), Is.False);
+        Assert.That(negativeTestPhrase.Like(searchingWord, '&'), Is.False);
       }
     }
 
     [Test]
-    public void Case2Test()
-    {
-      using (var session = Domain.OpenSession())
-      using (var transaction = session.OpenTransaction()) {
-        new SimpleEntity {Name = "azaza&%nameazaza"};
-        new SimpleEntity {Name = "&%name"};
-        new SimpleEntity {Name = "%name"};
-        new SimpleEntity {Name = "name%"};
-
-        Assert.Throws<InvalidOperationException>(() => session.Query.All<SimpleEntity>().ToArray().Single(z => z.Name.Like("name", '&')));
-        Assert.Throws<InvalidOperationException>(() => session.Query.All<SimpleEntity>().Single(z => z.Name.Like("name", '&')));
-      }
-    }
-
-    [Test]
-    public void Case3Test()
-    {
-      using (var session = Domain.OpenSession())
-      using (var transaction = session.OpenTransaction()) {
-        new SimpleEntity {Name = "azaza&%nameazaza"};
-        new SimpleEntity {Name = "&%name"};
-        new SimpleEntity {Name = "%name"};
-        new SimpleEntity {Name = "name%"};
-        new SimpleEntity {Name = "&name"};
-        new SimpleEntity {Name = "name&"};
-        new SimpleEntity {Name = "name"};
-
-        session.Query.All<SimpleEntity>().ToArray().Single(z => z.Name.Like("name", '&'));
-        session.Query.All<SimpleEntity>().Single(z => z.Name.Like("name", '&'));
-      }
-    }
-
-    [Test]
-    public void Case4Test()
+    public void LocalLikeConsistencyTest02()
     {
       var searchingWord = "%name%";
       var positiveTestPhrases = new[] {
@@ -110,10 +192,11 @@ namespace Xtensive.Orm.Tests.Issues
     }
 
     [Test]
-    public void Case5Test()
+    public void LocalLikeConsistencyTest03()
     {
-      var searchingWord = "name";
+      var searchingWord = "name%";
       var positiveTestPhrases = new[] {
+        "name is only what she knew about him",
         "name",
       };
 
@@ -121,7 +204,6 @@ namespace Xtensive.Orm.Tests.Issues
         "This is how we do",
         "There is no searching phrase in the phrase",
         string.Empty,
-        "name is only what she knew about him",
         "his name was realy strage so it popped out",
         "And he asked for her name",
       };
@@ -137,25 +219,32 @@ namespace Xtensive.Orm.Tests.Issues
       }
     }
 
-    private void AreEqual(Session session, Func<IQueryable<SimpleEntity>, SimpleEntity> selectClause)
+    [Test]
+    public void LocalLikeConsistencyTest04()
     {
-      var queryable = session.Query.All<SimpleEntity>();
-      var ent1 = selectClause(queryable);
-      var ent2 = selectClause(queryable.ToArray().AsQueryable());
-      Assert.AreEqual(ent1, ent2);
+      var searchingWord = "%name";
+      var positiveTestPhrases = new[] {
+        "And he asked for her name",
+        "name",
+      };
+
+      var negativeTestPhrases = new[] {
+        "This is how we do",
+        "There is no searching phrase in the phrase",
+        string.Empty,
+        "name is only what she knew about him",
+        "his name was realy strage so it popped out",
+      };
+
+      foreach (var positiveTestPhrase in positiveTestPhrases) {
+        Assert.That(positiveTestPhrase.Like(searchingWord), Is.True);
+        Assert.That(positiveTestPhrase.Like(searchingWord, '&'), Is.True);
+      }
+
+      foreach (var negativeTestPhrase in negativeTestPhrases) {
+        Assert.That(negativeTestPhrase.Like(searchingWord), Is.False);
+        Assert.That(negativeTestPhrase.Like(searchingWord, '&'), Is.False);
+      }
     }
-  }
-}
-
-namespace Xtensive.Orm.Tests.Issues.IssueJira0746_MethodLikeDifferentBehaviorInSQLAndMemoryModel
-{
-  [HierarchyRoot]
-  public class SimpleEntity : Entity
-  {
-    [Field,Key]
-    public Guid Id { get; private set; }
-
-    [Field]
-    public string Name { get; set; }
   }
 }
