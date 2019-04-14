e3c87d0d29e30fc6288abe1208bf0cb4688b4d66
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2019-04-14 19:56:12 +0500

    ParametersLimitExidedException: Added summary

--- Orm/Xtensive.Orm/Orm/Exceptions/ParametersLimitExceededException.cs
+++ Orm/Xtensive.Orm/Orm/Exceptions/ParametersLimitExceededException.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
+﻿// Copyright (C) 2003-2010 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Kulakov
+// Created:    20.08.17
+
+using System;
 
 namespace Xtensive.Orm
 {
+  /// <summary>
+  /// An exception that is throws when number of parameters in <see cref="System.Data.Common.DbCommand"/> exeeded maximum value for the storage.
+  /// </summary>
+  [Serializable]
   public sealed class ParametersLimitExceededException : StorageException
   {
+    /// <summary>
+    /// Current number of parameters in <see cref="System.Data.Common.DbCommand"/> which caused exception.
+    /// </summary>
     public int CurrentParameterCount { get; private set; }
 
+    /// <summary>
+    /// Maximum number of parameters declared for <see cref="System.Data.Common.DbCommand"/> which is allowed.
+    /// </summary>
     public int MaxParameterCount { get; private set; }
 
     public ParametersLimitExceededException(int currentParameterCount, int maxParameterCount)
