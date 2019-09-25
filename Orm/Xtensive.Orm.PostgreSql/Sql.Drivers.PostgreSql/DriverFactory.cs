664e7400a12b8efc481fa5e333d490b679429d78
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2019-09-25 20:56:17 +0500

    PostgreSql: Implemented provider for v10

--- Orm/Xtensive.Orm.PostgreSql/Sql.Drivers.PostgreSql/DriverFactory.cs
+++ Orm/Xtensive.Orm.PostgreSql/Sql.Drivers.PostgreSql/DriverFactory.cs
@@ -88,7 +88,10 @@ namespace Xtensive.Sql.Drivers.PostgreSql
           return new v8_4.Driver(coreServerInfo);
         }
 
-        return new v9_0.Driver(coreServerInfo);
+        if (version.Major==9)
+          return new v9_0.Driver(coreServerInfo);
+
+        return new v10_0.Driver(coreServerInfo);
       }
     }
 
---
+++ Orm/Xtensive.Orm.PostgreSql/Sql.Drivers.PostgreSql/v10_0/Driver.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (C) 2019 Xtensive LLC.
+// All rights reserved.
+// For conditions of distribution and use, see license.
+// Created by: Alexey Kulakov
+// Created:    2019.09.25
+
+using Xtensive.Sql.Compiler;
+using Xtensive.Sql.Info;
+
+namespace Xtensive.Sql.Drivers.PostgreSql.v10_0
+{
+  internal class Driver : v9_0.Driver
+  {
+    protected override Sql.TypeMapper CreateTypeMapper()
+    {
+      return new TypeMapper(this);
+    }
+
+    protected override SqlCompiler CreateCompiler()
+    {
+      return new Compiler(this);
+    }
+
+    protected override Model.Extractor CreateExtractor()
+    {
+      return new Extractor(this);
+    }
+
+    protected override SqlTranslator CreateTranslator()
+    {
+      return new Translator(this);
+    }
+
+    protected override Info.ServerInfoProvider CreateServerInfoProvider()
+    {
+      return new ServerInfoProvider(this);
+    }
+
+    public Driver(CoreServerInfo coreServerInfo) : base(coreServerInfo)
+    {
+    }
+  }
+}
