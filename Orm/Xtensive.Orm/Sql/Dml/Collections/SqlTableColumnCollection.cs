dea0ab8cadb526e8cde846ca6705f1b31e3786b8
AlexUstinov <alex.ustinov@servicetitan.com>
2020-04-10 17:27:10 -0700

    Add column index by name to SqlTableColumnCollection

--- Orm/Xtensive.Orm/Sql/Dml/Collections/SqlTableColumnCollection.cs
+++ Orm/Xtensive.Orm/Sql/Dml/Collections/SqlTableColumnCollection.cs
@@ -3,8 +3,8 @@
 // For conditions of distribution and use, see license.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 
 namespace Xtensive.Sql.Dml
 {
@@ -12,36 +12,71 @@ namespace Xtensive.Sql.Dml
   /// Represents collection of <see cref="SqlColumn"/>s.
   /// </summary>
   [Serializable]
-  public class SqlTableColumnCollection
-    : ReadOnlyCollection<SqlTableColumn>
+  public class SqlTableColumnCollection : IReadOnlyList<SqlTableColumn>
   {
     private static readonly StringComparer Comparer = StringComparer.OrdinalIgnoreCase;
+    private readonly List<SqlTableColumn> columnList;
+    private readonly Dictionary<string, SqlTableColumn> columnLookup;
 
     /// <summary>
-    /// An indexer that provides access to collection items by their names.
-    /// Returns <see langword="null"/> if there is no such item.
+    /// Gets the number of elements contained in the <see cref="SqlTableColumnCollection"/>.
+    /// </summary>
+    public int Count => columnList.Count;
+
+    /// <inheritdoc cref="IEnumerable.GetEnumerator"/>>
+    IEnumerator IEnumerable.GetEnumerator() => columnList.GetEnumerator();
+
+    /// <inheritdoc cref="IEnumerable{T}.GetEnumerator"/>>
+    IEnumerator<SqlTableColumn> IEnumerable<SqlTableColumn>.GetEnumerator() => columnList.GetEnumerator();
+
+    /// <summary>
+    /// Returns a <see cref="List{T}.Enumerator"/> that iterates through the <see cref="SqlTableColumnCollection"/>.
+    /// </summary>
+    public List<SqlTableColumn>.Enumerator GetEnumerator() => columnList.GetEnumerator();
+
+    /// <summary>
+    /// Gets the column at the specified <paramref name="index"/>.
+    /// </summary>
+    public SqlTableColumn this[int index] => columnList[index];
+
+    /// <summary>
+    /// Gets the column with the specified <paramref name="name"/>
+    /// or <see langword="null"/> if collection doesn't contain such a column.
     /// </summary>
     public SqlTableColumn this[string name]
     {
-      get
-      {
-        if (string.IsNullOrEmpty(name))
+      get {
+        if (string.IsNullOrEmpty(name)) {
           return null;
-        foreach (SqlTableColumn column in this)
-          if (Comparer.Equals(column.Name, name))
-            return column;
-        return null;
+        }
+
+        if (columnLookup == null) {
+          return columnList.Find(column => Comparer.Equals(column.Name, name));
+        }
+
+        {
+          return columnLookup.TryGetValue(name, out var column) ? column : null;
+        }
       }
     }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="SqlTableColumnCollection"/> class.
     /// </summary>
-    /// <param name="list">The list to wrap.</param>
-    /// <exception cref="T:System.ArgumentNullException">list is null.</exception>
-    public SqlTableColumnCollection(IList<SqlTableColumn> list)
-      : base(list)
+    /// <param name="columns">A collection of <see cref="SqlTableColumn"/>s to be wrapped.</param>
+    public SqlTableColumnCollection(IReadOnlyCollection<SqlTableColumn> columns)
     {
+      if (columns.Count <= 8) {
+        columnList = new List<SqlTableColumn>(columns);
+      }
+      else {
+        columnList = new List<SqlTableColumn>(columns.Count);
+        columnLookup = new Dictionary<string, SqlTableColumn>(columns.Count, Comparer);
+        foreach (var column in columns) {
+          columnList.Add(column);
+          columnLookup.Add(column.Name, column);
+        }
+      }
     }
   }
 }
