4368df2f9492ef5897269dcd2bf1bef1cb6d9d1b
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2020-05-15 18:17:57 +0500

    Summary fix

--- Orm/Xtensive.Orm/Linq/FastExpression.cs
+++ Orm/Xtensive.Orm/Linq/FastExpression.cs
@@ -32,7 +32,7 @@ namespace Xtensive.Linq
     /// <summary>
     /// Generates <see cref="LambdaExpression"/> faster than <see cref="Expression.Lambda(Type,Expression,ParameterExpression[])"/>.
     /// </summary>
-    /// <param name="delegateType">A type that represents a delegate type.</param>
+    /// <typeparam name="TDelegate">A type that represents a delegate type.</typeparam>
     /// <param name="body">The body of lambda expression.</param>
     /// <param name="parameters">The parameters of lambda expression.</param>
     /// <returns>Constructed lambda expression.</returns>
