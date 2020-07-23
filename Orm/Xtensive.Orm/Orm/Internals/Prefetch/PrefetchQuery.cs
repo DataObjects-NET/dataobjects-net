a532ff9e5e6f0d0359877066fa09dd2e381f0940
AlexUstinov <alex.ustinov@servicetitan.com>
2020-07-22 21:31:09 -0700

    Add xml comments to PrefetchQuery struct.

--- Orm/Xtensive.Orm/Orm/Internals/Prefetch/PrefetchQuery.cs
+++ Orm/Xtensive.Orm/Orm/Internals/Prefetch/PrefetchQuery.cs
@@ -8,6 +8,12 @@ using Xtensive.Collections;
 
 namespace Xtensive.Orm.Internals.Prefetch
 {
+  /// <summary>
+  /// Represents query configured to perform additional fetch operations for the entities
+  /// reachable by following references from <typeparamref name="TElement"/>s of the
+  /// initial query result.
+  /// </summary>
+  /// <typeparam name="TElement">The type of the queried elements.</typeparam>
   public readonly struct PrefetchQuery<TElement> : IEnumerable<TElement>
   {
     private readonly Session session;
@@ -20,11 +26,16 @@ namespace Xtensive.Orm.Internals.Prefetch
       return node==null ? this : new PrefetchQuery<TElement>(session, source, nodes.Add(node));
     }
 
+    /// <inheritdoc />
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+    /// <inheritdoc />
     public IEnumerator<TElement> GetEnumerator() =>
       new PrefetchQueryEnumerable<TElement>(session, source, nodes).GetEnumerator();
 
+    /// <summary>
+    /// Transforms <see cref="PrefetchQuery{TElement}"/> to an <see cref="IAsyncEnumerable{T}"/> sequence.
+    /// </summary>
     public IAsyncEnumerable<TElement> AsAsyncEnumerable() =>
       new PrefetchQueryAsyncEnumerable<TElement>(session, source, nodes);
 
@@ -45,6 +56,18 @@ namespace Xtensive.Orm.Internals.Prefetch
       }
     }
 
+    /// <summary>
+    /// Asynchronously executes given <see cref="PrefetchQuery{TElement}"/> instance.
+    /// </summary>
+    /// <remarks>
+    /// This method internally puts all elements of the resulting sequence to a list
+    /// and then it wraps mentioned list as a <see cref="QueryResult{TItem}"/>.
+    /// As a consequence it is more efficient to use asynchronous enumeration over result of
+    /// <see cref="AsAsyncEnumerable"/> method call because it can perform lazily
+    /// not putting everything into intermediate list.
+    /// </remarks>
+    /// <param name="token"></param>
+    /// <returns></returns>
     public async Task<QueryResult<TElement>> ExecuteAsync(CancellationToken token = default)
     {
       var list = new List<TElement>();
