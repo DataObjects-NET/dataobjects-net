4bac417656eda639e543e7f1f2b2b5b939561f27
AlexUstinov <alex.ustinov@servicetitan.com>
2020-07-29 22:37:34 -0700

    Make Postgre sql extractor async

--- Orm/Xtensive.Orm.PostgreSql/Sql.Drivers.PostgreSql/DriverFactory.cs
+++ Orm/Xtensive.Orm.PostgreSql/Sql.Drivers.PostgreSql/DriverFactory.cs
@@ -7,8 +7,9 @@
 using System;
 using System.Data.Common;
 using System.Security;
+using System.Threading;
+using System.Threading.Tasks;
 using Npgsql;
-using Xtensive.Core;
 using Xtensive.Orm;
 using Xtensive.Sql.Info;
 using Xtensive.Sql.Drivers.PostgreSql.Resources;
@@ -20,7 +21,6 @@ namespace Xtensive.Sql.Drivers.PostgreSql
   /// </summary>
   public class DriverFactory : SqlDriverFactory
   {
-    private const string DataSourceFormat = "{0}:{1}/{2}";
     private const string DatabaseAndSchemaQuery = "select current_database(), current_schema()";
 
     /// <inheritdoc/>
@@ -30,28 +30,28 @@ namespace Xtensive.Sql.Drivers.PostgreSql
       SqlHelper.ValidateConnectionUrl(url);
 
       var builder = new NpgsqlConnectionStringBuilder();
-      
+
       // host, port, database
       builder.Host = url.Host;
-      if (url.Port!=0)
+      if (url.Port!=0) {
         builder.Port = url.Port;
+      }
+
       builder.Database = url.Resource ?? string.Empty;
 
       // user, password
-      if (!String.IsNullOrEmpty(url.User)) {
-#if NETSTANDARD
+      if (!string.IsNullOrEmpty(url.User)) {
         builder.Username = url.User;
-#else
-        builder.UserName = url.User;
-#endif
         builder.Password = url.Password;
       }
-      else
+      else {
         builder.IntegratedSecurity = true;
+      }
 
       // custom options
-      foreach (var param in url.Params)
+      foreach (var param in url.Params) {
         builder[param.Key] = param.Value;
+      }
 
       return builder.ToString();
     }
@@ -60,52 +60,75 @@ namespace Xtensive.Sql.Drivers.PostgreSql
     [SecuritySafeCritical]
     protected override SqlDriver CreateDriver(string connectionString, SqlDriverConfiguration configuration)
     {
-      using (var connection = new NpgsqlConnection(connectionString)) {
-        connection.Open();
-        SqlHelper.ExecuteInitializationSql(connection, configuration);
-        var version = string.IsNullOrEmpty(configuration.ForcedServerVersion)
-          ? connection.PostgreSqlVersion
-          : new Version(configuration.ForcedServerVersion);
-        var builder = new NpgsqlConnectionStringBuilder(connectionString);
-        var dataSource = string.Format(DataSourceFormat, builder.Host, builder.Port, builder.Database);
-        var defaultSchema = GetDefaultSchema(connection);
-        var coreServerInfo = new CoreServerInfo {
-          ServerVersion = version,
-          ConnectionString = connectionString,
-          MultipleActiveResultSets = false,
-          DatabaseName = defaultSchema.Database,
-          DefaultSchemaName = defaultSchema.Schema,
-        };
-
-        if (version.Major < 8 || version.Major==8 && version.Minor < 3)
-          throw new NotSupportedException(Strings.ExPostgreSqlBelow83IsNotSupported);
-
-        // We support 8.3, 8.4 and any 9.0+
-
-        if (version.Major==8) {
-          if (version.Minor==3)
-            return new v8_3.Driver(coreServerInfo);
-          return new v8_4.Driver(coreServerInfo);
-        }
-
-        if (version.Major==9) {
-          if (version.Minor==0)
-            return new v9_0.Driver(coreServerInfo);
-          return new v9_1.Driver(coreServerInfo);
-        }
-
-        if (version.Major < 12) {
-          return new v10_0.Driver(coreServerInfo);
-        }
-
-        return new v12_0.Driver(coreServerInfo);
-      }
+      using var connection = new NpgsqlConnection(connectionString);
+      connection.Open();
+      SqlHelper.ExecuteInitializationSql(connection, configuration);
+      var version = GetVersion(configuration, connection);
+      var defaultSchema = GetDefaultSchema(connection);
+      return CreateDriverInstance(connectionString, version, defaultSchema);
     }
 
     /// <inheritdoc/>
-    protected override DefaultSchemaInfo ReadDefaultSchema(DbConnection connection, DbTransaction transaction)
+    protected override async Task<SqlDriver> CreateDriverAsync(
+      string connectionString, SqlDriverConfiguration configuration, CancellationToken token)
     {
-      return SqlHelper.ReadDatabaseAndSchema(DatabaseAndSchemaQuery, connection, transaction);
+      var connection = new NpgsqlConnection(connectionString);
+      await using (connection.ConfigureAwait(false)) {
+        await connection.OpenAsync(token).ConfigureAwait(false);
+        await SqlHelper.ExecuteInitializationSqlAsync(connection, configuration, token).ConfigureAwait(false);
+        var version = GetVersion(configuration, connection);
+        var defaultSchema = await GetDefaultSchemaAsync(connection, token: token).ConfigureAwait(false);
+        return CreateDriverInstance(connectionString, version, defaultSchema);
+      }
     }
+
+    private static Version GetVersion(SqlDriverConfiguration configuration, NpgsqlConnection connection)
+    {
+      var version = string.IsNullOrEmpty(configuration.ForcedServerVersion)
+        ? connection.PostgreSqlVersion
+        : new Version(configuration.ForcedServerVersion);
+      return version;
+    }
+
+    private static SqlDriver CreateDriverInstance(
+      string connectionString, Version version, DefaultSchemaInfo defaultSchema)
+    {
+      var coreServerInfo = new CoreServerInfo {
+        ServerVersion = version,
+        ConnectionString = connectionString,
+        MultipleActiveResultSets = false,
+        DatabaseName = defaultSchema.Database,
+        DefaultSchemaName = defaultSchema.Schema,
+      };
+
+      if (version.Major < 8 || (version.Major == 8 && version.Minor < 3)) {
+        throw new NotSupportedException(Strings.ExPostgreSqlBelow83IsNotSupported);
+      }
+
+      // We support 8.3, 8.4 and any 9.0+
+
+      if (version.Major == 8) {
+        return version.Minor == 3 ? new v8_3.Driver(coreServerInfo) : new v8_4.Driver(coreServerInfo);
+      }
+
+      if (version.Major == 9) {
+        return version.Minor == 0 ? new v9_0.Driver(coreServerInfo) : new v9_1.Driver(coreServerInfo);
+      }
+
+      if (version.Major < 12) {
+        return new v10_0.Driver(coreServerInfo);
+      }
+
+      return new v12_0.Driver(coreServerInfo);
+    }
+
+    /// <inheritdoc/>
+    protected override DefaultSchemaInfo ReadDefaultSchema(DbConnection connection, DbTransaction transaction) =>
+      SqlHelper.ReadDatabaseAndSchema(DatabaseAndSchemaQuery, connection, transaction);
+
+    /// <inheritdoc/>
+    protected override Task<DefaultSchemaInfo> ReadDefaultSchemaAsync(
+      DbConnection connection, DbTransaction transaction, CancellationToken token) =>
+      SqlHelper.ReadDatabaseAndSchemaAsync(DatabaseAndSchemaQuery, connection, transaction, token);
   }
 }
--- Orm/Xtensive.Orm.PostgreSql/Sql.Drivers.PostgreSql/v8_3/Extractor.cs
+++ Orm/Xtensive.Orm.PostgreSql/Sql.Drivers.PostgreSql/v8_3/Extractor.cs
@@ -1,25 +1,26 @@
-﻿// Copyright (C) 2012-2020 Xtensive LLC.
+﻿// Copyright (C) 2009-2020 Xtensive LLC.
 // This code is distributed under MIT license terms.
 // See the License.txt file in the project root for more information.
 
-using System;
 using System.Data.Common;
-using Xtensive.Sql.Model;
 using Xtensive.Sql.Dml;
 using Index = Xtensive.Sql.Model.Index;
 
 namespace Xtensive.Sql.Drivers.PostgreSql.v8_3
 {
+  /// <inheritdoc/>
   internal class Extractor : v8_2.Extractor
   {
     private const int indoptionDesc = 0x0001;
 
+    /// <inheritdoc/>
     protected override void AddSpecialIndexQueryColumns(SqlSelect query, SqlTableRef spc, SqlTableRef rel, SqlTableRef ind, SqlTableRef depend)
     {
       base.AddSpecialIndexQueryColumns(query, spc, rel, ind, depend);
       query.Columns.Add(ind["indoption"]);
     }
 
+    /// <inheritdoc/>
     protected override void ReadSpecialIndexProperties(DbDataReader dr, Index i)
     {
       base.ReadSpecialIndexProperties(dr, i);
@@ -31,7 +32,7 @@ namespace Xtensive.Sql.Drivers.PostgreSql.v8_3
       }
     }
 
-    // Consructors
+    // Constructors
 
     public Extractor(SqlDriver driver)
       : base(driver)
