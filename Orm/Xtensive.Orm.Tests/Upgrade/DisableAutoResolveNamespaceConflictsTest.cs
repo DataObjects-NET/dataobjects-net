4de2b27fc3f4993e5223cb3166177ae181f27129
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2020-08-19 21:36:43 +0500

    Copyright changes

--- Orm/Xtensive.Orm.Tests/Upgrade/DisableAutoResolveNamespaceConflictsTest.cs
+++ Orm/Xtensive.Orm.Tests/Upgrade/DisableAutoResolveNamespaceConflictsTest.cs
@@ -1,6 +1,6 @@
-// Copyright (C) 2014 Xtensive LLC.
-// All rights reserved.
-// For conditions of distribution and use, see license.
+// Copyright (C) 2014-2020 Xtensive LLC.
+// This code is distributed under MIT license terms.
+// See the License.txt file in the project root for more information.
 // Created by: Alexey Kulakov
 // Created:    2014.05.29
 
