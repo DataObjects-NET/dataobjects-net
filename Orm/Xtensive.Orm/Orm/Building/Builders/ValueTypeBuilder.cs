4f5c5a4d5f1789dd68ecfb1607bdceb24b3888fc
AlexUstinov <alex.ustinov@servicetitan.com>
2020-09-09 20:03:59 -0700

    Fix code formatting in the ValueTypeBuilder class

--- Orm/Xtensive.Orm/Orm/Building/Builders/ValueTypeBuilder.cs
+++ Orm/Xtensive.Orm/Orm/Building/Builders/ValueTypeBuilder.cs
@@ -18,26 +18,26 @@ namespace Xtensive.Orm.Building.Builders
     {
       var fieldType = targetField.ValueType;
 
-      if (fieldType.IsAssignableFrom(value.GetType()))
+      if (fieldType.IsInstanceOfType(value)) {
         return value;
+      }
 
       // We can't do anything here cause we don't know the structure of referenced Entity's key
-      if (WellKnownOrmInterfaces.Entity.IsAssignableFrom(fieldType))
+      if (WellKnownOrmInterfaces.Entity.IsAssignableFrom(fieldType)) {
         return value;
+      }
 
       return AdjustValue(targetField.Name, fieldType, value);
     }
 
-    public static object AdjustValue(Model.FieldInfo targetField, Type targetType, object value)
-    {
-      return AdjustValue(targetField.Name, targetType, value);
-    }
+    public static object AdjustValue(Model.FieldInfo targetField, Type targetType, object value) =>
+      AdjustValue(targetField.Name, targetType, value);
 
     private static object AdjustValue(string fieldName, Type fieldType, object value)
     {
       var valueType = fieldType.StripNullable();
 
-      if (valueType==WellKnownTypes.Guid) {
+      if (valueType == WellKnownTypes.Guid) {
         Guid guid;
         try {
           guid = new Guid((string) value);
@@ -45,13 +45,15 @@ namespace Xtensive.Orm.Building.Builders
         catch (FormatException) {
           throw FailToParseValue(fieldName, value);
         }
+
         return guid;
       }
 
-      if (valueType==WellKnownTypes.TimeSpan) {
-        TimeSpan timespan;
-        if (value is string && !TimeSpan.TryParse((string) value, out timespan))
-          throw FailToParseValue(fieldName, value);
+      if (valueType == WellKnownTypes.TimeSpan) {
+        if (value is string timespanString && !TimeSpan.TryParse(timespanString, out var timespan)) {
+          throw FailToParseValue(fieldName, timespanString);
+        }
+
         var ticks = (long) Convert.ChangeType(value, WellKnownTypes.Int64);
         timespan = TimeSpan.FromTicks(ticks);
         return timespan;
@@ -60,9 +62,7 @@ namespace Xtensive.Orm.Building.Builders
       return Convert.ChangeType(value, valueType);
     }
 
-    private static DomainBuilderException FailToParseValue(string fieldName, object value)
-    {
-      return new DomainBuilderException(String.Format(Strings.ExUnableToParseValueXForFieldY, value, fieldName));
-    }
+    private static DomainBuilderException FailToParseValue(string fieldName, object value) =>
+      new DomainBuilderException(string.Format(Strings.ExUnableToParseValueXForFieldY, value, fieldName));
   }
 }
