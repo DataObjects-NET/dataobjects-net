7f32990d198a3f0bd42cd504ccce2f41c55d4754
AlexUstinov <alex.ustinov@servicetitan.com>
2020-09-09 19:35:36 -0700

    Fix code formatting in the DomainBuilder class

--- Orm/Xtensive.Orm/Orm/Building/Builders/DomainBuilder.cs
+++ Orm/Xtensive.Orm/Orm/Building/Builders/DomainBuilder.cs
@@ -30,10 +30,10 @@ namespace Xtensive.Orm.Building.Builders
     /// <returns>Built domain.</returns>
     public static Domain Run(DomainBuilderConfiguration builderConfiguration)
     {
-      ArgumentValidator.EnsureArgumentNotNull(builderConfiguration, "builderConfiguration");
+      ArgumentValidator.EnsureArgumentNotNull(builderConfiguration, nameof(builderConfiguration));
 
       var context = new BuildingContext(builderConfiguration);
-      using (BuildLog.InfoRegion(Strings.LogBuildingX, typeof (Domain).GetShortName())) {
+      using (BuildLog.InfoRegion(Strings.LogBuildingX, typeof(Domain).GetShortName())) {
         new DomainBuilder(context).Run();
       }
 
@@ -51,12 +51,12 @@ namespace Xtensive.Orm.Building.Builders
 
     private void CreateDomain()
     {
-      using (BuildLog.InfoRegion(Strings.LogCreatingX, typeof (Domain).GetShortName())) {
+      using (BuildLog.InfoRegion(Strings.LogCreatingX, typeof(Domain).GetShortName())) {
         var services = context.BuilderConfiguration.Services;
         var useSingleConnection =
           services.ProviderInfo.Supports(ProviderFeatures.SingleConnection)
-          && context.BuilderConfiguration.Stage==UpgradeStage.Final;
-        
+          && context.BuilderConfiguration.Stage == UpgradeStage.Final;
+
         context.Domain = new Domain(
           context.Configuration,
           context.BuilderConfiguration.UpgradeContextCookie,
@@ -70,7 +70,7 @@ namespace Xtensive.Orm.Building.Builders
       var handlers = context.Domain.Handlers;
       var services = context.BuilderConfiguration.Services;
 
-      using (BuildLog.InfoRegion(Strings.LogCreatingX, typeof (DomainHandler).GetShortName())) {
+      using (BuildLog.InfoRegion(Strings.LogCreatingX, typeof(DomainHandler).GetShortName())) {
         // HandlerFactory
         handlers.Factory = services.HandlerFactory;
 
@@ -94,11 +94,12 @@ namespace Xtensive.Orm.Building.Builders
 
     private void CreateServices()
     {
-      using (BuildLog.InfoRegion(Strings.LogCreatingX, typeof (IServiceContainer).GetShortName())) {
+      using (BuildLog.InfoRegion(Strings.LogCreatingX, typeof(IServiceContainer).GetShortName())) {
         var domain = context.Domain;
         var configuration = domain.Configuration;
-        var userContainerType = configuration.ServiceContainerType ?? typeof (ServiceContainer);
-        var registrations = CreateServiceRegistrations(configuration).Concat(KeyGeneratorFactory.GetRegistrations(context));
+        var userContainerType = configuration.ServiceContainerType ?? typeof(ServiceContainer);
+        var registrations = CreateServiceRegistrations(configuration)
+          .Concat(KeyGeneratorFactory.GetRegistrations(context));
         var systemContainer = domain.CreateSystemServices();
         var userContainer = ServiceContainer.Create(userContainerType, systemContainer);
         domain.Services = new ServiceContainer(registrations, userContainer);
@@ -123,7 +124,7 @@ namespace Xtensive.Orm.Building.Builders
         var generators = domain.KeyGenerators;
         var keysToProcess = domain.Model.Hierarchies
           .Select(h => h.Key)
-          .Where(k => k.GeneratorKind!=KeyGeneratorKind.None);
+          .Where(k => k.GeneratorKind != KeyGeneratorKind.None);
         foreach (var keyInfo in keysToProcess) {
           var generator = domain.Services.Demand<KeyGenerator>(keyInfo.GeneratorName);
           generators.Register(keyInfo, generator);
@@ -132,7 +133,7 @@ namespace Xtensive.Orm.Building.Builders
           }
 
           var temporaryGenerator = domain.Services.Get<TemporaryKeyGenerator>(keyInfo.GeneratorName);
-          if (temporaryGenerator==null) {
+          if (temporaryGenerator == null) {
             continue; // Temporary key generators are optional
           }
 
@@ -141,6 +142,7 @@ namespace Xtensive.Orm.Building.Builders
             temporaryGenerator.Initialize(context.Domain, keyInfo.TupleDescriptor);
           }
         }
+
         generators.Lock();
       }
 
