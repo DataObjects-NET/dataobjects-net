04d5efb451aac369d1d034f5352aa75c2d2d0833
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2020-09-23 17:18:09 +0500

    Test class reformatting

--- Orm/Xtensive.Orm.Tests/Issues/IssueGitHub0070_IncludeProviderParameterMismatchWithinParameterContext.cs
+++ Orm/Xtensive.Orm.Tests/Issues/IssueGitHub0070_IncludeProviderParameterMismatchWithinParameterContext.cs
@@ -31,41 +31,39 @@ namespace Xtensive.Orm.Tests.Issues
     protected override DomainConfiguration BuildConfiguration()
     {
       var config = base.BuildConfiguration();
-      config.Types.Register(Assembly.GetExecutingAssembly(), typeof (Token).Namespace);
+      config.Types.Register(typeof(Token).Assembly, typeof(Token).Namespace);
       return config;
     }
 
     [Test]
     public void TwoDifferentInStatementsInTheSameQuery()
     {
-      using (var session = Domain.OpenSession()) {
-        using (session.OpenTransaction()) {
-          var tokenId = Guid.NewGuid();
-          var token = new Token(session, tokenId) {
-            Salt = -1
-          };
-          session.SaveChanges();
-          var query = session.Query.All<Token>()
-            .Where(t => t.Salt.In(-1, 1) && t.Id.In(tokenId, Guid.Empty));
-          Assert.AreSame(token, query.FirstOrDefault());
-        }
+      using (var session = Domain.OpenSession())
+      using (session.OpenTransaction()) {
+        var tokenId = Guid.NewGuid();
+        var token = new Token(session, tokenId) {
+          Salt = -1
+        };
+        session.SaveChanges();
+        var query = session.Query.All<Token>()
+          .Where(t => t.Salt.In(-1, 1) && t.Id.In(tokenId, Guid.Empty));
+        Assert.AreSame(token, query.FirstOrDefault());
       }
     }
 
     [Test]
     public void TwoSimilarInStatementsInTheSameQuery()
     {
-      using (var session = Domain.OpenSession()) {
-        using (session.OpenTransaction()) {
-          var tokenId = Guid.NewGuid();
-          var token = new Token(session, tokenId) {
-            Salt = -1
-          };
-          session.SaveChanges();
-          var query = session.Query.All<Token>()
-            .Where(t => t.Salt.In(-1, 1) || t.Salt.In(4, 5));
-          Assert.AreSame(token, query.FirstOrDefault());
-        }
+      using (var session = Domain.OpenSession())
+      using (session.OpenTransaction()) {
+        var tokenId = Guid.NewGuid();
+        var token = new Token(session, tokenId) {
+          Salt = -1
+        };
+        session.SaveChanges();
+        var query = session.Query.All<Token>()
+          .Where(t => t.Salt.In(-1, 1) || t.Salt.In(4, 5));
+        Assert.AreSame(token, query.FirstOrDefault());
       }
     }
   }
