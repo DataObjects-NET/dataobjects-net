1e698c0eddc425cc7ab239beeda6a6b69812adf1
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2020-09-28 19:31:12 +0500

    Adds CheckRequirements to base test class for extensions

--- Extensions/TestCommon/CommonModelTest.cs
+++ Extensions/TestCommon/CommonModelTest.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+// Copyright (C) 2019-2020 Xtensive LLC.
+// This code is distributed under MIT license terms.
+// See the License.txt file in the project root for more information.
+
+using System.Collections.Generic;
 using NUnit.Framework;
 using TestCommon.Model;
 using Xtensive.Core;
@@ -53,6 +57,10 @@ namespace TestCommon
     }
 
     protected virtual void PopulateData()
+    {
+    }
+
+    protected virtual void CheckRequirements()
     {
     }
   }
