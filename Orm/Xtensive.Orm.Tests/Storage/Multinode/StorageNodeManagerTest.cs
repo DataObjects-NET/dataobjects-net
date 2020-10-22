31166373ebd74e193159d946583d973d8f3a789d
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2020-10-22 20:34:08 +0500

    Adds test for StorageNodeManager API

---
+++ Orm/Xtensive.Orm.Tests/Storage/Multinode/StorageNodeManagerTest.cs
@@ -0,0 +1,215 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Xtensive.Orm.Configuration;
+using Xtensive.Orm.Providers;
+
+namespace Xtensive.Orm.Tests.Storage.Multinode
+{
+  public class StorageNodeManagerTest : AutoBuildTest
+  {
+    [HierarchyRoot]
+    public class TestEntity : Entity
+    {
+      [Field, Key]
+      public int Id { get; private set; }
+    }
+
+    private readonly List<string> NodesToClear = new List<string>();
+
+    protected override void CheckRequirements()
+    {
+      Require.ProviderIs(StorageProvider.SqlServer);
+    }
+
+    protected override DomainConfiguration BuildConfiguration()
+    {
+      var config = base.BuildConfiguration();
+      config.Types.Register(typeof(TestEntity));
+      config.DefaultSchema = "dbo";
+      return config;
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+      NodesToClear.Clear();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      foreach (var nodeName in NodesToClear) {
+        _ = Domain.StorageNodeManager.RemoveNode(nodeName);
+      }
+    }
+
+    [Test]
+    public void AddUniqueNodeTest()
+    {
+      var nodeConfiguration = GetBaseNodeConfig();
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      var result = Domain.StorageNodeManager.AddNode(nodeConfiguration);
+      Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void AddAlreadyExistingTest()
+    {
+      var nodeConfiguration = GetBaseNodeConfig();
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      _ = Domain.StorageNodeManager.AddNode(nodeConfiguration);
+
+      var sameConfig = (NodeConfiguration)nodeConfiguration.Clone();
+      var result = Domain.StorageNodeManager.AddNode(sameConfig);
+      Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void AddNodeWithNullNameTest()
+    {
+      var nodeConfiguration = new NodeConfiguration() { UpgradeMode = DomainUpgradeMode.Recreate };
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      _ = Assert.Throws<InvalidOperationException>(() => Domain.StorageNodeManager.AddNode(nodeConfiguration));
+    }
+
+    [Test]
+    public void AddNodeWithEmptyNameTest()
+    {
+      var nodeConfiguration = new NodeConfiguration(string.Empty) { UpgradeMode = DomainUpgradeMode.Recreate };
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      _ = Assert.Throws<InvalidOperationException>(() => Domain.StorageNodeManager.AddNode(nodeConfiguration));
+    }
+
+    [Test]
+    public void AddNodeWithNullConfigTest()
+    {
+      _ = Assert.Throws<ArgumentNullException>(() => Domain.StorageNodeManager.AddNode(null));
+    }
+
+    [Test]
+    public void AddNodeForMultidatabaseDomainTest()
+    {
+      var nodeConfiguration = new NodeConfiguration() { UpgradeMode = DomainUpgradeMode.Recreate };
+      nodeConfiguration.DatabaseMapping.Add("DO-Tests", "DO-Tests-1");
+      _ = Assert.Throws<InvalidOperationException>(() => Domain.StorageNodeManager.AddNode(nodeConfiguration));
+    }
+
+    [Test]
+    public void GetNodeByNullStringTest()
+    {
+      _ = Assert.Throws<InvalidOperationException>(() => Domain.StorageNodeManager.GetNode(null));
+    }
+
+    [Test]
+    public void GetNodeByEmptyStringTest()
+    {
+      var nodeConfiguration = GetBaseNodeConfig();
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      var result = Domain.StorageNodeManager.AddNode(nodeConfiguration);
+      Assert.That(result, Is.True);
+
+      var node = Domain.StorageNodeManager.GetNode(string.Empty);
+
+      Assert.That(node, Is.Not.Null);
+      Assert.That(node.Id, Is.Not.EqualTo(nodeConfiguration.NodeId));
+    }
+
+    [Test]
+    public void GetNodeByExistingNodeNameTest()
+    {
+      var nodeConfiguration = GetBaseNodeConfig();
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      var result = Domain.StorageNodeManager.AddNode(nodeConfiguration);
+      Assert.That(result, Is.True);
+
+      var node = Domain.StorageNodeManager.GetNode(nodeConfiguration.NodeId);
+
+      Assert.That(node, Is.Not.Null);
+      Assert.That(node.Id, Is.EqualTo(nodeConfiguration.NodeId));
+    }
+
+    [Test]
+    public void GetNodeWhichDoesntExistTest()
+    {
+      var nodeConfiguration = GetBaseNodeConfig();
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      var result = Domain.StorageNodeManager.AddNode(nodeConfiguration);
+      Assert.That(result, Is.True);
+
+      var node = Domain.StorageNodeManager.GetNode(nodeConfiguration.NodeId + "dummy");
+
+      Assert.That(node, Is.Null);
+    }
+
+    [Test]
+    public void RemoveNodeWhichDoesntExistTest()
+    {
+      var result = Domain.StorageNodeManager.RemoveNode("dummy");
+      Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void RemoveExistingNode()
+    {
+      var nodeConfiguration = GetBaseNodeConfig();
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      var result = Domain.StorageNodeManager.AddNode(nodeConfiguration);
+      Assert.That(result, Is.True);
+
+      result = Domain.StorageNodeManager.RemoveNode(nodeConfiguration.NodeId);
+      Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void GetOrAddByConfigWithNullNameTest()
+    {
+      var nodeConfiguration = new NodeConfiguration() { UpgradeMode = DomainUpgradeMode.Recreate };
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      _ = Assert.Throws<InvalidOperationException>(() => Domain.StorageNodeManager.GetOrAddNode(nodeConfiguration));
+    }
+
+    [Test]
+    public void GetOrAddByConfigWithEmptyNameTest()
+    {
+      var nodeConfiguration = new NodeConfiguration(string.Empty) { UpgradeMode = DomainUpgradeMode.Recreate };
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      _ = Assert.Throws<InvalidOperationException>(() => Domain.StorageNodeManager.GetOrAddNode(nodeConfiguration));
+    }
+
+    [Test]
+    public void GetOrAddByConfigOfExistingNodeTest()
+    {
+      var nodeConfiguration = GetBaseNodeConfig();
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      var result = Domain.StorageNodeManager.AddNode(nodeConfiguration);
+      Assert.That(result, Is.True);
+      var existingNode = Domain.StorageNodeManager.GetNode(nodeConfiguration.NodeId);
+
+      var node = Domain.StorageNodeManager.GetOrAddNode((NodeConfiguration)nodeConfiguration.Clone());
+      Assert.That(node, Is.EqualTo(existingNode));
+    }
+
+    [Test]
+    public void GetOrAddByConfigOfAbsentNodeTest()
+    {
+      var nodeConfiguration = GetBaseNodeConfig();
+      nodeConfiguration.SchemaMapping.Add("dbo", "Model1");
+      var existingNode = Domain.StorageNodeManager.GetNode(nodeConfiguration.NodeId);
+      Assert.That(existingNode, Is.Null);
+
+      var node = Domain.StorageNodeManager.GetOrAddNode((NodeConfiguration) nodeConfiguration.Clone());
+      Assert.That(node, Is.Not.Null);
+    }
+
+    private NodeConfiguration GetBaseNodeConfig()
+    {
+      var nodeConfiguration = new NodeConfiguration(Guid.NewGuid().ToString()) {
+        UpgradeMode = DomainUpgradeMode.Recreate
+      };
+      NodesToClear.Add(nodeConfiguration.NodeId);
+      return nodeConfiguration;
+    }
+  }
+}
