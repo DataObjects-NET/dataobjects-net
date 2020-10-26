dfbbcc371e10c315c324249ff7255d1466bd1fae
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2020-10-26 21:15:51 +0500

    Files' copyright update

--- Orm/Xtensive.Orm.Tests/Storage/Multinode/ObsoleteSelectStorageNodeTest.cs
+++ Orm/Xtensive.Orm.Tests/Storage/Multinode/ObsoleteSelectStorageNodeTest.cs
@@ -1,3 +1,7 @@
+// Copyright (C) 2014-2020 Xtensive LLC.
+// This code is distributed under MIT license terms.
+// See the License.txt file in the project root for more information.
+
 using System;
 using System.Collections.Generic;
 using System.Text;
