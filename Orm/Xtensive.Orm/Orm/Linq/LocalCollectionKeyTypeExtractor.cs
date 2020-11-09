0f5e585cbc2edb62034a2cf3ae22f2c427796a13
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2020-11-09 16:03:47 +0500

    Improves LocalCollectionKeyTypeExtractor
    If part of BinaryExpression  is already KeyExpression then
    we can get KeyType form it

--- Orm/Xtensive.Orm/Orm/Linq/LocalCollectionKeyTypeExtractor.cs
+++ Orm/Xtensive.Orm/Orm/Linq/LocalCollectionKeyTypeExtractor.cs
@@ -1,6 +1,6 @@
-﻿// Copyright (C) 2013 Xtensive LLC.
-// All rights reserved.
-// For conditions of distribution and use, see license.
+// Copyright (C) 2013-2020 Xtensive LLC.
+// This code is distributed under MIT license terms.
+// See the License.txt file in the project root for more information.
 // Created by: Alexey Kulakov
 // Created:    2013.12.30
 
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using Xtensive.Core;
+using Xtensive.Orm.Linq.Expressions;
 
 namespace Xtensive.Orm.Linq
 {
@@ -18,28 +19,34 @@ namespace Xtensive.Orm.Linq
     public static Type Extract(BinaryExpression expression)
     {
       ArgumentValidator.EnsureArgumentNotNull(expression, "expression");
+      if (expression.Right.StripMarkers() is KeyExpression key) {
+        return key.EntityType.UnderlyingType;
+      }
+
       var expr = VisitBinaryExpession(expression);
-      if (expr.Type.IsSubclassOf(typeof (Entity)))
+      if (expr.Type.IsSubclassOf(typeof(Entity))) {
         return expr.Type;
+      }
+
       throw new NotSupportedException(string.Format(Strings.ExCurrentTypeXIsNotSupported, expr.Type));
     }
 
     private static Expression VisitBinaryExpession(BinaryExpression binaryExpression)
     {
-      var memberExpression = binaryExpression.Right as MemberExpression;
-      if (memberExpression==null)
-        throw new InvalidOperationException(string.Format(Strings.ExCantConvertXToY, binaryExpression.Type, typeof (MemberExpression)));
+      if (!(binaryExpression.Right is MemberExpression memberExpression)) {
+        throw new InvalidOperationException(string.Format(Strings.ExCantConvertXToY, binaryExpression.Type, typeof(MemberExpression)));
+      }
       return VisitMemberExpression(memberExpression);
     }
 
     private static Expression VisitMemberExpression(MemberExpression memberExpression)
     {
-      var parameter = memberExpression.Expression as ParameterExpression;
-      if (parameter!=null)
+      if (memberExpression.Expression is ParameterExpression parameter) {
         return parameter;
-      var member = memberExpression.Expression as MemberExpression;
-      if (member!=null)
+      }
+      if (memberExpression.Expression is MemberExpression member) {
         return member;
+      }
       throw new NotSupportedException(string.Format(Strings.ExCurrentTypeOfExpressionXIsNotSupported, memberExpression.Expression.Type));
     }
   }
