d30ed32e131371ef06fd7d823cd4a6d872694945
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2020-11-12 21:33:05 +0500

    Code-style improvements

--- Extensions/Xtensive.Orm.BulkOperations/Internals/QueryOperation.cs
+++ Extensions/Xtensive.Orm.BulkOperations/Internals/QueryOperation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using Xtensive.Core;
 using Xtensive.Orm.Linq;
 using Xtensive.Orm.Model;
 using Xtensive.Orm.Rse;
@@ -14,7 +15,7 @@ namespace Xtensive.Orm.BulkOperations
   internal abstract class QueryOperation<T> : Operation<T>
     where T : class, IEntity
   {
-    private readonly static MethodInfo inMethod = GetInMethod();
+    private readonly static MethodInfo InMethod = GetInMethod();
     protected IQueryable<T> query;
 
     protected QueryOperation(QueryProvider queryProvider)
@@ -24,55 +25,55 @@ namespace Xtensive.Orm.BulkOperations
 
     private static MethodInfo GetInMethod()
     {
-      foreach (var method in typeof (QueryableExtensions).GetMethods().Where(a => string.Equals(a.Name, "In", StringComparison.Ordinal))) {
+      foreach (var method in typeof(QueryableExtensions).GetMethods().Where(a => string.Equals(a.Name, "In", StringComparison.Ordinal))) {
         var parameters = method.GetParameters();
-        if (parameters.Length == 3 && string.Equals(parameters[2].ParameterType.Name, "IEnumerable`1", StringComparison.Ordinal))
+        if (parameters.Length == 3 && string.Equals(parameters[2].ParameterType.Name, "IEnumerable`1", StringComparison.Ordinal)) {
           return method;
+        }
       }
       return null;
     }
 
     protected override int ExecuteInternal()
     {
-      Expression e = query.Expression.Visit((MethodCallExpression ex) =>
-        {
-          var methodInfo = ex.Method;
-          //rewrite localCollection.Contains(entity.SomeField) -> entity.SomeField.In(localCollection)
-          if (methodInfo.DeclaringType == typeof(Enumerable) &&
-              string.Equals(methodInfo.Name, "Contains", StringComparison.Ordinal) &&
-              ex.Arguments.Count == 2) {
-            var localCollection = ex.Arguments[0];//IEnumerable<T>
-            var valueToCheck = ex.Arguments[1];
-            var genericInMethod = inMethod.MakeGenericMethod(new[] { valueToCheck.Type });
-            ex = Expression.Call(genericInMethod, valueToCheck, Expression.Constant(IncludeAlgorithm.ComplexCondition), localCollection);
-            methodInfo = ex.Method;
-          }
+      var e = query.Expression.Visit((MethodCallExpression ex) => {
+        var methodInfo = ex.Method;
+        //rewrite localCollection.Contains(entity.SomeField) -> entity.SomeField.In(localCollection)
+        if (methodInfo.DeclaringType == typeof(Enumerable) &&
+            string.Equals(methodInfo.Name, "Contains", StringComparison.Ordinal) &&
+            ex.Arguments.Count == 2) {
+          var localCollection = ex.Arguments[0];//IEnumerable<T>
+          var valueToCheck = ex.Arguments[1];
+          var genericInMethod = InMethod.MakeGenericMethod(new[] { valueToCheck.Type });
+          ex = Expression.Call(genericInMethod, valueToCheck, Expression.Constant(IncludeAlgorithm.ComplexCondition), localCollection);
+          methodInfo = ex.Method;
+        }
 
-          if (methodInfo.DeclaringType == typeof(QueryableExtensions) &&
-              string.Equals(methodInfo.Name, "In", StringComparison.Ordinal) &&
-              ex.Arguments.Count > 1) {
-            if (ex.Arguments[1].Type == typeof(IncludeAlgorithm)) {
-              var algorithm = (IncludeAlgorithm) ex.Arguments[1].Invoke();
-              if (algorithm == IncludeAlgorithm.TemporaryTable) {
-                throw new NotSupportedException("IncludeAlgorithm.TemporaryTable is not supported");
-              }
-              if (algorithm == IncludeAlgorithm.Auto) {
-                List<Expression> arguments = ex.Arguments.ToList();
-                arguments[1] = Expression.Constant(IncludeAlgorithm.ComplexCondition);
-                ex = Expression.Call(methodInfo, arguments);
-              }
+        if (methodInfo.DeclaringType == typeof(QueryableExtensions) &&
+            string.Equals(methodInfo.Name, "In", StringComparison.Ordinal) &&
+            ex.Arguments.Count > 1) {
+          if (ex.Arguments[1].Type == typeof(IncludeAlgorithm)) {
+            var algorithm = (IncludeAlgorithm) ex.Arguments[1].Invoke();
+            if (algorithm == IncludeAlgorithm.TemporaryTable) {
+              throw new NotSupportedException("IncludeAlgorithm.TemporaryTable is not supported");
             }
-            else {
-              List<Expression> arguments = ex.Arguments.ToList();
-              arguments.Insert(1, Expression.Constant(IncludeAlgorithm.ComplexCondition));
-              List<Type> types = methodInfo.GetParameters().Select(a => a.ParameterType).ToList();
-              types.Insert(1, typeof(IncludeAlgorithm));
-              ex = Expression.Call(inMethod.MakeGenericMethod(methodInfo.GetGenericArguments()),
-                                   arguments.ToArray());
+            if (algorithm == IncludeAlgorithm.Auto) {
+              var arguments = ex.Arguments.ToList();
+              arguments[1] = Expression.Constant(IncludeAlgorithm.ComplexCondition);
+              ex = Expression.Call(methodInfo, arguments);
             }
           }
-          return ex;
-        });
+          else {
+            var arguments = ex.Arguments.ToList();
+            arguments.Insert(1, Expression.Constant(IncludeAlgorithm.ComplexCondition));
+            var types = methodInfo.GetParameters().Select(a => a.ParameterType).ToList();
+            types.Insert(1, typeof(IncludeAlgorithm));
+            ex = Expression.Call(InMethod.MakeGenericMethod(methodInfo.GetGenericArguments()),
+                                 arguments.ToArray());
+          }
+        }
+        return ex;
+      });
       query = QueryProvider.CreateQuery<T>(e);
       return 0;
     }
@@ -84,10 +85,12 @@ namespace Xtensive.Orm.BulkOperations
 
     protected void Join(SqlQueryStatement statement, SqlSelect select)
     {
-      if (select.HasLimit)
+      if (select.HasLimit) {
         JoinWhenQueryHasLimitation(statement, select);
-      else
+      }
+      else {
         JoinWhenQueryHasNoLimitation(statement, select);
+      }
     }
 
     protected abstract void SetStatementFrom(SqlStatement statement, SqlTable from);
@@ -99,39 +102,40 @@ namespace Xtensive.Orm.BulkOperations
 
     private void JoinWhenQueryHasLimitation(SqlStatement statement, SqlSelect select)
     {
-      if (!SupportsLimitation() && !SupportsJoin())
+      if (!SupportsLimitation() && !SupportsJoin()) {
         throw new NotSupportedException("This provider does not supported limitation of affected rows.");
+      }
 
-      var sqlTableRef = @select.From as SqlTableRef;
-      if (sqlTableRef!=null) {
+      if (@select.From is SqlTableRef sqlTableRef) {
         SetStatementTable(statement, sqlTableRef);
         JoinedTableRef = sqlTableRef;
       }
-      else
+      else {
         sqlTableRef = GetStatementTable(statement);
+      }
+
       if (SupportsLimitation()) {
         SetStatementLimit(statement, select.Limit);
         SetStatementWhere(statement, select.Where);
       }
-      else
+      else {
         JoinViaFrom(statement, select);
-      
-      
+      }
     }
     private void JoinWhenQueryHasNoLimitation(SqlStatement statement, SqlSelect select)
     {
-      var sqlTableRef = @select.From as SqlTableRef;
-      if (sqlTableRef!=null) {
+      if (@select.From is SqlTableRef sqlTableRef) {
         SetStatementTable(statement, sqlTableRef);
         SetStatementWhere(statement, select.Where);
         JoinedTableRef = sqlTableRef;
         return;
       }
-
-      if (SupportsJoin())
+      if (SupportsJoin()) {
         JoinViaFrom(statement, select);
-      else
+      }
+      else {
         JoinViaIn(statement, select);
+      }
     }
 
     private void JoinViaIn(SqlStatement statement, SqlSelect @select)
@@ -165,37 +169,40 @@ namespace Xtensive.Orm.BulkOperations
       SetStatementFrom(statement, queryRef);
       var sqlTableRef = GetStatementTable(statement);
       SqlExpression whereExpression = null;
-      PrimaryIndexMapping indexMapping = PrimaryIndexes[0];
-      foreach (ColumnInfo columnInfo in indexMapping.PrimaryIndex.KeyColumns.Keys) {
+      var indexMapping = PrimaryIndexes[0];
+      foreach (var columnInfo in indexMapping.PrimaryIndex.KeyColumns.Keys) {
         var leftColumn = queryRef.Columns[columnInfo.Name];
-        var rightColumn = sqlTableRef==null ? GetStatementTable(statement).Columns[columnInfo.Name] : sqlTableRef.Columns[columnInfo.Name];
-        if (leftColumn==null || rightColumn==null)
+        var rightColumn = sqlTableRef == null
+          ? GetStatementTable(statement).Columns[columnInfo.Name]
+          : sqlTableRef.Columns[columnInfo.Name];
+        if (leftColumn == null || rightColumn == null) {
           throw new InvalidOperationException("Source query doesn't contain one of key columns of updated table.");
+        }
         var columnEqualityExperssion = SqlDml.Equals(queryRef.Columns[columnInfo.Name], sqlTableRef.Columns[columnInfo.Name]);
-        if (whereExpression==null)
-          whereExpression = columnEqualityExperssion;
-        else
-          whereExpression = SqlDml.And(whereExpression, columnEqualityExperssion);
+        whereExpression = whereExpression == null
+          ? columnEqualityExperssion
+          : SqlDml.And(whereExpression, columnEqualityExperssion);
       }
       SetStatementWhere(statement, whereExpression);
     }
 
     private void JoinViaJoin(SqlStatement statement, SqlSelect @select)
     {
-      PrimaryIndexMapping indexMapping = PrimaryIndexes[0];
-      SqlTableRef left = SqlDml.TableRef(indexMapping.Table);
-      SqlQueryRef right = SqlDml.QueryRef(@select);
+      var indexMapping = PrimaryIndexes[0];
+      var left = SqlDml.TableRef(indexMapping.Table);
+      var right = SqlDml.QueryRef(@select);
       SqlExpression joinExpression = null;
-      for (int i = 0; i < indexMapping.PrimaryIndex.KeyColumns.Count; i++)
-      {
-        SqlBinary binary = (left.Columns[i] == right.Columns[i]);
-        if (joinExpression.IsNullReference())
+      for (var i = 0; i < indexMapping.PrimaryIndex.KeyColumns.Count; i++) {
+        var binary = (left.Columns[i] == right.Columns[i]);
+        if (joinExpression.IsNullReference()) {
           joinExpression = binary;
-        else
+        }
+        else {
           joinExpression &= binary;
+        }
       }
       JoinedTableRef = left;
-      SqlJoinedTable joinedTable = left.InnerJoin(right, joinExpression);
+      var joinedTable = left.InnerJoin(right, joinExpression);
       SetStatementFrom(statement, joinedTable);
     }
 
