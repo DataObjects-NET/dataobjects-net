69c06c9bba12d8e86cc8c4533b854cc78e2657f0
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2021-02-20 16:28:10 +0500

    Copyright update

--- Orm/Xtensive.Orm/Collections/TypeClassifier.cs
+++ Orm/Xtensive.Orm/Collections/TypeClassifier.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2003-2010 Xtensive LLC.
+// Copyright (C) 2010-2021 Xtensive LLC.
 // All rights reserved.
 // For conditions of distribution and use, see license.
 // Created by: Alex Yakunin
