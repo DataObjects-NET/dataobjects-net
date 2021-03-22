8c8ea56ffe89803e08c7d2359fea02a0971aadbd
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2021-03-22 19:38:58 +0500

    Oracle: interval precision increse for literal values
    We use INTERVAL DAYS TO SECOND for columns which is equivalent to
    INTERVAL DAYS(2) TO SECOND(6). But for liternal values had less precision
    which is not good idea.

--- Orm/Xtensive.Orm.Oracle/Sql.Drivers.Oracle/v11/Translator.cs
+++ Orm/Xtensive.Orm.Oracle/Sql.Drivers.Oracle/v11/Translator.cs
@@ -1,6 +1,6 @@
-// Copyright (C) 2003-2010 Xtensive LLC.
-// All rights reserved.
-// For conditions of distribution and use, see license.
+// Copyright (C) 2009-2021 Xtensive LLC.
+// This code is distributed under MIT license terms.
+// See the License.txt file in the project root for more information.
 // Created by: Denis Krjuchkov
 // Created:    2009.07.17
 
@@ -11,6 +11,14 @@ namespace Xtensive.Sql.Drivers.Oracle.v11
 {
   internal class Translator : v10.Translator
   {
+    public override string Translate(SqlValueType type)
+    {
+      // we need to explicitly specify maximum interval precision
+      if (type.Type == SqlType.Interval)
+        return "INTERVAL DAY(6) TO SECOND(6)";
+      return base.Translate(type);
+    }
+
     // Constructors
 
     public Translator(SqlDriver driver)
