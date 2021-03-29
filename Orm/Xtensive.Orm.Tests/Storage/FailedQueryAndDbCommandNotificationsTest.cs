4987eefa49c63eccfc910d9f4e22666861e4dc0e
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2021-03-29 17:36:32 +0500

    Fix test for some storages
    - Correct low-level query for Firebird
    - Right exception to expect for Firebird and Mysql

--- Orm/Xtensive.Orm.Tests/Storage/FailedQueryAndDbCommandNotificationsTest.cs
+++ Orm/Xtensive.Orm.Tests/Storage/FailedQueryAndDbCommandNotificationsTest.cs
@@ -1,6 +1,6 @@
-﻿// Copyright (C) 2017 Xtensive LLC.
-// All rights reserved.
-// For conditions of distribution and use, see license.
+// Copyright (C) 2017-2021 Xtensive LLC.
+// This code is distributed under MIT license terms.
+// See the License.txt file in the project root for more information.
 // Created by: Julian Mamokin
 // Created:    2017.02.08
 
@@ -38,26 +38,35 @@ namespace Xtensive.Orm.Tests.Storage
     public void InvalidQueryTest()
     {
       Exception thrownException = null;
-      EventHandler<QueryEventArgs> invalidQueryHandler = (sender, args) => {thrownException = args.Exception;};
       using (var session = Domain.OpenSession()) {
-        session.Events.QueryExecuted += invalidQueryHandler;
-        using (session.OpenTransaction())
-          Assert.Throws<InvalidOperationException>(() => session.Query.All<TestModel>().Single(m => m.SomeStringField=="lol"));
-        session.Events.QueryExecuted -= invalidQueryHandler;
+        session.Events.QueryExecuted += InvalidQueryHandler;
+        using (session.OpenTransaction()) {
+          _ = Assert.Throws<InvalidOperationException>(() => session.Query.All<TestModel>().Single(m => m.SomeStringField == "lol"));
+        }
+        session.Events.QueryExecuted -= InvalidQueryHandler;
         Assert.IsNotNull(thrownException);
         Assert.AreSame(typeof (InvalidOperationException), thrownException.GetType());
+      }
+
+      void InvalidQueryHandler(object sender, QueryEventArgs args)
+      {
+        thrownException = args.Exception;
       }
     }
 
     [Test]
     public void ValidQueryTest()
     {
-      EventHandler<QueryEventArgs> validQueryHandler = (sender, args) => { Assert.IsNull(args.Exception); };
       using (var session = Domain.OpenSession()) {
-        session.Events.QueryExecuted += validQueryHandler;
+        session.Events.QueryExecuted += ValidQueryHandler;
         using (session.OpenTransaction()) 
           Assert.DoesNotThrow(() => session.Query.All<TestModel>().Single(m => m.SomeStringField=="string1"));
-        session.Events.QueryExecuted -= validQueryHandler;
+        session.Events.QueryExecuted -= ValidQueryHandler;
+      }
+
+      static void ValidQueryHandler(object sender, QueryEventArgs args)
+      {
+        Assert.IsNull(args.Exception);
       }
     }
 
@@ -65,32 +74,40 @@ namespace Xtensive.Orm.Tests.Storage
     public void InvalidDbCommandTest()
     {
       Exception commandExecutedException = null;
-      EventHandler<DbCommandEventArgs> invalidDbCommandHandler = (sender, args) => { commandExecutedException = args.Exception; };
       using (var session = Domain.OpenSession()) {
         using (session.OpenTransaction()) {
-          new TestModel { SomeStringField = "wat", SomeDateTimeField = DateTime.Now, UniqueValue = 1 };
+          _ = new TestModel { SomeStringField = "wat", SomeDateTimeField = DateTime.Now, UniqueValue = 1 };
           var expectedException = GetUniqueConstraintViolationExceptionType();
-          session.Events.DbCommandExecuted += invalidDbCommandHandler;
-          Assert.Throws(expectedException, () => session.SaveChanges());
-          session.Events.DbCommandExecuted -= invalidDbCommandHandler;
+          session.Events.DbCommandExecuted += InvalidDbCommandHandler;
+          _ = Assert.Throws(expectedException, () => session.SaveChanges());
+          session.Events.DbCommandExecuted -= InvalidDbCommandHandler;
           Assert.NotNull(commandExecutedException);
           Assert.AreEqual(expectedException, commandExecutedException.GetType());
         }
+      }
+
+      void InvalidDbCommandHandler(object sender, DbCommandEventArgs args)
+      {
+        commandExecutedException = args.Exception;
       }
     }
 
     [Test]
     public void ValidDbCommandTest()
     {
-      EventHandler<DbCommandEventArgs> validDbCommandHandler = (sender, args) => { Assert.IsNull(args.Exception); };
       using (var session = Domain.OpenSession()) {
         using (session.OpenTransaction()) {
-          new TestModel { SomeStringField = "wat", SomeDateTimeField = DateTime.Now, UniqueValue = 3 };
-          new TestModel { SomeStringField = "dat", SomeDateTimeField = DateTime.Now, UniqueValue = 4 };
-          session.Events.DbCommandExecuted += validDbCommandHandler;
+          _ = new TestModel { SomeStringField = "wat", SomeDateTimeField = DateTime.Now, UniqueValue = 3 };
+          _ = new TestModel { SomeStringField = "dat", SomeDateTimeField = DateTime.Now, UniqueValue = 4 };
+          session.Events.DbCommandExecuted += ValidDbCommandHandler;
           Assert.DoesNotThrow(() => session.SaveChanges());
-          session.Events.DbCommandExecuted -= validDbCommandHandler;
+          session.Events.DbCommandExecuted -= ValidDbCommandHandler;
         }
+      }
+
+      static void ValidDbCommandHandler(object sender, DbCommandEventArgs args)
+      {
+        Assert.IsNull(args.Exception);
       }
     }
 
@@ -98,7 +115,7 @@ namespace Xtensive.Orm.Tests.Storage
     {
       using(var session = Domain.OpenSession())
       using (var transaction = session.OpenTransaction()) {
-        new TestModel { SomeStringField = "string1", SomeDateTimeField = DateTime.Now, UniqueValue = 1 };
+        _ = new TestModel { SomeStringField = "string1", SomeDateTimeField = DateTime.Now, UniqueValue = 1 };
         transaction.Complete();
       }
     }
@@ -106,7 +123,7 @@ namespace Xtensive.Orm.Tests.Storage
     protected override DomainConfiguration BuildConfiguration()
     {
       var configuration = base.BuildConfiguration();
-      configuration.Types.Register(typeof (TestModel).Assembly, typeof (TestModel).Namespace);
+      configuration.Types.Register(typeof(TestModel).Assembly, typeof(TestModel).Namespace);
       configuration.UpgradeMode = DomainUpgradeMode.Recreate;
       return configuration;
     }
@@ -115,12 +132,10 @@ namespace Xtensive.Orm.Tests.Storage
     {
       var providerName = ProviderInfo.ProviderName;
       switch (providerName) {
-        case WellKnown.Provider.MySql:
         case WellKnown.Provider.Sqlite:
-        case WellKnown.Provider.Firebird:
-          return typeof (StorageException);
+          return typeof(StorageException);
         default:
-        return typeof (UniqueConstraintViolationException);
+          return typeof(UniqueConstraintViolationException);
       }
     }
   }
