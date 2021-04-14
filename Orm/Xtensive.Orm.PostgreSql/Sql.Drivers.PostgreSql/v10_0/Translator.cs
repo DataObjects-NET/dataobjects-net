1582c012144a4516a62a85a4d9b924313a99626a
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2021-04-14 10:46:27 +0500

    PostgreSQL:Add support for CrossApply feature for v10 and newer
    Since 9.3 there is native support for cross apply and outer apply.
    This is improve queries with apply significantly.

--- Orm/Xtensive.Orm.PostgreSql/Sql.Drivers.PostgreSql/v10_0/Translator.cs
+++ Orm/Xtensive.Orm.PostgreSql/Sql.Drivers.PostgreSql/v10_0/Translator.cs
@@ -1,13 +1,46 @@
-// Copyright (C) 2019-2020 Xtensive LLC.
+// Copyright (C) 2019-2021 Xtensive LLC.
 // This code is distributed under MIT license terms.
 // See the License.txt file in the project root for more information.
 // Created by: Alexey Kulakov
 // Created:    2019.09.25
 
+using System;
+using Xtensive.Sql.Compiler;
+using Xtensive.Sql.Dml;
+
 namespace Xtensive.Sql.Drivers.PostgreSql.v10_0
 {
   internal class Translator : v9_1.Translator
   {
+    public override string Translate(SqlCompilerContext context, SqlJoinExpression node, JoinSection section)
+    {
+      switch (section) {
+        case JoinSection.Specification: {
+          if (node.Expression == null) {
+            switch (node.JoinType) {
+              case SqlJoinType.InnerJoin:
+              case SqlJoinType.LeftOuterJoin:
+              case SqlJoinType.RightOuterJoin:
+              case SqlJoinType.FullOuterJoin:
+                throw new NotSupportedException();
+              case SqlJoinType.CrossApply:
+                return "CROSS JOIN LATERAL";
+              case SqlJoinType.LeftOuterApply:
+                return "LEFT JOIN LATERAL";
+            }
+          }
+          return Translate(node.JoinType) + " JOIN";
+        }
+        case JoinSection.Exit: {
+          if (node.JoinType == SqlJoinType.LeftOuterApply) {
+            return "ON TRUE";
+          }
+          return string.Empty;
+        }
+      }
+      return base.Translate(context, node, section);
+    }
+
     // Constructors
 
     public Translator(SqlDriver driver)
