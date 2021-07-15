430912d9614cdd973b0ad03eec97fbe431412e7b
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2021-07-15 18:11:12 +0500

    Copyright update

--- Orm/Xtensive.Orm/Orm/ConnectionInitEventData.cs
+++ Orm/Xtensive.Orm/Orm/ConnectionInitEventData.cs
@@ -1,4 +1,8 @@
-﻿using System.Data.Common;
+// Copyright (C) 2021 Xtensive LLC.
+// This code is distributed under MIT license terms.
+// See the License.txt file in the project root for more information.
+
+using System.Data.Common;
 using Xtensive.Core;
 
 namespace Xtensive.Orm
