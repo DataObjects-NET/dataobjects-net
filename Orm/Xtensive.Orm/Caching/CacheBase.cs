f434ac9466dc5f5f070e226ff53e729b672c3d81
Sergei Pavlov <spavlov@servicetitan.com>
2021-09-09 01:29:06 -0700

    Declare CacheBase methods as abstract; Change file summary

--- Orm/Xtensive.Orm/Caching/CacheBase.cs
+++ Orm/Xtensive.Orm/Caching/CacheBase.cs
@@ -19,15 +19,21 @@ namespace Xtensive.Caching
     public virtual TItem this[TKey key, bool markAsHit] => TryGetItem(key, markAsHit, out var item) ? item : default;
 
     /// <inheritdoc/>
-    public virtual int Count => throw new NotImplementedException();
+    public abstract int Count { get; }
 
-    public virtual TItem Add(TItem item, bool replaceIfExists) => throw new NotImplementedException();
+    /// <inheritdoc/>
+    public abstract TItem Add(TItem item, bool replaceIfExists);
 
     /// <inheritdoc/>
     public virtual void Add(TItem item) => Add(item, true);
 
+    /// <inheritdoc/>
     public virtual void Clear() => throw new NotImplementedException();
+
+    /// <inheritdoc/>
     public virtual bool ContainsKey(TKey key) => throw new NotImplementedException();
+
+    /// <inheritdoc/>
     public virtual IEnumerator<TItem> GetEnumerator() => throw new NotImplementedException();
 
     /// <inheritdoc/>
@@ -37,9 +43,14 @@ namespace Xtensive.Caching
       RemoveKey(KeyExtractor(item));
     }
 
-    public virtual void RemoveKey(TKey key) => throw new NotImplementedException();
-    public virtual void RemoveKey(TKey key, bool removeCompletely) => throw new NotImplementedException();
-    public virtual bool TryGetItem(TKey key, bool markAsHit, out TItem item) => throw new NotImplementedException();
+    /// <inheritdoc/>
+    public abstract void RemoveKey(TKey key);
+
+    /// <inheritdoc/>
+    public abstract void RemoveKey(TKey key, bool removeCompletely);
+
+    /// <inheritdoc/>
+    public abstract bool TryGetItem(TKey key, bool markAsHit, out TItem item);
   }
 }
 
