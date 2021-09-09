3b486c5cea41601335af15af4022ddf38e3522ac
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2021-09-09 16:08:32 +0500

    CacheBase changes
    - regrouped members
    - virtual members that had NotSupportedException as default implementation replaced with abstract
    - correct copyright

--- Orm/Xtensive.Orm/Caching/CacheBase.cs
+++ Orm/Xtensive.Orm/Caching/CacheBase.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2007-2021 Xtensive LLC.
+// Copyright (C) 2021 Xtensive LLC.
 // This code is distributed under MIT license terms.
 // See the License.txt file in the project root for more information.
 
@@ -16,10 +16,16 @@ namespace Xtensive.Caching
     public virtual Converter<TItem, TKey> KeyExtractor { [DebuggerStepThrough]get; protected set; }
 
     /// <inheritdoc/>
+    public abstract int Count { get; }
+
+    /// <inheritdoc/>
     public virtual TItem this[TKey key, bool markAsHit] => TryGetItem(key, markAsHit, out var item) ? item : default;
 
     /// <inheritdoc/>
-    public abstract int Count { get; }
+    public abstract bool TryGetItem(TKey key, bool markAsHit, out TItem item);
+
+    /// <inheritdoc/>
+    public abstract bool ContainsKey(TKey key);
 
     /// <inheritdoc/>
     public abstract TItem Add(TItem item, bool replaceIfExists);
@@ -28,13 +34,7 @@ namespace Xtensive.Caching
     public virtual void Add(TItem item) => Add(item, true);
 
     /// <inheritdoc/>
-    public virtual void Clear() => throw new NotImplementedException();
-
-    /// <inheritdoc/>
-    public virtual bool ContainsKey(TKey key) => throw new NotImplementedException();
-
-    /// <inheritdoc/>
-    public virtual IEnumerator<TItem> GetEnumerator() => throw new NotImplementedException();
+    public abstract void Clear();
 
     /// <inheritdoc/>
     public virtual void Remove(TItem item)
@@ -50,7 +50,7 @@ namespace Xtensive.Caching
     public abstract void RemoveKey(TKey key, bool removeCompletely);
 
     /// <inheritdoc/>
-    public abstract bool TryGetItem(TKey key, bool markAsHit, out TItem item);
+    public abstract IEnumerator<TItem> GetEnumerator();
   }
 }
 
