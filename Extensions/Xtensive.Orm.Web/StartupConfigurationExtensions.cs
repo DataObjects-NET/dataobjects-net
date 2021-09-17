e5d2e487a2a4112f6439643b294dd275bdb4147f
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2021-09-17 15:55:57 +0500

    Startup extensions
    - some were renamed
    - new extensions added
    - summary updates

--- Extensions/Xtensive.Orm.Web/StartupConfigurationExtensions.cs
+++ Extensions/Xtensive.Orm.Web/StartupConfigurationExtensions.cs
@@ -2,8 +2,12 @@
 // This code is distributed under MIT license terms.
 // See the License.txt file in the project root for more information.
 
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Xtensive.Orm.Web.Filters;
+using Xtensive.Orm.Web.Middleware;
 
 namespace Xtensive.Orm.Web
 {
@@ -13,23 +17,38 @@ namespace Xtensive.Orm.Web
   public static class StartupConfigurationExtensions
   {
     /// <summary>
-    /// Adds <see cref="SessionManager"/> to ASP.NET Core middleware pipeline.
+    /// Adds <see cref="OpenSessionMiddleware"/> to ASP.NET Core middleware pipeline.
     /// </summary>
     /// <param name="builder"><see cref="IApplicationBuilder"/> instance.</param>
-    /// <returns><paramref name="builder"/> with <see cref="SessionManager"/>.</returns>
-    public static IApplicationBuilder UseDataObjectsSessionsOpener(this IApplicationBuilder builder)
-    {
-      return builder.UseMiddleware<OpenSessionMiddlewere>();
-    }
+    /// <returns><paramref name="builder"/> with <see cref="OpenSessionMiddleware"/>.</returns>
+    public static IApplicationBuilder UseDataObjectsSessionOpener(this IApplicationBuilder builder) =>
+      builder.UseMiddleware<OpenSessionMiddleware>();
 
     /// <summary>
-    /// Registers a SessionAccessor as scoped service for futher usage with
+    /// Registers a <see cref="SessionAccessor"/> as scoped service to be able to
+    /// have access to session/transaction as parameter in either MVC controllers' actions or
+    /// constuctors, and in Razor Pages.
     /// </summary>
-    /// <param name="services"></param>
-    /// <returns>The same instance collection with service.</returns>
-    public static IServiceCollection AddSessionAccessor(this IServiceCollection services)
-    {
-      return services.AddScoped<SessionAccessor>();
-    }
+    /// <param name="services">A service collection to add <see cref="SessionAccessor"/>.</param>
+    /// <returns>The same collection instance with registered accessor.</returns>
+    public static IServiceCollection AddDataObjectsSessionAccessor(this IServiceCollection services) =>
+      services.AddScoped<SessionAccessor>();
+
+    /// <summary>
+    /// Adds <see cref="SessionActionFilter"/> to action filters.
+    /// </summary>
+    /// <param name="filters">The filter collection to add the filter.</param>
+    /// <returns>A <see cref="IFilterMetadata"/> representing added filter.</returns>
+    public static IFilterMetadata AddDataObjectsSessionActionFilter(this FilterCollection filters) =>
+      AddDataObjectsSessionActionFilter(filters, 0);
+
+    /// <summary>
+    /// Adds <see cref="SessionActionFilter"/> to action filters.
+    /// </summary>
+    /// <param name="filters">The filter collection to add the filter.</param>
+    /// <param name="order">The order of the added filter.</param>
+    /// <returns>A <see cref="IFilterMetadata"/> representing added filter.</returns>
+    public static IFilterMetadata AddDataObjectsSessionActionFilter(this FilterCollection filters, int order) =>
+      filters.Add(typeof(SessionActionFilter), order);
   }
 }
