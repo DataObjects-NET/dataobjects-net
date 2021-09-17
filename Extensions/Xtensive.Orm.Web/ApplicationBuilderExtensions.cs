cb962453de8e969c1f4694f42a2b112d168ded61
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2021-09-17 15:57:07 +0500

    Mark obsolete entire class instead of members

--- Extensions/Xtensive.Orm.Web/ApplicationBuilderExtensions.cs
+++ Extensions/Xtensive.Orm.Web/ApplicationBuilderExtensions.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2020 Xtensive LLC.
+// Copyright (C) 2019-2021 Xtensive LLC.
 // This code is distributed under MIT license terms.
 // See the License.txt file in the project root for more information.
 
@@ -11,6 +11,7 @@ namespace Xtensive.Orm.Web
   /// <summary>
   /// Contains helper methods to apply <see cref="SessionManager"/> middleware to ASP.NET Core pipeline.
   /// </summary>
+  [Obsolete]
   public static class ApplicationBuilderExtensions
   {
     /// <summary>
@@ -18,7 +19,6 @@ namespace Xtensive.Orm.Web
     /// </summary>
     /// <param name="builder"><see cref="IApplicationBuilder"/> instance.</param>
     /// <returns><paramref name="builder"/> with <see cref="SessionManager"/>.</returns>
-    [Obsolete]
     public static IApplicationBuilder UseSessionManager(this IApplicationBuilder builder)
     {
       return builder.UseMiddleware<SessionManager>();
@@ -30,7 +30,6 @@ namespace Xtensive.Orm.Web
     /// <param name="builder"><see cref="IApplicationBuilder"/> instance.</param>
     /// <param name="sessionProvider">User-defined session provider which will be used instead of built-in provider.</param>
     /// <returns><paramref name="builder"/> with <see cref="SessionManager"/>.</returns>
-    [Obsolete]
     public static IApplicationBuilder UseSessionManager(this IApplicationBuilder builder, Func<Pair<Session, System.IDisposable>> sessionProvider)
     {
       SessionManager.SessionProvider = sessionProvider;
