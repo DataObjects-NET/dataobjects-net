444bddc0c3e8ba5ae21a2cd6826ae62dd04de25d
Sergei Pavlov <spavlov@servicetitan.com>
2021-11-03 14:05:58 -0700

    Remove unnecessary immutable object creation

--- Orm/Xtensive.Orm/Orm/Linq/Translator.Materialization.cs
+++ Orm/Xtensive.Orm/Orm/Linq/Translator.Materialization.cs
@@ -107,10 +107,6 @@ namespace Xtensive.Orm.Linq
     private Materializer
       BuildMaterializer(ProjectionExpression projection, IEnumerable<Parameter<Tuple>> tupleParameters)
     {
-      var tupleReader = Expression.Parameter(typeof (RecordSetReader), "tupleReader");
-      var session = Expression.Parameter(typeof (Session), "session");
-      var parameterContext = Expression.Parameter(WellKnownOrmTypes.ParameterContext, "parameterContext");
-
       var itemProjector = projection.ItemProjector;
       var materializationInfo = itemProjector.Materialize(context, tupleParameters);
       var elementType = itemProjector.Item.Type;
