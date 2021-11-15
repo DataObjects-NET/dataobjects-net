746dbceb9e216ef9744572b5144e38d500b5d7db
Sergei Pavlov <spavlov@servicetitan.com>
2021-11-15 15:27:27 -0800

    Refactor static readonly field initializers for better readability (#54)
    * Refactor static readonly field initializers for better readability
    
    * Fix case
    
    * Fix PascalCase
    
    * Use .ToArray() in static readonly fields for thread safety
    
    Bugfix: reorder static fields in CompareProvider because of dependency

--- Orm/Xtensive.Orm/Orm/Linq/Model/QueryableMethodInfo.cs
+++ Orm/Xtensive.Orm/Orm/Linq/Model/QueryableMethodInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -8,25 +8,16 @@ namespace Xtensive.Orm.Linq.Model
 {
   internal static class QueryableMethodInfo
   {
-    public static readonly MethodInfo Select;
-    public static readonly MethodInfo GroupBy;
-    public static readonly MethodInfo GroupByWithElementSelector;
-    public static readonly MethodInfo GroupByWithResultSelector;
-    public static readonly MethodInfo GroupByWithElementAndResultSelectors;
+    public static readonly MethodInfo Select = MethodOf(q => q.Select(o => o));
+    public static readonly MethodInfo GroupBy = MethodOf(q => q.GroupBy(o => o));
+    public static readonly MethodInfo GroupByWithElementSelector = MethodOf(q => q.GroupBy(o => o, o => o.ToString()));
+    public static readonly MethodInfo GroupByWithResultSelector = MethodOf(q => q.GroupBy(o => o, (key, items) => items.Count()));
+    public static readonly MethodInfo GroupByWithElementAndResultSelectors = MethodOf(q => q.GroupBy(o => o, o => o.ToString(), (key, items) => items.Count()));
 
     private static MethodInfo MethodOf<T>(Expression<Func<IQueryable<object>, IQueryable<T>>> methodExpression)
     {
       var callExpression = (MethodCallExpression) methodExpression.Body.StripCasts();
       return callExpression.Method.GetGenericMethodDefinition();
-    }
-
-    static QueryableMethodInfo()
-    {
-      Select = MethodOf(q => q.Select(o => o));
-      GroupBy = MethodOf(q => q.GroupBy(o => o));
-      GroupByWithElementSelector = MethodOf(q => q.GroupBy(o => o, o => o.ToString()));
-      GroupByWithResultSelector = MethodOf(q => q.GroupBy(o => o, (key, items) => items.Count()));
-      GroupByWithElementAndResultSelectors = MethodOf(q => q.GroupBy(o => o, o => o.ToString(), (key, items) => items.Count()));
     }
   }
 }
