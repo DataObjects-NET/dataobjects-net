a25fe9e6d166d547410c1db00e3325e3e181e2c0
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2021-12-20 16:26:38 +0500

    Full-text extraction for Azure Sql Server

--- Orm/Xtensive.Orm.SqlServer/Sql.Drivers.SqlServer/Azure/Extractor.cs
+++ Orm/Xtensive.Orm.SqlServer/Sql.Drivers.SqlServer/Azure/Extractor.cs
@@ -1,6 +1,6 @@
-// Copyright (C) 2003-2010 Xtensive LLC.
-// All rights reserved.
-// For conditions of distribution and use, see license.
+// Copyright (C) 2010-2021 Xtensive LLC.
+// This code is distributed under MIT license terms.
+// See the License.txt file in the project root for more information.
 // Created by: Denis Krjuchkov
 // Created:    2010.03.02
 
@@ -8,11 +8,6 @@ namespace Xtensive.Sql.Drivers.SqlServer.Azure
 {
   internal class Extractor : v12.Extractor
   {
-    protected override void ExtractFulltextIndexes()
-    {
-    }
-
-
     // Constructors
 
     public Extractor(SqlDriver driver)
