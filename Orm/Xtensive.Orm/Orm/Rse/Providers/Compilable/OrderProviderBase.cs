64276ac562a74e2bbc857de9300cc1f95f0a7ebc
Alex Ustinov <alex.ustinov@servicetitan.com>
2021-12-29 12:32:44 -0800

    Remove excessive MapTransform usage in the OrderProviderBase

--- Orm/Xtensive.Orm/Orm/Rse/Providers/Compilable/OrderProviderBase.cs
+++ Orm/Xtensive.Orm/Orm/Rse/Providers/Compilable/OrderProviderBase.cs
@@ -5,17 +5,9 @@
 // Created:    2008.07.08
 
 using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using Xtensive.Collections;
-using Xtensive.Comparison;
 using Xtensive.Core;
-using Xtensive.Orm.FullTextSearchCondition.Nodes;
-using Xtensive.Tuples;
-using Tuple = Xtensive.Tuples.Tuple;
-using Xtensive.Tuples.Transform;
-using Xtensive.Orm.Rse.Providers;
 
 namespace Xtensive.Orm.Rse.Providers
 {
@@ -30,21 +22,6 @@ namespace Xtensive.Orm.Rse.Providers
     /// </summary>
     public DirectionCollection<int> Order { get; private set; }
 
-    /// <summary>
-    /// Gets the key extractor transform.
-    /// </summary>
-    public MapTransform OrderKeyExtractorTransform { get; private set; }
-
-    /// <summary>
-    /// Extracts the key part from <paramref name="tuple"/> using <see cref="OrderKeyExtractorTransform"/>.
-    /// </summary>
-    /// <param name="tuple">The tuple to extract the key from.</param>
-    /// <returns>A tuple containing extracted order key.</returns>
-    public Tuple OrderKeyExtractor(Tuple tuple)
-    {
-      return OrderKeyExtractorTransform.Apply(TupleTransformType.Auto, tuple);
-    }
-
     /// <inheritdoc/>
     protected override RecordSetHeader BuildHeader()
     {
@@ -57,32 +34,6 @@ namespace Xtensive.Orm.Rse.Providers
       return Order
         .Select(pair => Header.Columns[pair.Key].Name + (pair.Value == Direction.Negative ? " desc" : string.Empty))
         .ToCommaDelimitedString();
-    }
-
-    /// <inheritdoc/>
-    protected override void Initialize()
-    {
-      base.Initialize();
-      var comparisonRules = new ComparisonRules[Order.Count];
-      for (int i = 0; i < Order.Count; i++) {
-        var orderItem = Order[i];
-        var culture = CultureInfo.InvariantCulture;
-        var column = Header.Columns[orderItem.Key];
-        var mappedColumn = column as MappedColumn;
-        if (mappedColumn != null && mappedColumn.ColumnInfoRef != null)
-          culture = mappedColumn.ColumnInfoRef.CultureInfo;
-        comparisonRules[i] = new ComparisonRule(orderItem.Value, culture);
-      }
-
-      var fieldTypes = new Type[Order.Count];
-      var map = new int[Order.Count];
-      for (var i = 0; i < Order.Count; i++) {
-        var p = Order[i];
-        fieldTypes[i] = Header.Columns[p.Key].Type;
-        map[i] = p.Key;
-      }
-      var orderKeyDescriptor = TupleDescriptor.Create(fieldTypes);
-      OrderKeyExtractorTransform = new MapTransform(true, orderKeyDescriptor, map);
     }
 
 
