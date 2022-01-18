806bafd0573d6c86304b3220aaad47adc2dd8411
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2022-01-18 09:48:54 +0500

    Return back old DateTime substraction formula for some scenarios

--- Orm/Xtensive.Orm.SqlServer/Sql.Drivers.SqlServer/v13/Compiler.cs
+++ Orm/Xtensive.Orm.SqlServer/Sql.Drivers.SqlServer/v13/Compiler.cs
@@ -10,36 +10,73 @@ namespace Xtensive.Sql.Drivers.SqlServer.v13
 {
   internal class Compiler : v12.Compiler
   {
-    protected const string DayPart = "DAY";
-    protected const string MillisecondPart = "MS";
-    protected const string NanosecondPart = "NS";
-    protected const string ZeroTime = "'00:00:00.0000000'";
+    protected const string MicrosecondPart = "MCS";
+    protected const long NanosecondsPerMicrosecond = 1000;
 
     /// <inheritdoc/>
     public override void Visit(SqlFunctionCall node)
     {
-      if (node.FunctionType == SqlFunctionType.DateTimeOffsetTimeOfDay) {
-        DateTimeOffsetTimeOfDay(node.Arguments[0]).AcceptVisitor(this);
-      }
-      else {
-        base.Visit(node);
+      switch (node.FunctionType) {
+        case SqlFunctionType.DateTimeOffsetTimeOfDay:
+          DateTimeOffsetTimeOfDay(node.Arguments[0]).AcceptVisitor(this);
+          break;
+        case SqlFunctionType.IntervalToMilliseconds: {
+          if (node.Arguments[0] is SqlBinary binary 
+              && (binary.NodeType == SqlNodeType.DateTimeMinusDateTime || binary.NodeType == SqlNodeType.DateTimeOffsetMinusDateTimeOffset)) {
+            Visit(DateDiffBigMicrosecond(binary.Right, binary.Left) / CastToLong(1000));
+          }
+          else {
+            base.Visit(node);
+          }
+          break;
+        }
+        case SqlFunctionType.IntervalToNanoseconds: {
+          if (node.Arguments[0] is SqlBinary binary
+              && (binary.NodeType == SqlNodeType.DateTimeMinusDateTime || binary.NodeType == SqlNodeType.DateTimeOffsetMinusDateTimeOffset)) {
+            // we have to use time consuming algorithm here because
+            // DATEDIFF_BIG can throw arithmetic overflow on nanoseconds
+            // so we should handle it by this big formula
+            Visit(CastToLong(DateTimeSubtractDateTimeExpensive(binary.Right, binary.Left)));
+          }
+          else {
+            base.Visit(node);
+          }
+          break;
+        }
+        default:
+          base.Visit(node); break;
       }
     }
 
     protected override SqlExpression DateTimeSubtractDateTime(SqlExpression date1, SqlExpression date2)
     {
-      return DateDiffBigNanosecond(date2, date1);
+      return CastToDecimal(DateDiffBigMicrosecond(date2, date1), 18, 0) * CastToLong(1000);
+    }
+
+    private SqlExpression DateTimeSubtractDateTimeExpensive(SqlExpression date1, SqlExpression date2)
+    {
+      return CastToDecimal(DateDiffBigDay(date2, date1), 18, 0) * NanosecondsPerDay
+          + CastToDecimal(DateDiffBigMillisecond(DateAddDay(date2, DateDiffBigDay(date2, date1)), date1), 18, 0) * NanosecondsPerMillisecond;
     }
 
     #region Static Helpers
 
-    protected static SqlUserFunctionCall DateDiffBigNanosecond(SqlExpression date1, SqlExpression date2) =>
-      SqlDml.FunctionCall("DATEDIFF_BIG", SqlDml.Native(NanosecondPart), date1, date2);
-
-    private static SqlExpression DateTimeOffsetTimeOfDay(SqlExpression dateTimeOffset) =>
+    protected static SqlExpression DateTimeOffsetTimeOfDay(SqlExpression dateTimeOffset) =>
       DateDiffBigNanosecond(
         SqlDml.Native(ZeroTime),
         SqlDml.Cast(dateTimeOffset, new SqlValueType("time")));
+
+    protected static SqlUserFunctionCall DateDiffBigNanosecond(SqlExpression date1, SqlExpression date2) =>
+      SqlDml.FunctionCall("DATEDIFF_BIG", SqlDml.Native(NanosecondPart), date1, date2);
+
+    protected static SqlUserFunctionCall DateDiffBigMicrosecond(SqlExpression date1, SqlExpression date2) =>
+      SqlDml.FunctionCall("DATEDIFF_BIG", SqlDml.Native(MicrosecondPart), date1, date2);
+
+    protected static SqlUserFunctionCall DateDiffBigMillisecond(SqlExpression date1, SqlExpression date2) =>
+      SqlDml.FunctionCall("DATEDIFF_BIG", SqlDml.Native(MillisecondPart), date1, date2);
+
+    protected static SqlUserFunctionCall DateDiffBigDay(SqlExpression date1, SqlExpression date2) =>
+      SqlDml.FunctionCall("DATEDIFF_BIG", SqlDml.Native(DayPart), date1, date2);
 
     #endregion
 
