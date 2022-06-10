de91fe0aced8052413e63fe23c1f025f6a909c93
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2022-06-10 10:54:31 +0500

    Remove the v8.0's overriding method, the v5.7's is enough

--- Orm/Xtensive.Orm.MySql/Sql.Drivers.MySql/v8_0/Compiler.cs
+++ Orm/Xtensive.Orm.MySql/Sql.Drivers.MySql/v8_0/Compiler.cs
@@ -4,47 +4,10 @@
 // Created by: Alexey Kulakov
 // Created:    2022.02.03
 
-using Xtensive.Sql.Compiler;
-using Xtensive.Sql.Dml;
-
 namespace Xtensive.Sql.Drivers.MySql.v8_0
 {
   internal class Compiler : v5_7.Compiler
   {
-    /// <inheritdoc/>
-    public override void Visit(SqlQueryExpression node)
-    {
-      using (context.EnterScope(node)) {
-        var wrapLeft = node.Left is SqlSelect sL
-          && (sL.OrderBy.Count > 0 || sL.HasLimit || sL.Lock != SqlLockType.Empty);
-        var wrapRight = node.Left is SqlSelect sR
-          && (sR.OrderBy.Count > 0 || sR.HasLimit || sR.Lock != SqlLockType.Empty);
-
-        AppendTranslatedEntry(node);
-        if (wrapLeft) {
-          _ = context.Output.Append("(");
-          node.Left.AcceptVisitor(this);
-          _ = context.Output.Append(")");
-        }
-        else {
-          node.Left.AcceptVisitor(this);
-        }
-
-        AppendTranslated(node.NodeType);
-        AppendTranslated(node, QueryExpressionSection.All);
-
-        if (wrapRight) {
-          _ = context.Output.Append("(");
-          node.Right.AcceptVisitor(this);
-          _ = context.Output.Append(")");
-        }
-        else {
-          node.Right.AcceptVisitor(this);
-        }
-        AppendTranslatedExit(node);
-      }
-    }
-
 
     // Constructors
 
