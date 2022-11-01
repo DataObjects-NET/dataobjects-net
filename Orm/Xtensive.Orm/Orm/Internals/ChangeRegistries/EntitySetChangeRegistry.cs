8dddd4002f1c3d8671da82344bb34c59247ce27a
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2022-11-01 13:53:50 +0500

    Group registries of change in one subfolder
    Namespaces are the same

--- Orm/Xtensive.Orm/Orm/Internals/ChangeRegistries/EntitySetChangeRegistry.cs
+++ Orm/Xtensive.Orm/Orm/Internals/ChangeRegistries/EntitySetChangeRegistry.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2014 Xtensive LLC.
+// Copyright (C) 2014 Xtensive LLC.
 // All rights reserved.
 // For conditions of distribution and use, see license.
 // Created by: Alexey Kulakov
