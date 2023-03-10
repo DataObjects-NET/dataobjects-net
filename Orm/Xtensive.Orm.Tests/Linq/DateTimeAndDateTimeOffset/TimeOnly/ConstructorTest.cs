ff7c10c6beb7f85691026d6c40876deb799d4448
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2023-03-10 16:59:59 +0500

    Add tests for TimeOnly(ticks) ctor

--- Orm/Xtensive.Orm.Tests/Linq/DateTimeAndDateTimeOffset/TimeOnly/ConstructorTest.cs
+++ Orm/Xtensive.Orm.Tests/Linq/DateTimeAndDateTimeOffset/TimeOnly/ConstructorTest.cs
@@ -22,7 +22,8 @@ namespace Xtensive.Orm.Tests.Linq.DateTimeAndDateTimeOffset.TimeOnlys
           .Select(e => new {
             Entity = e,
             ConstructedTime = new TimeOnly(e.Hour, e.Minute, e.Second, e.Millisecond) })
-          .Where(a => a.ConstructedTime == FirstMillisecondTimeOnly).OrderBy(a => a.Entity.Id).ToList(3);
+          .Where(a => a.ConstructedTime == FirstMillisecondTimeOnly)
+          .OrderBy(a => a.Entity.Id).ToList(3);
         Assert.That(result.Count, Is.EqualTo(1));
       });
     }
@@ -36,7 +37,8 @@ namespace Xtensive.Orm.Tests.Linq.DateTimeAndDateTimeOffset.TimeOnlys
             Entity = e,
             ConstructedTime = new TimeOnly(e.Hour, e.Minute, e.Second)
           })
-          .Where(a => a.ConstructedTime == FirstTimeOnly).OrderBy(a => a.Entity.Id).ToList(3);
+          .Where(a => a.ConstructedTime == FirstTimeOnly)
+          .OrderBy(a => a.Entity.Id).ToList(3);
         Assert.That(result.Count, Is.EqualTo(1));
       });
     }
@@ -48,6 +50,140 @@ namespace Xtensive.Orm.Tests.Linq.DateTimeAndDateTimeOffset.TimeOnlys
         var result = s.Query.All<AllPossiblePartsEntity>()
           .Select(e => new { Entity = e, ConstructedTime = new TimeOnly(e.Hour, e.Minute) })
           .Where(a => a.ConstructedTime == FirstTimeOnly.Add(TimeSpan.FromSeconds(-5)))
+          .OrderBy(a => a.Entity.Id).ToList(3);
+        Assert.That(result.Count, Is.EqualTo(1));
+      });
+    }
+
+    [Test]
+    public void CtorTicksLiteralValue()
+    {
+      var ticksPerHour = new TimeOnly(1, 0).Ticks;
+      var ticksPerMinute = new TimeOnly(0, 1).Ticks;
+      var ticksPerSecond = new TimeOnly(0, 0, 1).Ticks;
+      var testTicks = ticksPerHour * FirstTimeOnly.Hour +
+        ticksPerMinute * FirstTimeOnly.Minute +
+        ticksPerSecond * FirstTimeOnly.Second;
+
+      ExecuteInsideSession((s) => {
+        var result = s.Query.All<AllPossiblePartsEntity>()
+          .Select(e => new { Entity = e, ConstructedTime = new TimeOnly(testTicks) })
+          .Where(a => a.ConstructedTime == FirstTimeOnly)
+          .OrderBy(a => a.Entity.Id).ToList(3);
+        Assert.That(result.Count, Is.EqualTo(1));
+      });
+    }
+
+    [Test]
+    public void CtorTicksLiteralExpressions()
+    {
+      var ticksPerHour = new TimeOnly(1, 0).Ticks;
+      var ticksPerMinute = new TimeOnly(0, 1).Ticks;
+      var ticksPerSecond = new TimeOnly(0, 0, 1).Ticks;
+      var testTicks = ticksPerHour * FirstTimeOnly.Hour +
+        ticksPerMinute * FirstTimeOnly.Minute +
+        ticksPerSecond * FirstTimeOnly.Second;
+
+      ExecuteInsideSession((s) => {
+        var result = s.Query.All<AllPossiblePartsEntity>()
+          .Select(e => new { Entity = e, ConstructedTime = new TimeOnly(testTicks + 1000 - 1000) })
+          .Where(a => a.ConstructedTime == FirstTimeOnly)
+          .OrderBy(a => a.Entity.Id).ToList(3);
+        Assert.That(result.Count, Is.EqualTo(1));
+      });
+    }
+
+    [Test]
+    public void CtorTicksFromIntervalTicks()
+    {
+      Require.ProviderIsNot(StorageProvider.MySql);
+
+      ExecuteInsideSession((s) => {
+        var result = s.Query.All<AllPossiblePartsEntity>()
+          .Select(e => new { Entity = e, ConstructedTime = new TimeOnly(e.TimeSpan.Ticks) })
+          .Where(a => a.ConstructedTime == FirstMillisecondTimeOnly)
+          .OrderBy(a => a.Entity.Id).ToList(3);
+        Assert.That(result.Count, Is.EqualTo(1));
+      });
+    }
+
+    [Test]
+    public void MysqlCtorTicksFromIntervalTicks()
+    {
+      Require.ProviderIs(StorageProvider.MySql);
+
+      if (StorageProviderInfo.Instance.Info.StorageVersion <= StorageProviderVersion.MySql55) {
+        ExecuteInsideSession((s) => {
+          var result = s.Query.All<AllPossiblePartsEntity>()
+            .Select(e => new { Entity = e, ConstructedTime = new TimeOnly(e.TimeSpan.Ticks) })
+            .Where(a => a.ConstructedTime == FirstTimeOnly)
+            .OrderBy(a => a.Entity.Id).ToList(3);
+          Assert.That(result.Count, Is.EqualTo(1));
+        });
+      }
+      else {
+        ExecuteInsideSession((s) => {
+          var result = s.Query.All<AllPossiblePartsEntity>()
+            .Select(e => new { Entity = e, ConstructedTime = new TimeOnly(e.TimeSpan.Ticks) })
+            .Where(a => a.ConstructedTime == FirstMillisecondTimeOnly)
+            .OrderBy(a => a.Entity.Id).ToList(3);
+          Assert.That(result.Count, Is.EqualTo(1));
+        });
+      }
+    }
+
+    [Test]
+    public void TicksFromColumnsBasedExpression()
+    {
+      var ticksPerHour = new TimeOnly(1, 0).Ticks;
+      var ticksPerMinute = new TimeOnly(0, 1).Ticks;
+      var ticksPerSecond = new TimeOnly(0, 0, 1).Ticks;
+      var testTicks = ticksPerHour * FirstTimeOnly.Hour +
+        ticksPerMinute * FirstTimeOnly.Minute +
+        ticksPerSecond * FirstTimeOnly.Second;
+
+      Assert.That(new TimeOnly(testTicks).Ticks, Is.EqualTo(testTicks));
+      Assert.That(new TimeOnly(testTicks), Is.EqualTo(FirstTimeOnly));
+
+      ExecuteInsideSession((s) => {
+        var result = s.Query.All<AllPossiblePartsEntity>()
+          .Select(e => new {
+            Entity = e,
+            ConstructedTime = new TimeOnly(e.Hour * ticksPerHour + e.Minute * ticksPerMinute + e.Second * ticksPerSecond)
+          })
+          .Where(a => a.ConstructedTime == FirstTimeOnly)
+          .OrderBy(a => a.Entity.Id).ToList(3);
+        Assert.That(result.Count, Is.EqualTo(1));
+      });
+    }
+
+    [Test]
+    public void TicksFromColumnsBasedExpressionMilliseconds()
+    {
+      if (StorageProviderInfo.Instance.CheckProviderIs(StorageProvider.MySql)) {
+        Require.ProviderVersionAtLeast(StorageProviderVersion.MySql56);
+      }
+
+      var ticksPerHour = new TimeOnly(1, 0).Ticks;
+      var ticksPerMinute = new TimeOnly(0, 1).Ticks;
+      var ticksPerSecond = new TimeOnly(0, 0, 1).Ticks;
+      var ticksPerMillisecond = new TimeOnly(0,0,0,1).Ticks;
+      var testTicks = ticksPerHour * FirstMillisecondTimeOnly.Hour +
+        ticksPerMinute * FirstMillisecondTimeOnly.Minute +
+        ticksPerSecond * FirstMillisecondTimeOnly.Second +
+        ticksPerMillisecond * FirstMillisecondTimeOnly.Millisecond;
+
+      Assert.That(new TimeOnly(testTicks).Ticks, Is.EqualTo(testTicks));
+      Assert.That(new TimeOnly(testTicks), Is.EqualTo(FirstMillisecondTimeOnly));
+
+      ExecuteInsideSession((s) => {
+        var result = s.Query.All<AllPossiblePartsEntity>()
+          .Select(e => new {
+            Entity = e,
+            ConstructedTime = new TimeOnly(
+              e.Hour * ticksPerHour + e.Minute * ticksPerMinute + e.Second * ticksPerSecond + e.Millisecond * ticksPerMillisecond)
+          })
+          .Where(a => a.ConstructedTime == FirstMillisecondTimeOnly)
           .OrderBy(a => a.Entity.Id).ToList(3);
         Assert.That(result.Count, Is.EqualTo(1));
       });
