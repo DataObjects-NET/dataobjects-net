6ef9ab5e2df66864f553c671352dfa3439032f55
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2023-07-21 18:53:55 +0500

    Fix typo in Strings

--- Orm/Xtensive.Orm/Strings.Designer.cs
+++ Orm/Xtensive.Orm/Strings.Designer.cs
@@ -19,7 +19,7 @@ namespace Xtensive {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Strings {
@@ -9864,7 +9864,7 @@ namespace Xtensive {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Value can not be less then {0}..
+        ///   Looks up a localized string similar to Value can not be less than {0}..
         /// </summary>
         internal static string ValueCanNotBeLessThenX {
             get {
