0bbe950163d449c6eedbf9272e5016280f4dbe65
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2023-07-28 16:17:32 +0500

    Prevent continuation happening if parent task was canceled
    otherwise OperationCanceledException will be swallowed

--- Orm/Xtensive.Orm/Orm/Domain.cs
+++ Orm/Xtensive.Orm/Orm/Domain.cs
@@ -362,7 +362,7 @@ namespace Xtensive.Orm
                 }
                 exceptionDispatchInfo = ExceptionDispatchInfo.Capture(ex);
               }
-            }, TaskContinuationOptions.ExecuteSynchronously)
+            }, TaskContinuationOptions.NotOnCanceled | TaskContinuationOptions.ExecuteSynchronously)
             .ConfigureAwait(false);
         }
         catch (OperationCanceledException) {
