4ef287c9d17e3d5f80291a41fa285266c1c75168
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2024-11-29 18:22:50 +0500

    EnumRewriter handles nullable values correctly

--- Orm/Xtensive.Orm/Orm/Linq/Expressions/Visitors/EnumRewriter.cs
+++ Orm/Xtensive.Orm/Orm/Linq/Expressions/Visitors/EnumRewriter.cs
@@ -11,6 +11,8 @@ namespace Xtensive.Orm.Linq.Expressions.Visitors
 {
   internal sealed class EnumRewriter : ExpressionVisitor
   {
+    private readonly static System.Collections.Concurrent.ConcurrentDictionary<Type, Delegate> NullableValueCreators = new();
+
     public static Expression Rewrite(Expression target)
     {
       return new EnumRewriter().Visit(target);
@@ -41,13 +43,34 @@ namespace Xtensive.Orm.Linq.Expressions.Visitors
     {
       if (c.Type.StripNullable().IsEnum) {
         var underlyingType = Enum.GetUnderlyingType(c.Type.StripNullable());
-        if (c.Type.IsNullable())
-          underlyingType = WellKnownTypes.NullableOfT.CachedMakeGenericType(underlyingType);
-        var underlyingTypeValue = Convert.ChangeType(c.Value, underlyingType);
+
+        object underlyingTypeValue;
+        if (c.Type.IsNullable()) {
+          underlyingTypeValue = (c.Value is null)
+            ? null
+            : CreateNullableUnderlyingValue(c.Value, underlyingType);
+        }
+        else {
+          underlyingTypeValue = Convert.ChangeType(c.Value, underlyingType);
+        }
         var constantExpression = Expression.Constant(underlyingTypeValue);
         return Expression.Convert(constantExpression, c.Type);
       }
       return c;
+    }
+
+    private static object CreateNullableUnderlyingValue(object value, Type underlyingType)
+    {
+      var instanceCreator = NullableValueCreators.GetOrAdd(underlyingType, (t) => {
+        var parameter = Expression.Parameter(underlyingType);
+        var type = WellKnownTypes.NullableOfT.MakeGenericType(underlyingType);
+        var ctor = type.GetConstructors()[0];
+        var body = Expression.New(ctor, parameter);
+        var instanceCreatorLambda = Expression.Lambda(body, parameter);
+        return instanceCreatorLambda.Compile();
+      });
+
+      return instanceCreator.DynamicInvoke(value);
     }
 
     private EnumRewriter()
