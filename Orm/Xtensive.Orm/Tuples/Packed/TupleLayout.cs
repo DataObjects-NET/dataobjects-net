ec89a00ded67877319dbc94da757503546345e09
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2024-12-25 12:12:49 +0500

    After-merge changes

--- Orm/Xtensive.Orm/Tuples/Packed/TupleLayout.cs
+++ Orm/Xtensive.Orm/Tuples/Packed/TupleLayout.cs
@@ -74,10 +74,8 @@ namespace Xtensive.Tuples.Packed
           ReferenceEquals(type, WellKnownTypes.Decimal) ? DecimalAccessor :
           ReferenceEquals(type, WellKnownTypes.Single) ? SingleAccessor :
           ReferenceEquals(type, WellKnownTypes.DateTimeOffset) ? DateTimeOffsetAccessor :
-#if NET6_0_OR_GREATER
           ReferenceEquals(type, WellKnownTypes.DateOnly) ? DateOnlyAccessor :
           ReferenceEquals(type, WellKnownTypes.TimeOnly) ? TimeOnlyAccessor :
-#endif
           ReferenceEquals(type, WellKnownTypes.Int16) ? Int16Accessor :
           ReferenceEquals(type, WellKnownTypes.Byte) ? ByteAccessor :
           ReferenceEquals(type, WellKnownTypes.SByte) ? SByteAccessor :
@@ -97,10 +95,8 @@ namespace Xtensive.Tuples.Packed
           ReferenceEquals(type, WellKnownTypes.NullableTimeSpan) ? TimeSpanAccessor :
           ReferenceEquals(type, WellKnownTypes.NullableDateTimeOffset) ? DateTimeOffsetAccessor :
           ReferenceEquals(type, WellKnownTypes.NullableSingle) ? SingleAccessor :
-#if NET6_0_OR_GREATER
           ReferenceEquals(type, WellKnownTypes.NullableDateOnly) ? DateOnlyAccessor :
           ReferenceEquals(type, WellKnownTypes.NullableTimeOnly) ? TimeOnlyAccessor :
-#endif
           ReferenceEquals(type, WellKnownTypes.NullableGuid) ? GuidAccessor :
           ReferenceEquals(type, WellKnownTypes.NullableInt16) ? Int16Accessor :
           ReferenceEquals(type, WellKnownTypes.NullableByte) ? ByteAccessor :
