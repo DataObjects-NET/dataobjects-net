fb05c33607cf7a4b3b633b1ae7c44f48255e4340
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2025-07-10 18:49:14 +0500

    IgnoreIfGithubActions attribute has provider filter

--- Orm/Xtensive.Orm.Tests.Framework/NUnitFrameworkExtensions/IgnoreIfGithubActionsAttribute.cs
+++ Orm/Xtensive.Orm.Tests.Framework/NUnitFrameworkExtensions/IgnoreIfGithubActionsAttribute.cs
@@ -10,14 +10,29 @@ namespace Xtensive.Orm.Tests
   {
     public string Reason { get; private set; }
 
+    public StorageProvider? Provider { get; private set; }
+
     protected override void OnBeforeTestCheck(ITest test)
     {
       if (TestInfo.IsGitHubActions) {
+        if (Provider.HasValue && !StorageProviderInfo.Instance.CheckProviderIs(Provider.Value))
+          return;
         throw new IgnoreException(
           string.IsNullOrEmpty(Reason)
             ? "Ignored due to run on Github Actions. Test is always failing."
             : $"Ignored due to run on Github Actions. {Reason}");
       }
+    }
+
+    public IgnoreIfGithubActionsAttribute(StorageProvider provider, string reason)
+    {
+      Reason = reason;
+      Provider = provider;
+    }
+
+    public IgnoreIfGithubActionsAttribute(StorageProvider provider)
+    {
+      Provider = provider;
     }
 
     public IgnoreIfGithubActionsAttribute(string reason)
