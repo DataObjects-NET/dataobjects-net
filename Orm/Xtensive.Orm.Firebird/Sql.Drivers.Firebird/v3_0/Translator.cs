582ea2b28b45a89f5dd2b1134f5a0690c6826d13
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2025-09-14 16:12:59 +0500

    Firebird providers: add Firebird v3 provider

---
+++ Orm/Xtensive.Orm.Firebird/Sql.Drivers.Firebird/v3_0/Translator.cs
@@ -0,0 +1,21 @@
+// Copyright (C) 2021-2022 Xtensive LLC.
+// This code is distributed under MIT license terms.
+// See the License.txt file in the project root for more information.
+
+using Xtensive.Sql.Compiler;
+using Xtensive.Sql.Dml;
+
+namespace Xtensive.Sql.Drivers.Firebird.v3_0
+{
+  internal class Translator : v2_5.Translator
+  {
+
+    // Constructors
+
+    /// <inheritdoc/>
+    public Translator(SqlDriver driver)
+      : base(driver)
+    {
+    }
+  }
+}
