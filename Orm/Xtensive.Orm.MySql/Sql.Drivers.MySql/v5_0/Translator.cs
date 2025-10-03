2628b5e789590a4b5ad2cce1223ecc31b4abd114
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2025-10-03 17:29:10 +0500

    No new line after batch item for MySQL

--- Orm/Xtensive.Orm.MySql/Sql.Drivers.MySql/v5_0/Translator.cs
+++ Orm/Xtensive.Orm.MySql/Sql.Drivers.MySql/v5_0/Translator.cs
@@ -35,10 +35,10 @@ namespace Xtensive.Sql.Drivers.MySql.v5_0
       get { return ";"; }
     }
 
-    public override string BatchItemDelimiter
-    {
-      get { return ";\r\n"; }
-    }
+    //public override string BatchItemDelimiter
+    //{
+    //  get { return ";\r\n"; }
+    //}
 
     public override void Initialize()
     {
