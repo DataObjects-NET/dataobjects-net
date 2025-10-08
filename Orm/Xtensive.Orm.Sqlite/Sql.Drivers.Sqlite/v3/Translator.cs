1f8efded0d50fa4b43f9daf321a6e849198c120a
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2025-10-08 12:58:34 +0500

    Remove commented code

--- Orm/Xtensive.Orm.Sqlite/Sql.Drivers.Sqlite/v3/Translator.cs
+++ Orm/Xtensive.Orm.Sqlite/Sql.Drivers.Sqlite/v3/Translator.cs
@@ -41,12 +41,6 @@ namespace Xtensive.Sql.Drivers.Sqlite.v3
       get { return ";"; }
     }
 
-    ///// <inheritdoc/>
-    //public override string BatchItemDelimiter
-    //{
-    //  get { return ";\r\n"; }
-    //}
-
     /// <inheritdoc/>
     public override void Initialize()
     {
