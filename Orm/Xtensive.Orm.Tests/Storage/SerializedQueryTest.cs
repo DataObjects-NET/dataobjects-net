0904cbad30c4ff7c2c93f087377cb62e8644830c
Alexey Kulakov <alexey.kulakov@dataobjects.net>
2026-05-18 11:00:51 +0500

    SerializedQueryTest: cloning expression using different ways of serialization

--- Orm/Xtensive.Orm.Tests/Storage/SerializedQueryTest.cs
+++ Orm/Xtensive.Orm.Tests/Storage/SerializedQueryTest.cs
@@ -8,10 +8,7 @@ using System;
 using System.IO;
 using NUnit.Framework;
 using Xtensive.Core;
-using Xtensive.Linq.SerializableExpressions;
 using Xtensive.Orm.Linq;
-using Xtensive.Orm.Configuration;
-using Xtensive.Orm.Serialization;
 using Xtensive.Orm.Tests.ObjectModel;
 using Xtensive.Orm.Tests.ObjectModel.ChinookDO;
 using System.Linq;
@@ -23,7 +20,8 @@ namespace Xtensive.Orm.Tests.Storage
   public class SerializedQueryTest : ChinookDOModelTest
   {
     [Test]
-    public void CombinedTest()
+    [Obsolete("No BinaryFormating allowed from now on")]
+    public void BinarySerializationTest()
     {
       Require.AllFeaturesSupported(ProviderFeatures.RowNumber);
       var query = Session.Query.All<MediaType>()
@@ -34,6 +32,60 @@ namespace Xtensive.Orm.Tests.Storage
 
       var serializableExpression = query.Expression.ToSerializableExpression();
       var serializedExpression = Cloner.CloneViaBinarySerialization(serializableExpression);
+
+      var deserializedExpression = serializedExpression.ToExpression();
+      var deserializedQuery = new Queryable<MediaType>((QueryProvider) query.Provider, deserializedExpression);
+      var result = deserializedQuery.ToList();
+    }
+
+    [Test]
+    public void DataContractXmlSerializationTest()
+    {
+      Require.AllFeaturesSupported(ProviderFeatures.RowNumber);
+      var query = Session.Query.All<MediaType>()
+        .Where(c => c.Name == "MPEG audio file")
+        .Where(c => c.MediaTypeId > 0)
+        .Take(5)
+        .Skip(0);
+
+      var serializableExpression = query.Expression.ToSerializableExpression();
+      var serializedExpression = Cloner.CloneViaXmlSerialization(serializableExpression, Cloner.SerializationDomains.System.Value);
+
+      var deserializedExpression = serializedExpression.ToExpression();
+      var deserializedQuery = new Queryable<MediaType>((QueryProvider) query.Provider, deserializedExpression);
+      var result = deserializedQuery.ToList();
+    }
+
+    [Test]
+    public void DataContractJsonSerializationTest()
+    {
+      Require.AllFeaturesSupported(ProviderFeatures.RowNumber);
+      var query = Session.Query.All<MediaType>()
+        .Where(c => c.Name == "MPEG audio file")
+        .Where(c => c.MediaTypeId > 0)
+        .Take(5)
+        .Skip(0);
+
+      var serializableExpression = query.Expression.ToSerializableExpression();
+      var serializedExpression = Cloner.CloneViaJsonSerialization(serializableExpression, useDataContract: true);
+
+      var deserializedExpression = serializedExpression.ToExpression();
+      var deserializedQuery = new Queryable<MediaType>((QueryProvider) query.Provider, deserializedExpression);
+      var result = deserializedQuery.ToList();
+    }
+
+    [Test]
+    public void JsonSerializerSerializationTest()
+    {
+      Require.AllFeaturesSupported(ProviderFeatures.RowNumber);
+      var query = Session.Query.All<MediaType>()
+        .Where(c => c.Name == "MPEG audio file")
+        .Where(c => c.MediaTypeId > 0)
+        .Take(5)
+        .Skip(0);
+
+      var serializableExpression = query.Expression.ToSerializableExpression();
+      var serializedExpression = Cloner.CloneViaJsonSerialization(serializableExpression);
 
       var deserializedExpression = serializedExpression.ToExpression();
       var deserializedQuery = new Queryable<MediaType>((QueryProvider) query.Provider, deserializedExpression);
